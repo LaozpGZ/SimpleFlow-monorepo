25377d2b90ac66c2faa4f212c26a9ec9e640280a
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-10-29 10:33:51 +0800

    feat: routing sdk add support for native token pools

--- packages/routing-sdk/src/graph/edge.ts
+++ packages/routing-sdk/src/graph/edge.ts
@@ -1,10 +1,14 @@
+import { sortCurrencies } from '@pancakeswap/swap-sdk-core'
+
 import type { Edge, Pool, Vertice } from '../types'
+import { getVerticeKey } from './vertice'
 
 export function getNeighbour(e: Edge, v: Vertice): Vertice {
   return e.vertice0.currency.equals(v.currency) ? e.vertice1 : e.vertice0
 }
 
 export function getEdgeKey(p: Pool, vertA: Vertice, vertB: Vertice): string {
-  const [vert0, vert1] = vertA.currency.wrapped.sortsBefore(vertB.currency.wrapped) ? [vertA, vertB] : [vertB, vertA]
-  return `${vert0.currency.chainId}-${vert0.currency.wrapped.address}-${vert1.currency.wrapped.address}-${p.getId()}`
+  const [currency0] = sortCurrencies([vertA.currency, vertB.currency])
+  const [vert0, vert1] = currency0.wrapped.equals(vertA.currency) ? [vertA, vertB] : [vertB, vertA]
+  return `${getVerticeKey(vert0)}-${getVerticeKey(vert1)}-${p.getId()}`
 }
