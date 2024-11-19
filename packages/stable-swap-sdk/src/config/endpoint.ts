eebabc405f89088368c6f40604336a80d30ee6d0
chefmomota <momota@pancakeswap.com>
2024-11-19 10:47:17 +0800

    fix: Conflict

---
+++ packages/stable-swap-sdk/src/config/endpoint.ts
@@ -0,0 +1,2 @@
+// export const STABLE_SWAP_API = 'https://configs.pancakeswap.com/api/data/cached/stable-swap'
+export const STABLE_SWAP_API = 'https://cms-public-api-7ys4p.ondigitalocean.app/api/data/cached/stable-swap'
--- packages/stable-swap-sdk/src/constants/pools/index.ts
+++ packages/stable-swap-sdk/src/constants/pools/index.ts
@@ -1,14 +1,66 @@
 import { ChainId } from '@pancakeswap/chains'
-
-import { isStableSwapSupported, STABLE_POOL_MAP, STABLE_SUPPORTED_CHAIN_IDS } from './pools'
+import { ERC20Token } from '@pancakeswap/sdk'
+import { STABLE_SWAP_API } from '../../config/endpoint'
 import { StableSwapPool } from '../../types'
+import { isStableSwapSupported, STABLE_SUPPORTED_CHAIN_IDS } from './pools'
 
-export function getStableSwapPools(chainId: ChainId): StableSwapPool[] {
+const stableSwapCache: Record<string, StableSwapPool[]> = {}
+
+export const fetchStableSwapData = async (chainId: ChainId) => {
+  const cacheKey = `${chainId}-all}`
+
+  // Return cached data if it exists
+  if (stableSwapCache[cacheKey]) {
+    return stableSwapCache[cacheKey]
+  }
+
+  try {
+    const params = { chainId }
+    const queryString = Object.entries(params)
+      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+      .join('&')
+
+    const response = await fetch(`${STABLE_SWAP_API}?${queryString}`, {
+      signal: AbortSignal.timeout(3000),
+    })
+    const result = await response.json()
+    const newData: StableSwapPool[] = result.map((p: any) => ({
+      ...p,
+      token: new ERC20Token(
+        p.token.chainId,
+        p.token.address,
+        p.token.decimals,
+        p.token.symbol,
+        p.token.name,
+        p.token.projectLink,
+      ),
+      quoteToken: new ERC20Token(
+        p.quoteToken.chainId,
+        p.quoteToken.address,
+        p.quoteToken.decimals,
+        p.quoteToken.symbol,
+        p.quoteToken.name,
+        p.quoteToken.projectLink,
+      ),
+    }))
+
+    // Cache the result before returning it
+    stableSwapCache[cacheKey] = newData
+
+    return newData
+  } catch (error) {
+    return []
+  }
+}
+
+export async function getStableSwapPools(chainId: ChainId): Promise<StableSwapPool[]> {
   // Stable swap is only supported on BSC chain & BSC testnet
   if (!isStableSwapSupported(chainId)) {
     return []
   }
-  return STABLE_POOL_MAP[chainId] || []
+
+  const stableSwapData = await fetchStableSwapData(chainId)
+  return stableSwapData
 }
 
 export { isStableSwapSupported, STABLE_SUPPORTED_CHAIN_IDS }
