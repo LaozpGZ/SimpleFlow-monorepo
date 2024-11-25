ebef442f2d8bf962c0b517da8d9fd46168425a68
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-11-25 10:43:40 +0800

    chore: affiliate-sunset-modal

---
+++ apps/web/src/components/Modal/AffiliateSunsetModal.tsx
@@ -0,0 +1,88 @@
+import { useTranslation } from '@pancakeswap/localization'
+import { LinkExternal, Text, useModal } from '@pancakeswap/uikit'
+import { ReactNode, useCallback, useEffect } from 'react'
+import { useAccount } from 'wagmi'
+
+import DisclaimerModal from 'components/DisclaimerModal'
+import { useAffiliateExpired } from 'hooks/useAffiliateExpired'
+import { useUserAcknowledgement } from 'hooks/useUserAcknowledgement'
+
+const transRegex = /(%[^%]+%)/
+
+function Trans({ text, data = {} }: { text: string; data?: { [key: string]: ReactNode } }) {
+  const parts = text.split(transRegex)
+  return parts.map((p) => {
+    if (!transRegex.test(p)) {
+      return p
+    }
+    const key = p.replace(/%/g, '')
+    return data[key] || p
+  })
+}
+
+export function AffiliateSunsetModal() {
+  const { t } = useTranslation()
+  const { address } = useAccount()
+  const expired = useAffiliateExpired(address)
+  const [ack, setACK] = useUserAcknowledgement('affiliate-referral-sunset-v1')
+  const onConfirm = useCallback(() => setACK(true), [setACK])
+
+  const [onOptionsConfirmModalPresent] = useModal(
+    <DisclaimerModal
+      bodyMaxWidth={['100%', '100%', '100%', '640px']}
+      bodyMaxHeight="80vh"
+      modalHeader={t('Important Update: Closure of the PancakeSwap Affiliate Program')}
+      header={
+        <>
+          <Text bold fontSize="1.25rem">
+            {t('Important Update')}
+          </Text>
+          <Text mt="1.5rem">
+            {t(`The PancakeSwap Affiliate Program will officially close on %time%`, {
+              time: 'December 31, 2024',
+            })}
+          </Text>
+          <Text mt="1.5rem">
+            <Trans
+              text={t(
+                `Please claim any pending trading discounts before this date, as discount redemptions will no longer be available after that.`,
+                { time: 'Oct 31, 2024' },
+              )}
+              data={{
+                claim: (
+                  <LinkExternal
+                    bold
+                    style={{ display: 'inline-flex' }}
+                    showExternalIcon={false}
+                    href="/affiliates-program/dashboard"
+                  >
+                    {t('claim')}
+                  </LinkExternal>
+                ),
+              }}
+            />
+          </Text>
+        </>
+      }
+      id="affiliate-sunset-modal"
+      checks={[
+        {
+          key: 'checkbox',
+          content: t(`I've read and understood the following update.`),
+        },
+      ]}
+      onSuccess={onConfirm}
+    />,
+    false,
+    false,
+    'affiliateExpiredModal',
+  )
+
+  useEffect(() => {
+    if (expired && ack === false) {
+      onOptionsConfirmModalPresent()
+    }
+  }, [expired, ack, onOptionsConfirmModalPresent])
+
+  return null
+}
