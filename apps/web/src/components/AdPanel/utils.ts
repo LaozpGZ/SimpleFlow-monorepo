34402b5201b203538d6740a2ffa178f0985e9cf2
Chef Penguin <penguin@pancakeswap.com>
2024-11-29 16:10:31 +0530

    chore: memoize, remove springboard ad

--- apps/web/src/components/AdPanel/utils.ts
+++ apps/web/src/components/AdPanel/utils.ts
@@ -1,3 +1,5 @@
+import memoize from 'lodash/memoize'
+
 const AD_ASSETS_URL = 'https://assets.pancakeswap.finance/web/promotions'
 
-export const getImageUrl = (asset: string) => `${AD_ASSETS_URL}/${asset}.png`
+export const getImageUrl = memoize((asset: string) => `${AD_ASSETS_URL}/${asset}.png`)
