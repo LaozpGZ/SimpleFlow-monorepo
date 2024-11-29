017d730f083ab20970ab990b9469f96d5872bf59
Chef Penguin <penguin@pancakeswap.com>
2024-11-29 16:00:50 +0530

    chore: hide springboard ad

--- apps/web/src/components/AdPanel/config.tsx
+++ apps/web/src/components/AdPanel/config.tsx
@@ -4,7 +4,6 @@ import { AdMevProtection } from './Ads/AdMevProtection'
 import { AdOptionsTrading } from './Ads/AdOptionsTrading'
 import { AdPCSX } from './Ads/AdPCSX'
 import { AdRocker } from './Ads/AdRocker'
-import { AdSpringboard } from './Ads/AdSpringboard'
 import { AdTelegramBot } from './Ads/AdTelegramBot'
 import { ExpandableAd } from './Expandable/ExpandableAd'
 import { shouldRenderOnPages } from './renderConditions'
@@ -33,10 +32,6 @@ export const useAdConfig = () => {
       component: <ExpandableAd />,
       priority: Priority.FIRST_AD,
       shouldRender: [shouldRenderOnPages(['/buy-crypto', '/', '/prediction'])],
-    },
-    {
-      id: 'ad-springboard',
-      component: <AdSpringboard />,
     },
     {
       id: 'ad-mev',
