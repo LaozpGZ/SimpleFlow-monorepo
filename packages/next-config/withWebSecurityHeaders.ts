3d0fce458a0d9b2668bfd714bed2cc2a88dd983c
memoyil <2213635+memoyil@users.noreply.github.com>
2024-12-03 09:14:42 +0100

    fix: Keep same-origin and override it in app

--- packages/next-config/withWebSecurityHeaders.ts
+++ packages/next-config/withWebSecurityHeaders.ts
@@ -39,7 +39,7 @@ export function withWebSecurityHeaders(config: NextConfig): NextConfig {
           },
           {
             key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin-allow-popups',
+            value: 'same-origin',
           },
           // createCSP(),
         ],
