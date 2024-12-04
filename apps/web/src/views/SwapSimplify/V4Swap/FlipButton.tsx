0c5bfbc87de12c7f3528af578c7433dd0abfb700
Chef Penguin <penguin@pancakeswap.com>
2024-12-04 16:09:21 +0530

    chore: add flip animation

--- apps/web/src/views/SwapSimplify/V4Swap/FlipButton.tsx
+++ apps/web/src/views/SwapSimplify/V4Swap/FlipButton.tsx
@@ -10,7 +10,7 @@ import { AutoRow } from 'components/Layout/Row'
 import { Field } from 'state/swap/actions'
 import { useSwapState } from 'state/swap/hooks'
 import { useSwapActionHandlers } from 'state/swap/useSwapActionHandlers'
-import { styled } from 'styled-components'
+import { keyframes, styled } from 'styled-components'
 
 import { useTheme } from '@pancakeswap/hooks'
 import { SwapUIV2 } from '@pancakeswap/widgets-internal'
@@ -22,6 +22,18 @@ import ArrowLight from '../../../../public/images/swap/arrow_light.json' assert 
 
 const Lottie = dynamic(() => import('lottie-react'), { ssr: false })
 
+const switchAnimation = keyframes`
+  from {transform: rotate(0deg);}
+  to {transform: rotate(180deg);}
+`
+
+const FlipButtonWrapper = styled.div`
+  will-change: transform;
+  &.switch-animation {
+    animation: ${switchAnimation} 0.25s forwards ease-in-out;
+  }
+`
+
 export const Line = styled.div`
   position: absolute;
   left: -16px;
@@ -32,6 +44,7 @@ export const Line = styled.div`
 `
 
 export const FlipButton = memo(function FlipButton() {
+  const flipButtonRef = useRef<HTMLDivElement>(null)
   const lottieRef = useRef<LottieRefCurrentProps | null>(null)
   const { isDark } = useTheme()
   const { isDesktop } = useMatchBreakpoints()
@@ -52,21 +65,38 @@ export const FlipButton = memo(function FlipButton() {
     })
   }, [onSwitchTokens, inputCurrencyId, outputCurrencyId])
 
+  const handleAnimatedButtonClick = useCallback(() => {
+    onFlip()
+
+    if (flipButtonRef.current && !flipButtonRef.current.classList.contains('switch-animation')) {
+      flipButtonRef.current.classList.add('switch-animation')
+    }
+  }, [onFlip])
+
+  const handleAnimationEnd = useCallback(() => {
+    flipButtonRef.current?.classList.remove('switch-animation')
+  }, [])
+
   return (
     <AutoColumn justify="space-between" position="relative">
       <Line />
       <AutoRow justify="center" style={{ padding: '0 1rem', marginTop: '1em' }}>
         {isDesktop ? (
-          <Lottie
-            lottieRef={lottieRef}
-            animationData={animationData}
-            style={{ height: '40px', cursor: 'pointer' }}
-            onClick={onFlip}
-            autoplay={false}
-            loop={false}
-            onMouseEnter={() => lottieRef.current?.playSegments([7, 19], true)}
-            onMouseLeave={() => lottieRef.current?.playSegments([39, 54], true)}
-          />
+          <FlipButtonWrapper ref={flipButtonRef} onAnimationEnd={handleAnimationEnd}>
+            <Lottie
+              lottieRef={lottieRef}
+              animationData={animationData}
+              style={{ height: '40px', cursor: 'pointer' }}
+              onClick={handleAnimatedButtonClick}
+              autoplay={false}
+              loop={false}
+              onMouseEnter={() => lottieRef.current?.playSegments([7, 19], true)}
+              onMouseLeave={() => {
+                handleAnimationEnd()
+                lottieRef.current?.playSegments([39, 54], true)
+              }}
+            />
+          </FlipButtonWrapper>
         ) : (
           <SwapUIV2.SwitchButtonV2 onClick={onFlip} />
         )}
