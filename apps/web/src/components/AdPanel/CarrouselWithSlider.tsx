6bb96fc88d944f277ca4830d748b56ef43935f7f
memoyil <2213635+memoyil@users.noreply.github.com>
2024-12-14 10:19:10 +0300

    chore: Change pagination bullet color in adpanel to make it visible

--- apps/web/src/components/AdPanel/CarrouselWithSlider.tsx
+++ apps/web/src/components/AdPanel/CarrouselWithSlider.tsx
@@ -32,7 +32,7 @@ export const StyledSwiper = styled(Swiper)<{ $showPagination?: boolean }>`
   .swiper-pagination-bullet {
     display: ${({ $showPagination = true }) => ($showPagination ? 'block' : 'none')};
     position: relative;
-    background-color: ${({ theme }) => theme.colors.inputSecondary};
+    background-color: ${({ theme }) => theme.colors.primary3D};
     margin: 0 !important;
     flex-grow: 1;
     border-radius: 4px;
