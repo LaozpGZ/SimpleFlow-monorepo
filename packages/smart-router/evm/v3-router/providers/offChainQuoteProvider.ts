d8f11e783e6ee00f1f58af15b77c2d1e0f2aa604
chefjackson <116779127+chefjackson@users.noreply.github.com>
2024-12-20 22:28:43 +0800

    fix: issue with exact output

--- packages/smart-router/evm/v3-router/providers/offChainQuoteProvider.ts
+++ packages/smart-router/evm/v3-router/providers/offChainQuoteProvider.ts
@@ -45,7 +45,8 @@ export function createOffChainQuoteProvider(): QuoteProvider {
       const routesWithQuote: RouteWithQuote[] = []
       for (const route of routes) {
         try {
-          const { pools, amount, output } = route
+          const { pools, amount, output, input } = route
+          const quoteCurrency = amount.currency.wrapped.equals(input.wrapped) ? output : input
           let quote = amount
           const initializedTickCrossedList = Array(pools.length).fill(0)
           let quoteSuccess = true
@@ -75,7 +76,7 @@ export function createOffChainQuoteProvider(): QuoteProvider {
             continue
           }
 
-          const finalQuote = CurrencyAmount.fromRawAmount(output, quote.quotient)
+          const finalQuote = CurrencyAmount.fromRawAmount(quoteCurrency, quote.quotient)
           const { gasEstimate, gasCostInUSD, gasCostInToken } = gasModel.estimateGasCost(
             {
               ...route,
