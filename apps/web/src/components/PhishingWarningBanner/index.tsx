631a2035f8da16e1da802ac1c6da2c16e83af498
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2024-12-27 19:06:38 +0800

    chore: list new TC

--- apps/web/src/components/PhishingWarningBanner/index.tsx
+++ apps/web/src/components/PhishingWarningBanner/index.tsx
@@ -11,6 +11,12 @@ import { Countdown } from './Countdown'
 import { Step1 } from './Step1'
 import { Step2 } from './Step2'
 import { Step3 } from './Step3'
+import {
+  TradingCompetitionAiTech,
+  TradingCompetitionApt,
+  TradingCompetitionBfg,
+  TradingCompetitionVinu,
+} from './TradingCompetition'
 
 const Container = styled(Flex).withConfig({ shouldForwardProp: (prop) => !['$background'].includes(prop) })<{
   $background?: string
@@ -115,6 +121,30 @@ const CONFIG: BannerConfig[] = [
     stripeImage: `${ASSET_CDN}/web/banners/pcsx/pcsx-bg-medium.png`,
     stripeImageWidth: '92px',
     stripeImageAlt: 'PCSX',
+  },
+  {
+    component: TradingCompetitionAiTech,
+    stripeImage: `${ASSET_CDN}/web/promotions/aitech_competion.png`,
+    stripeImageWidth: '92px',
+    stripeImageAlt: 'aitech_competion',
+  },
+  {
+    component: TradingCompetitionBfg,
+    stripeImage: `${ASSET_CDN}/web/promotions/bfg_competion.png`,
+    stripeImageWidth: '92px',
+    stripeImageAlt: 'bfg_competion',
+  },
+  {
+    component: TradingCompetitionApt,
+    stripeImage: `${ASSET_CDN}/web/promotions/apt_competion.png`,
+    stripeImageWidth: '92px',
+    stripeImageAlt: 'apt_competion',
+  },
+  {
+    component: TradingCompetitionVinu,
+    stripeImage: `${ASSET_CDN}/web/promotions/vinu_competion.png`,
+    stripeImageWidth: '92px',
+    stripeImageAlt: 'vinu_competion',
   },
 ]
 
