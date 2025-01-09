c5dbc55f92e2656ebf0fb7cd8c029545b5877fea
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-01-09 14:14:21 +0800

    chore: bsc testnet mock data

--- apps/web/src/views/Idos/hooks/ido/useCurrentIDOConfig.ts
+++ apps/web/src/views/Idos/hooks/ido/useCurrentIDOConfig.ts
@@ -2,5 +2,5 @@ import { ChainId } from '@pancakeswap/chains'
 import { ifos } from '../../config'
 
 export const useCurrentIDOConfig = () => {
-  return { activeIfo: { ...ifos[0], chainId: ChainId.ARBITRUM_ONE }, isPending: false }
+  return { activeIfo: { ...ifos[1], chainId: ChainId.BSC_TESTNET }, isPending: false }
 }
