11441b389c39f95bbd093ce6877f2e86370d7fc1
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-01-14 06:09:44 +0800

    chore: update toast message

--- apps/web/src/views/Idos/hooks/ido/useIDODepositCallback.tsx
+++ apps/web/src/views/Idos/hooks/ido/useIDODepositCallback.tsx
@@ -6,31 +6,38 @@ import useAccountActiveChain from 'hooks/useAccountActiveChain'
 import useCatchTxError from 'hooks/useCatchTxError'
 import { useIDOContract } from 'hooks/useContract'
 import { useCallback } from 'react'
+import { isUserRejected } from 'utils/sentry'
 
 export const useIDODepositCallback = () => {
   const idoContract = useIDOContract()
   const { t } = useTranslation()
   const { account } = useAccountActiveChain()
-  const { toastSuccess } = useToast()
-  const { fetchWithCatchTxError, loading: isPending } = useCatchTxError()
+  const { toastSuccess, toastWarning } = useToast()
+  const { fetchWithCatchTxError, loading: isPending } = useCatchTxError({ throwUserRejectError: true })
 
   const deposit = useCallback(
     async (amount: CurrencyAmount<Currency>) => {
       if (!account || !idoContract) return
       const value = amount.currency.isNative ? amount.quotient : 0n
       const amountPool = amount.currency.isNative ? 0n : amount.quotient
-      const receipt = await fetchWithCatchTxError(() =>
-        idoContract.write.depositPool([amountPool], {
-          account,
-          chain: idoContract.chain,
-          value,
-        }),
-      )
-      if (receipt?.status) {
-        toastSuccess(t('Deposit successful'), <ToastDescriptionWithTx txHash={receipt.transactionHash} />)
+      try {
+        const receipt = await fetchWithCatchTxError(() =>
+          idoContract.write.depositPool([amountPool], {
+            account,
+            chain: idoContract.chain,
+            value,
+          }),
+        )
+        if (receipt?.status) {
+          toastSuccess(t('Deposit successful'), <ToastDescriptionWithTx txHash={receipt.transactionHash} />)
+        }
+      } catch (error) {
+        if (isUserRejected(error)) {
+          toastWarning(t('You canceled deposit'), t(`You didn't confirm BNB deposit in your wallet`))
+        }
       }
     },
-    [account, idoContract, fetchWithCatchTxError, toastSuccess, t],
+    [account, idoContract, fetchWithCatchTxError, toastSuccess, toastWarning, t],
   )
 
   return {
