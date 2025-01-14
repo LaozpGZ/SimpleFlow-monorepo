7c08fc2af2fdb52416395c9b404d72181a71d6ca
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-01-14 15:18:19 +0800

    chore: display usd value on input

--- apps/web/src/views/Idos/components/IfoContainer.tsx
+++ apps/web/src/views/Idos/components/IfoContainer.tsx
@@ -14,12 +14,14 @@ interface TypeProps {
 
 const IfoContainer: React.FC<React.PropsWithChildren<TypeProps>> = ({ ifoSection, ifoSteps, faq }) => {
   return (
-    <IfoLayout id="current-ifo">
-      <Container px="0">
+    <IfoLayout id="current-ido">
+      {/* <Container px="0">
         <IfoLayoutWrapper>{ifoSection}</IfoLayoutWrapper>
-      </Container>
+      </Container> */}
       <SectionBackground>
-        <Container>{ifoSteps}</Container>
+        <Container px="0">
+          <IfoLayoutWrapper>{ifoSection}</IfoLayoutWrapper>
+        </Container>
       </SectionBackground>
       {/* {faq}
       <LinkExternal
