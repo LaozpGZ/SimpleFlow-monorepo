4de4ae4af2bd88311d8228c54a73e7c6630ed354
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-01-17 10:06:58 +0800

    chore: info card details

--- apps/web/src/views/Idos/hooks/ido/useIdoPublicData.ts
+++ apps/web/src/views/Idos/hooks/ido/useIdoPublicData.ts
@@ -51,7 +51,7 @@ export const useIdoPublicData = (chainId: ChainId): IDOPublicData => {
   } = useIDOUserStatus()
 
   const startTime = Number(poolInfo?.startTimestamp) || 0
-  const endTime = 1737407928 // Number(poolInfo?.endTimestamp) || 0
+  const endTime = Number(poolInfo?.endTimestamp) || 0 // 1737407928
   const now = Math.floor(Date.now() / 1000)
   const status = getStatusByTimestamp(now, startTime, endTime)
   const lpTokenPriceInUsd = useLpTokenPrice(stakeCurrency?.symbol ?? 'BNB')
@@ -64,7 +64,7 @@ export const useIdoPublicData = (chainId: ChainId): IDOPublicData => {
     isInitialized: true,
     startTime,
     endTime,
-    status: 'finished',
+    status,
     currencyPriceInUSD,
     poolInfo,
     plannedStartTime: poolInfo?.startTimestamp ? poolInfo?.startTimestamp - 432000 : 0, // five days before
