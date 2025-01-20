48bf8a1c2dad74ceb3423a861618be45af64fd3d
Chef Jerry <jerry@pancakeswap.com>
2025-01-20 18:55:35 +0800

    feat: add test address

--- apps/web/src/config/constants/contracts.ts
+++ apps/web/src/config/constants/contracts.ts
@@ -261,7 +261,7 @@ export default {
     [ChainId.ZKSYNC]: '0xB774c13bA5a665713037c42A12f0ED9De70585cB',
   },
   ido: {
-    [ChainId.BSC]: '0xD25fbd3F2fbD261798b987C84A409Ba687E39afb',
+    [ChainId.BSC]: getTestIdoAddress() ?? '0xD25fbd3F2fbD261798b987C84A409Ba687E39afb',
     [ChainId.BSC_TESTNET]: getTestIdoAddress() ?? '0x51bc9C57A9401d7826ADd434e4D20bf3cf4880Ac',
   },
 } as const satisfies Record<string, Record<number, `0x${string}`>>
