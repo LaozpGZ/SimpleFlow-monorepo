d2a4431ea612ab0a4ad369122dba850108266380
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-01-20 15:44:31 +0800

    chore: remove popup for ido

--- apps/web/src/views/Idos/index.tsx
+++ apps/web/src/views/Idos/index.tsx
@@ -1,8 +1,3 @@
-import { useTranslation } from '@pancakeswap/localization'
-import { useModal } from '@pancakeswap/uikit'
-import USCitizenConfirmModal from 'components/Modal/USCitizenConfirmModal'
-import { IdType, useUserNotUsCitizenAcknowledgement } from 'hooks/useUserIsUsCitizenAcknowledgement'
-import { useEffect } from 'react'
 import { styled } from 'styled-components'
 import Hero from './components/Hero'
 import IfoProvider from './contexts/IfoContext'
@@ -13,35 +8,6 @@ export const Wrapper = styled.div`
 `
 
 export const IdoPageLayout = ({ children }) => {
-  const { t } = useTranslation()
-
-  const [userNotUsCitizenAcknowledgement] = useUserNotUsCitizenAcknowledgement(IdType.IFO)
-  const [onUSCitizenModalPresent] = useModal(
-    <USCitizenConfirmModal
-      title={t('PancakeSwap IDOs')}
-      id={IdType.IFO}
-      checks={[
-        {
-          key: 'checkbox',
-          content: t('I confirm that I am eligible to participate in IDOs on this platform.'),
-        },
-      ]}
-    />,
-    false,
-    false,
-    'usCitizenConfirmModalIDO',
-  )
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (!userNotUsCitizenAcknowledgement) {
-        onUSCitizenModalPresent()
-      }
-    }, 2000)
-
-    return () => clearTimeout(timer)
-  }, [userNotUsCitizenAcknowledgement, onUSCitizenModalPresent])
-
   return (
     <IfoProvider>
       <Wrapper>
