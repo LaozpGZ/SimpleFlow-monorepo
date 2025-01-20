7b4d058b1db84552043b462c9bcdbaa623931127
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-01-20 14:16:09 +0800

    chore: update the token display

--- apps/web/src/views/Idos/components/Footer.tsx
+++ apps/web/src/views/Idos/components/Footer.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from '@pancakeswap/localization'
 import { BscScanIcon, CardBody, FlexGap, LanguageIcon, Text } from '@pancakeswap/uikit'
 import useTheme from 'hooks/useTheme'
 
-export const Footer = () => {
+export const Footer: React.FC<{ tokenSymbol?: string }> = ({ tokenSymbol }) => {
   const { theme } = useTheme()
   const { t } = useTranslation()
   return (
@@ -13,8 +13,9 @@ export const Footer = () => {
           <BscScanIcon width="24px" color={theme.colors.textSubtle} />
         </FlexGap>
         <Text color="textSubtle" fontSize="14px" lineHeight="16.8px">
+          {tokenSymbol}{' '}
           {t(
-            'TOKEN is a new DeFi platform that lorem ispum here is a brief introduction to the IDO token projects and above shows links to project websites',
+            'is a new DeFi platform that lorem ispum here is a brief introduction to the IDO token projects and above shows links to project websites',
           )}
         </Text>
       </FlexGap>
