6f1f52a5b9925902d4768628c51965fc1196e333
chef-ryan <ryan@pancakeswap.com>
2025-01-21 18:52:24 +0800

    fix: 🐛 Text

--- apps/web/src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/IfoCardDetails.tsx
+++ apps/web/src/views/Ifos/components/IfoFoldableCard/IfoPoolCard/IfoCardDetails.tsx
@@ -282,7 +282,7 @@ const IfoCardDetails: React.FC<React.PropsWithChildren<IfoCardDetailsProps>> = (
                 label={t('Vested percentage:')}
                 value={`${poolCharacteristic.vestingInformation.percentage}%`}
                 tooltipContent={t(
-                  '%percentageVested%% of the purchased token will be released at the end of the vesting schedule. 0% of the purchased token will be released immediately and available for claiming when IFO ends',
+                  '%percentageVested%% of the purchased token will be released at the end of the vesting schedule. 0% of the purchased token will be released immediately and available for claiming when IFO ends.',
                   {
                     percentageVested: poolCharacteristic.vestingInformation.percentage,
                     percentageTgeRelease: BIG_ONE_HUNDRED.minus(
