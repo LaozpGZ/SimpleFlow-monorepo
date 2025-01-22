9591a1dbea398e112da79775f36e1a46c0243c09
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-01-22 14:23:03 +0800

    fix: remove layout can't connect issue (#11156)

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -45,7 +45,7 @@ import { useWalletConnectRouterSync } from 'hooks/useWalletConnectRouterSync'
 import { Blocklist, Updaters } from '..'
 import { SEO } from '../../next-seo.config'
 import Providers from '../Providers'
-import Menu from '../components/Menu'
+import Menu, { SharedComponentWithOutMenu } from '../components/Menu'
 import GlobalStyle from '../style/Global'
 
 const EasterEgg = dynamic(() => import('components/EasterEgg'), { ssr: false })
@@ -102,15 +102,6 @@ function MyApp(props: AppProps<{ initialReduxState: any; dehydratedState: any }>
           content="Cheaper and faster than Uniswap? Discover PancakeSwap, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for CAKE."
         />
         <meta name="theme-color" content="#1FC7D4" />
-        {(Component as NextPageWithLayout).mp && (
-          // eslint-disable-next-line @next/next/no-sync-scripts
-          <script
-            src="https://public.bnbstatic.com/static/js/mp-webview-sdk/webview-v1.0.0.min.js"
-            integrity="sha384-PV6Pqh2oiQNNl9jwtcTIue3fwDnP5k80+DaPY8/AS4qxGA91MsE3G91BQ2jQ81oT"
-            crossOrigin="anonymous"
-            id="mp-webview"
-          />
-        )}
       </Head>
       <DefaultSeo {...SEO} />
       <LoadVConsole />
@@ -120,7 +111,7 @@ function MyApp(props: AppProps<{ initialReduxState: any; dehydratedState: any }>
           // @ts-ignore
           <Component.Meta {...pageProps} />
         )}
-        {(Component as NextPageWithLayout).mp ? <MPGlobalHooks /> : <GlobalHooks />}
+        <GlobalHooks />
         <ResetCSS />
         <GlobalStyle />
         <GlobalCheckClaimStatus excludeLocations={[]} />
@@ -179,7 +170,7 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
 
   // Use the layout defined at the page level, if available
   const Layout = Component.Layout || Fragment
-  const ShowMenu = Component.mp ? Fragment : Menu
+  const ShowMenu = Component.mp ? SharedComponentWithOutMenu : Menu
   const isShowScrollToTopButton = Component.isShowScrollToTopButton || true
   const shouldScreenWallet = Component.screen || false
   const isShowV4IconButton = Component.isShowV4IconButton || false
--- apps/web/src/pages/ido/index.tsx
+++ apps/web/src/pages/ido/index.tsx
@@ -12,6 +12,6 @@ CurrentIdoPage.Layout = IdoPageLayout
 
 CurrentIdoPage.chains = IDO_SUPPORT_CHAINS
 
-CurrentIdoPage.mp = false
+CurrentIdoPage.mp = true
 
 export default CurrentIdoPage
