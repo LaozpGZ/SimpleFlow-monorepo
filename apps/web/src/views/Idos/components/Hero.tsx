716f7199fe48aef0f3598c722816176a26b7ce3d
Chef Jerry <jerry@pancakeswap.com>
2025-02-06 20:48:03 +0800

    feat: add logo

--- apps/web/src/views/Idos/components/Hero.tsx
+++ apps/web/src/views/Idos/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { Box, Container, Flex, Heading, Text } from '@pancakeswap/uikit'
+import { Box, Container, Flex, FlexGap, Heading, LogoIcon, Text } from '@pancakeswap/uikit'
 import { useRouter } from 'next/router'
 import { styled } from 'styled-components'
 
@@ -42,7 +42,10 @@ const Hero = () => {
           >
             <Box>
               <StyledHeading as="h1" mb={['12px', '12px', '12px', '12px']}>
-                {t('Exclusive TGE')}
+                <FlexGap alignItems="center" gap="8px">
+                  <LogoIcon width="32px" height="32px" />
+                  {t('Exclusive TGE')}
+                </FlexGap>
               </StyledHeading>
               <p>
                 <StyledSubTitle bold>{t(`MyShell's Token Generation Event`)}</StyledSubTitle>
