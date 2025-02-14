4cd2b0a704a3d94ccbf5cd1415a96163edaa6d6e
chef-ryan <ryan@pancakeswap.com>
2025-02-14 16:22:18 +0800

    unused

--- apps/web/src/components/AdPanel/InfoStripes/TstPerpInfoStripe.tsx
+++
@@ -1,8 +0,0 @@
-import { useTstPerpConfig } from '../hooks/useTstPerpConfig'
-import { InfoStripeCommon } from './InfoStripeCommon'
-
-export const TstPerpInfoStripe = () => {
-  const config = useTstPerpConfig()
-
-  return <InfoStripeCommon config={config.infoStripe} />
-}
