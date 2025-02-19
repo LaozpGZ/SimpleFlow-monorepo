b43cac13daf2274b8e4dc10f2ceb76b2a1c83f9e
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-02-19 15:29:07 +0800

    fix: error

--- packages/tokens/src/constants/monadTestnet.ts
+++ packages/tokens/src/constants/monadTestnet.ts
@@ -1,8 +1,9 @@
 import { ChainId } from '@pancakeswap/chains'
-import { ERC20Token, WETH9 } from '@pancakeswap/sdk'
+import { WETH9 } from '@pancakeswap/sdk'
+import { BUSD, USDC } from './common'
 
 export const monadTestnetTokens = {
-  busd: new ERC20Token(ChainId.MONAD_TESTNET, '0xcf27F781841484d5CF7e155b44954D7224caF1dD', 18, 'BUSD', 'Binance USD'),
-  usdc: new ERC20Token(ChainId.MONAD_TESTNET, '0x673cD70FA883394a1f3DEb3221937Ceb7C2618D7', 18, 'USDC', 'USD Coin'),
   weth: WETH9[ChainId.MONAD_TESTNET],
+  busd: BUSD[ChainId.MONAD_TESTNET],
+  usdc: USDC[ChainId.MONAD_TESTNET],
 }
