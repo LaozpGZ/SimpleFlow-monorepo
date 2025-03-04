7014cde9dedf042b337c8d3c800e3dab45a405b8
chef-ryan <ryan@pancakeswap.com>
2025-03-04 16:27:08 +0800

    add suspense

--- apps/web/src/pages/info/[chainName]/tokens/[address].tsx
+++ apps/web/src/pages/info/[chainName]/tokens/[address].tsx
@@ -1,14 +1,26 @@
-import Token from 'views/Info/Tokens/TokenPage'
+import { Flex, Spinner } from '@pancakeswap/uikit'
 import { GetStaticPaths, GetStaticProps } from 'next'
-import { InfoPageLayout } from 'views/Info'
+import { Suspense } from 'react'
 import { getTokenStaticPaths, getTokenStaticProps } from 'utils/pageUtils'
+import { InfoPageLayout } from 'views/Info'
+import Token from 'views/Info/Tokens/TokenPage'
 
 const TokenPage = ({ address }: { address: string }) => {
   if (!address) {
     return null
   }
 
-  return <Token routeAddress={address.toLowerCase()} />
+  return (
+    <Suspense
+      fallback={
+        <Flex mt="80px" justifyContent="center">
+          <Spinner />
+        </Flex>
+      }
+    >
+      <Token routeAddress={address.toLowerCase()} />
+    </Suspense>
+  )
 }
 
 TokenPage.Layout = InfoPageLayout
