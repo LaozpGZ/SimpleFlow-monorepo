f0f3b8ca768993e41537a9010296c91ccff84e11
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-03-06 11:22:16 +0800

    chore: update mev swap display

--- apps/web/src/views/Mev/MevSwapDetail.tsx
+++ apps/web/src/views/Mev/MevSwapDetail.tsx
@@ -1,37 +1,43 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { Box, QuestionHelperV2, SkeletonV2, Text } from '@pancakeswap/uikit'
+import { QuestionHelperV2, ShieldCheckIcon, Text } from '@pancakeswap/uikit'
 import { RowBetween, RowFixed } from 'components/Layout/Row'
+import { styled } from 'styled-components'
 import { useIsMEVEnabled } from './hooks'
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => (theme.isDark ? theme.colors.backgroundAlt : '#F7F7F7')};
+  border-radius: 16px;
+  border: 1px solid ${({ theme }) => theme.colors.cardBorder};
+  padding: 12px 16px;
+  margin-top: 8px;
+`
 
 export const MevSwapDetail: React.FC = () => {
   const { t } = useTranslation()
   const { isMEVEnabled, isLoading } = useIsMEVEnabled()
-  if (!isMEVEnabled) {
+  if (!isMEVEnabled || isLoading) {
     return null
   }
   return (
-    <Box mt="10px" p="0px 4px">
+    <Wrapper>
       <RowBetween>
         <RowFixed>
           <QuestionHelperV2
             text={t('PancakeSwap MEV Guard protects you from frontrunning and sandwich attacks when Swapping.')}
             placement="top-start"
           >
+            <ShieldCheckIcon width="15px" color="success" />
             <Text
               fontSize="14px"
               color="textSubtle"
               style={{ textDecoration: 'underline', textDecorationStyle: 'dotted' }}
+              ml="7px"
             >
-              {t('MEV Protect')}
+              {t('MEV Protected')}
             </Text>
           </QuestionHelperV2>
         </RowFixed>
-        <RowFixed>
-          <SkeletonV2 width="80px" height="16px" borderRadius="8px" minHeight="auto" isDataReady={!isLoading}>
-            <Text fontSize="14px">{isMEVEnabled ? `${t('Enabled')}` : `${t('Disabled')}`}</Text>
-          </SkeletonV2>
-        </RowFixed>
       </RowBetween>
-    </Box>
+    </Wrapper>
   )
 }
