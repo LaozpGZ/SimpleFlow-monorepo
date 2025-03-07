137d69fe4ca18489ca39628a4363829b285d65f4
chef-ryan <ryan@pancakeswap.com>
2025-03-07 15:32:04 +0800

    add new homepage

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -213,12 +213,7 @@ const config = {
         source: '/images/tokens/:address',
         destination: 'https://tokens.pancakeswap.finance/images/:address',
         permanent: false,
-      },
-      {
-        source: '/swap',
-        destination: '/',
-        permanent: true,
-      },
+      }
     ]
   },
   webpack: (webpackConfig, { webpack, isServer }) => {
