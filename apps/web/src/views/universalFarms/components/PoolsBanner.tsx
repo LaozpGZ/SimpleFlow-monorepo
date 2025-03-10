d84aaab07bf5dc738611d59b52b3d0530b211bc2
chef-ryan <ryan@pancakeswap.com>
2025-03-11 00:32:07 +0800

    fix

--- apps/web/src/views/universalFarms/components/PoolsBanner.tsx
+++ apps/web/src/views/universalFarms/components/PoolsBanner.tsx
@@ -2,6 +2,8 @@ import { useTheme } from '@pancakeswap/hooks'
 import { useTranslation } from '@pancakeswap/localization'
 import { Box, Button, Column, LinkExternal, PageHeader, Row, Text } from '@pancakeswap/uikit'
 import { VerticalDivider } from '@pancakeswap/widgets-internal'
+import { PickAdSlides } from 'components/AdPanel/PickAdSlides'
+import { Suspense } from 'react'
 import { FarmFlexWrapper, FarmH1, FarmH2 } from 'views/Farms/styled'
 
 export const PoolsBanner = ({ additionLink }: { additionLink?: React.ReactNode }) => {
@@ -37,6 +39,11 @@ export const PoolsBanner = ({ additionLink }: { additionLink?: React.ReactNode }
                 </>
               )}
             </Row>
+          </Box>
+          <Box>
+            <Suspense>
+              <PickAdSlides isDismissible={false} />
+            </Suspense>
           </Box>
         </FarmFlexWrapper>
       </Column>
