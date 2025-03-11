28eefbd4797a17b50f1244c7c2be488c69c271d0
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-03-11 17:01:39 +0800

    chore: remove safePal and other wallet

--- apps/web/src/views/Mev/constant.ts
+++ apps/web/src/views/Mev/constant.ts
@@ -41,11 +41,6 @@ export const rpcData = {
 
 export const walletConfig = [
   {
-    title: 'SafePal',
-    image: 'safepal.png',
-    doc: 'https://safepalsupport.zendesk.com/hc/en-us/articles/14688426876443-How-to-add-a-Custom-network-in-the-SafePal-software-wallet',
-  },
-  {
     title: 'Rabby Wallet',
     image: 'rabby.png',
     doc: 'https://support.rabby.io/hc/en-us/articles/11319672399247-List-of-supported-chains-and-tokens#:~:text=For%20testnets%20and%20networks%20that%20are%20not%20yet%20integrated%2C%20you%20can%20still%20access%20them%20by%20adding%20a%20custom%20network%20through%3A%20More%20%3E%20Add%20Custom%20Network.',
@@ -54,12 +49,6 @@ export const walletConfig = [
     title: 'TokenPocket',
     image: 'token-pocket.png',
     doc: 'https://help.tokenpocket.pro/en/wallet-operation/custom-network-token/how-to-add-custom-network',
-  },
-
-  {
-    title: 'Others',
-    image: 'others.png',
-    doc: 'https://support.metamask.io/networks-and-sidechains/managing-networks/how-to-add-a-custom-network-rpc/',
   },
 ]
 
