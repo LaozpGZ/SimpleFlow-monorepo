e1bcda773698c2f2ed5ad2f9ab060772020a7d8f
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-03-17 12:25:19 +0800

    chore: ui details

--- apps/web/src/views/universalFarms/components/PoolsBanner.tsx
+++ apps/web/src/views/universalFarms/components/PoolsBanner.tsx
@@ -1,49 +1,101 @@
 import { useTheme } from '@pancakeswap/hooks'
 import { useTranslation } from '@pancakeswap/localization'
-import { Box, Button, Column, LinkExternal, PageHeader, Row, Text } from '@pancakeswap/uikit'
+import {
+  Box,
+  Button,
+  Column,
+  FlexGap,
+  HelpIcon,
+  LinkExternal,
+  PageHeader,
+  Row,
+  Text,
+  useMatchBreakpoints,
+} from '@pancakeswap/uikit'
 import { VerticalDivider } from '@pancakeswap/widgets-internal'
 import { PickAdSlides } from 'components/AdPanel/PickAdSlides'
 import { Suspense } from 'react'
+import { useUserPancakePicks } from 'state/user/hooks/useUserPancakePicks'
 import { FarmFlexWrapper, FarmH1, FarmH2 } from 'views/Farms/styled'
 
 export const PoolsBanner = ({ additionLink }: { additionLink?: React.ReactNode }) => {
   const { t } = useTranslation()
   const { theme } = useTheme()
+  const { isMobile } = useMatchBreakpoints()
+  const [isPancakePicks, setIsPancakePicks] = useUserPancakePicks(isMobile)
 
   return (
     <PageHeader>
       <Column>
         <FarmFlexWrapper>
           <Box style={{ flex: '1 1 100%' }}>
-            <FarmH1 as="h1" scale="xxl" color="secondary" mb="24px">
-              {t('Earn from LP')}
-            </FarmH1>
-            <FarmH2 scale="lg" color="text">
-              {t('Liquidity Pools & Farms')}
-            </FarmH2>
-            <Row flexWrap="wrap" gap="16px">
-              <LinkExternal
-                href="https://docs.pancakeswap.finance/products/yield-farming/how-to-use-farms"
-                showExternalIcon={false}
-              >
-                <Button p="0" variant="text">
-                  <Text color="primary" bold fontSize="16px" mr="4px">
-                    {t('Learn How')}
-                  </Text>
+            {!isMobile ? (
+              <FarmH1 as="h1" scale="xxl" color="secondary" mb="24px">
+                {t('Earn from LP')}
+              </FarmH1>
+            ) : (
+              <FlexGap gap="16px">
+                <Text fontSize="20px">{t('Earn from Farm / Liquidity')}</Text>
+                <Button
+                  width="40px"
+                  height="40px"
+                  variant="secondary"
+                  px="14px"
+                  scale="md"
+                  onClick={() => setIsPancakePicks((prev) => !prev)}
+                  style={{
+                    borderColor: theme.colors.cardBorder,
+                    backgroundColor: theme.colors.card,
+                    borderWidth: '1px',
+                  }}
+                >
+                  🔥
                 </Button>
-              </LinkExternal>
-              {!!additionLink && (
-                <>
-                  <VerticalDivider bg={theme.colors.inputSecondary} />
-                  {additionLink}
-                </>
-              )}
-            </Row>
+                <Button
+                  width="40px"
+                  height="40px"
+                  variant="subtle"
+                  px="16px"
+                  scale="md"
+                  onClick={() => setIsPancakePicks((prev) => !prev)}
+                >
+                  <HelpIcon />
+                </Button>
+              </FlexGap>
+            )}
+
+            {!isMobile && (
+              <FarmH2 scale="lg" color="text">
+                {t('Liquidity Pools & Farms')}
+              </FarmH2>
+            )}
+            {!isMobile && (
+              <Row flexWrap="wrap" gap="16px">
+                <LinkExternal
+                  href="https://docs.pancakeswap.finance/products/yield-farming/how-to-use-farms"
+                  showExternalIcon={false}
+                >
+                  <Button p="0" variant="text">
+                    <Text color="primary" bold fontSize="16px" mr="4px">
+                      {t('Learn How')}
+                    </Text>
+                  </Button>
+                </LinkExternal>
+                {!!additionLink && (
+                  <>
+                    <VerticalDivider bg={theme.colors.inputSecondary} />
+                    {additionLink}
+                  </>
+                )}
+              </Row>
+            )}
           </Box>
           <Box>
-            <Suspense>
-              <PickAdSlides isDismissible={false} />
-            </Suspense>
+            {(isPancakePicks || !isMobile) && (
+              <Suspense>
+                <PickAdSlides isDismissible={false} />
+              </Suspense>
+            )}
           </Box>
         </FarmFlexWrapper>
       </Column>
