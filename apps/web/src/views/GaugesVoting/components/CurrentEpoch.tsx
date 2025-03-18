c8af50c48ac4a4169d07a407e8cbcdd84ca6054b
Chef Philip <chef.philip@pancakeswap.com>
2025-03-17 22:26:42 -0400

    chore: Update mobile view

--- apps/web/src/views/GaugesVoting/components/CurrentEpoch.tsx
+++ apps/web/src/views/GaugesVoting/components/CurrentEpoch.tsx
@@ -1,5 +1,15 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { AutoRow, Balance, Box, ErrorIcon, FlexGap, Text, TooltipText, useMatchBreakpoints } from '@pancakeswap/uikit'
+import {
+  AutoRow,
+  Balance,
+  Box,
+  ErrorIcon,
+  Flex,
+  FlexGap,
+  Text,
+  TooltipText,
+  useMatchBreakpoints,
+} from '@pancakeswap/uikit'
 import {
   formatNumber,
   getBalanceNumber,
@@ -25,7 +35,7 @@ export const CurrentEpoch = () => {
   const nextEpochStart = useNextEpochStart()
   const currentTimestamp = useCurrentBlockTimestamp()
   const onTally = useEpochOnTally()
-  const { isDesktop } = useMatchBreakpoints()
+  const { isDesktop, isMobile } = useMatchBreakpoints()
 
   return (
     <Box padding={['16px', '16px', '16px 24px 24px']}>
@@ -39,14 +49,26 @@ export const CurrentEpoch = () => {
             justifyContent="space-between"
             width="100%"
           >
-            <Text bold fontSize={20}>
-              {t('Current EPOCH')}
-            </Text>
+            <Flex alignItems="center">
+              <Text bold fontSize={20} mr="8px">
+                {t('Current EPOCH')}
+              </Text>
+              {isMobile ? (
+                <Tooltips
+                  content={t(
+                    'Results are updated weekly. Vote numbers are estimations based on the veCAKE balance at 00:00 UTC on the upcoming Thursday.',
+                  )}
+                >
+                  <ErrorIcon color="#7A6EAA" width="24px" mr="8px" />
+                </Tooltips>
+              ) : null}
+            </Flex>
             <FlexGap
               justifyContent={['space-between', 'space-between', 'flex-end']}
               alignItems="baseline"
               gap="4px"
               width={isDesktop ? 'auto' : '100%'}
+              mb={['8px', '8px', '0px']}
             >
               <Tooltips
                 content={t('Results for the current epoch will be snapshotted and tallied at 00:00 UTC on %date%.', {
@@ -54,7 +76,7 @@ export const CurrentEpoch = () => {
                 })}
               >
                 <TooltipText fontSize={14} color="textSubtle">
-                  {t('snapshots in')}
+                  {t('Snapshots in')}
                 </TooltipText>
               </Tooltips>
               <FlexGap gap="2px" alignItems="baseline">
@@ -119,14 +141,16 @@ export const CurrentEpoch = () => {
           </Tooltips>
           <Balance bold fontSize={16} value={getBalanceNumber(new BN(totalWeight.toString()))} unit=" veCAKE" />
         </AutoRow>
-        <AutoRow alignItems="center" flexDirection="row" justifyContent="flex-start" flexWrap="nowrap" mt="16px">
-          <ErrorIcon color="#7A6EAA" width="24px" mr="8px" />
-          <Text color="textSubtle" fontSize={12}>
-            {t(
-              'Results are updated weekly. Vote numbers are estimations based on the veCAKE balance at 00:00 UTC on the upcoming Thursday.',
-            )}
-          </Text>
-        </AutoRow>
+        {isMobile ? null : (
+          <AutoRow alignItems="center" flexDirection="row" justifyContent="flex-start" flexWrap="nowrap" mt="16px">
+            <ErrorIcon color="#7A6EAA" width="24px" mr="8px" />
+            <Text color="textSubtle" fontSize={12}>
+              {t(
+                'Results are updated weekly. Vote numbers are estimations based on the veCAKE balance at 00:00 UTC on the upcoming Thursday.',
+              )}
+            </Text>
+          </AutoRow>
+        )}
       </FlexGap>
     </Box>
   )
