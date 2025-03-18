ea8226382545ffc2ae85c008a8a900cd5a06fcd3
Chef Jerry <jerry@pancakeswap.com>
2025-03-18 14:27:31 +0800

    feat: count down

--- apps/web/src/views/Idos/components/IdoCards/Timer.tsx
+++ apps/web/src/views/Idos/components/IdoCards/Timer.tsx
@@ -2,7 +2,6 @@ import { useCountdown } from '@pancakeswap/hooks'
 import { IfoStatus } from '@pancakeswap/ifos'
 import { useTranslation } from '@pancakeswap/localization'
 import { Flex, Skeleton, Text } from '@pancakeswap/uikit'
-import getTimePeriods from '@pancakeswap/utils/getTimePeriods'
 import useTheme from 'hooks/useTheme'
 import { styled } from 'styled-components'
 
@@ -20,11 +19,45 @@ const FlexGap = styled(Flex)<{ gap: string }>`
 
 const USE_BLOCK_TIMESTAMP_UNTIL = 3
 
+const CountDown: React.FC<{
+  time: number
+  textColor?: string
+}> = ({ time, textColor }) => {
+  const { t } = useTranslation()
+  const { theme } = useTheme()
+  const color = textColor ?? theme.colors.secondary
+  const { days, hours, minutes, seconds } = useCountdown(time) ?? { days: 0, hours: 0, minutes: 0, seconds: 0 }
+
+  return (
+    <FlexGap gap="4px" alignItems="baseline">
+      {days ? (
+        <Text fontSize="20px" bold color={color}>
+          {days}
+          {t('d')} :
+        </Text>
+      ) : null}
+      {hours ? (
+        <Text fontSize="20px" bold color={color}>
+          {hours}
+          {t('h')} :
+        </Text>
+      ) : null}
+      <Text fontSize="20px" bold color={color}>
+        {minutes ?? '0'}
+        {t('m')} :
+      </Text>
+      <Text fontSize="20px" bold color={color}>
+        {seconds}
+        {t('s')}
+      </Text>
+    </FlexGap>
+  )
+}
+
 export const SoonTimer: React.FC<React.PropsWithChildren<Props>> = ({ startTime, ifoStatus, plannedStartTime }) => {
   const { theme } = useTheme()
-  const { t } = useTranslation()
-  const { days, hours, minutes, seconds } = useCountdown(startTime) ?? { days: 0, hours: 0, minutes: 0, seconds: 0 }
   const textColor = theme.colors.secondary
+  const { t } = useTranslation()
 
   const countdownDisplay =
     ifoStatus !== 'idle' ? (
@@ -33,28 +66,7 @@ export const SoonTimer: React.FC<React.PropsWithChildren<Props>> = ({ startTime,
           <Text fontSize="16px" color={textColor}>
             {t('Starts in')}:
           </Text>
-          <FlexGap gap="4px" alignItems="baseline">
-            {days ? (
-              <Text fontSize="20px" bold color={textColor}>
-                {days}
-                {t('d')} :
-              </Text>
-            ) : null}
-            {hours ? (
-              <Text fontSize="20px" bold color={textColor}>
-                {hours}
-                {t('h')} :
-              </Text>
-            ) : null}
-            <Text fontSize="20px" bold color={textColor}>
-              {minutes ?? '0'}
-              {t('m')}
-            </Text>
-            <Text fontSize="20px" bold color={textColor}>
-              {seconds}
-              {t('s')}
-            </Text>
-          </FlexGap>
+          <CountDown time={startTime} />
         </FlexGap>
       </>
     ) : null
@@ -70,8 +82,6 @@ export const SoonTimer: React.FC<React.PropsWithChildren<Props>> = ({ startTime,
 
 const LiveTimer: React.FC<React.PropsWithChildren<Pick<Props, 'endTime' | 'ifoStatus'>>> = ({ endTime, ifoStatus }) => {
   const { t } = useTranslation()
-  const now = Math.floor(Date.now() / 1000)
-  const timeUntil = getTimePeriods(endTime - now)
 
   const timeDisplay =
     ifoStatus !== 'idle' ? (
@@ -79,24 +89,7 @@ const LiveTimer: React.FC<React.PropsWithChildren<Pick<Props, 'endTime' | 'ifoSt
         <FlexGap gap="8px" alignItems="center">
           <Text textTransform="uppercase" fontSize="16px" bold color="#FBCB01">{`${t('Live Now')}!`}</Text>
           <Text color="white">{t('Ends in')}:</Text>
-          <FlexGap gap="4px" alignItems="baseline">
-            {timeUntil.days ? (
-              <Text fontSize="20px" bold color="white">
-                {timeUntil.days}
-                {t('d')} :
-              </Text>
-            ) : null}
-            {timeUntil.days || timeUntil.hours ? (
-              <Text fontSize="20px" bold color="white">
-                {timeUntil.hours}
-                {t('h')} :
-              </Text>
-            ) : null}
-            <Text fontSize="20px" bold color="white">
-              {!timeUntil.days && !timeUntil.hours && timeUntil.minutes === 0 ? '< 1' : timeUntil.minutes}
-              {t('m')}
-            </Text>
-          </FlexGap>
+          <CountDown time={endTime} textColor="white" />
         </FlexGap>
       </>
     ) : null
