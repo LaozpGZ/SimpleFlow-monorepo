304d8b22bca61e9c54609a436b56132b961aa005
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-03-18 20:59:06 +0800

    chore: color details

--- apps/web/src/views/PoolDetail/components/MyPositions.tsx
+++ apps/web/src/views/PoolDetail/components/MyPositions.tsx
@@ -168,7 +168,7 @@ const MyPositionsInner: React.FC<{ poolInfo: PoolInfo }> = ({ poolInfo }) => {
         <Card>
           <CardBody>
             <FlexGap alignItems="center" justifyContent="center" flexDirection="column" gap="24px">
-              <Text>
+              <Text color="textSubtle">
                 {!account
                   ? t('Please connect wallet to view your position / add liquidity.')
                   : t('You currently have no position in this liquidity pair.')}
