637efa609e190f45baf71c1f57053ed4edddb5ae
Chef Philip <chef.philip@pancakeswap.com>
2025-03-17 21:55:11 -0400

    chore: update UI

--- apps/web/src/views/GaugesVoting/components/CurrentEpoch.tsx
+++ apps/web/src/views/GaugesVoting/components/CurrentEpoch.tsx
@@ -1,5 +1,15 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { AutoRow, Balance, Box, ErrorIcon, FlexGap, Text, TooltipText, useMatchBreakpoints } from '@pancakeswap/uikit'
+import {
+  AutoRow,
+  Balance,
+  Box,
+  ErrorIcon,
+  Flex,
+  FlexGap,
+  Text,
+  TooltipText,
+  useMatchBreakpoints,
+} from '@pancakeswap/uikit'
 import {
   formatNumber,
   getBalanceNumber,
@@ -8,9 +18,9 @@ import {
 } from '@pancakeswap/utils/formatBalance'
 import BN from 'bignumber.js'
 import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import { Tooltips } from 'views/CakeStaking/components/Tooltips'
 import { useCurrentBlockTimestamp } from 'views/CakeStaking/hooks/useCurrentBlockTimestamp'
-import relativeTime from 'dayjs/plugin/relativeTime'
 import { useCurrentEpochEnd, useEpochOnTally, useNextEpochStart } from '../hooks/useEpochTime'
 import { useGaugesTotalWeight } from '../hooks/useGaugesTotalWeight'
 
@@ -38,10 +48,20 @@ export const CurrentEpoch = () => {
             alignItems="baseline"
             justifyContent="space-between"
             width="100%"
+            mb="4px"
           >
-            <Text bold fontSize={20}>
-              {t('Current EPOCH')}
-            </Text>
+            <Flex>
+              <Text color="secondary" textTransform="uppercase" bold mr="8px">
+                {t('Current EPOCH')}
+              </Text>
+              <Tooltips
+                content={t(
+                  'Results are updated weekly. Vote numbers are estimations based on the veCAKE balance at 00:00 UTC on the upcoming Thursday.',
+                )}
+              >
+                <ErrorIcon color="#7A6EAA" width="24px" mr="8px" />
+              </Tooltips>
+            </Flex>
             <FlexGap
               justifyContent={['space-between', 'space-between', 'flex-end']}
               alignItems="baseline"
@@ -54,7 +74,7 @@ export const CurrentEpoch = () => {
                 })}
               >
                 <TooltipText fontSize={14} color="textSubtle">
-                  {t('snapshots in')}
+                  {t('Snapshots in')}
                 </TooltipText>
               </Tooltips>
               <FlexGap gap="2px" alignItems="baseline">
@@ -118,14 +138,6 @@ export const CurrentEpoch = () => {
             </TooltipText>
           </Tooltips>
           <Balance bold fontSize={16} value={getBalanceNumber(new BN(totalWeight.toString()))} unit=" veCAKE" />
-        </AutoRow>
-        <AutoRow alignItems="center" flexDirection="row" justifyContent="flex-start" flexWrap="nowrap" mt="16px">
-          <ErrorIcon color="#7A6EAA" width="24px" mr="8px" />
-          <Text color="textSubtle" fontSize={12}>
-            {t(
-              'Results are updated weekly. Vote numbers are estimations based on the veCAKE balance at 00:00 UTC on the upcoming Thursday.',
-            )}
-          </Text>
         </AutoRow>
       </FlexGap>
     </Box>
--- apps/web/src/views/GaugesVoting/index.tsx
+++ apps/web/src/views/GaugesVoting/index.tsx
@@ -14,9 +14,8 @@ import {
   Text,
   useMatchBreakpoints,
 } from '@pancakeswap/uikit'
-import Page from 'components/Layout/Page'
 import NextLink from 'next/link'
-import { PropsWithChildren, useState } from 'react'
+import { useState } from 'react'
 import styled from 'styled-components'
 import { CurrentEpoch } from './components/CurrentEpoch'
 import { FilterFieldByType, FilterFieldInput, FilterFieldSort } from './components/GaugesFilter'
@@ -49,16 +48,6 @@ const StyledPageHeader = styled(PageHeader)`
   }
 `
 
-const StyledPage = styled(Page)`
-  padding: 0px;
-
-  background: ${({ theme }) => theme.colors.backgroundAlt};
-
-  ${({ theme }) => theme.mediaQueries.md} {
-    background: transparent;
-  }
-`
-
 const BunnyImage = styled.img`
   /* width: 218px; */
   width: 180px;
@@ -82,7 +71,11 @@ const GaugesVoting = () => {
 
   return (
     <StyledGaugesVotingPage>
-      {!isMobile && (
+      {isMobile ? (
+        <Text px="16px" py="24px" lineHeight="110%" bold color="secondary" fontSize={['32px', '32px', '64px', '64px']}>
+          {t('Gauges Voting')}
+        </Text>
+      ) : (
         <>
           <StyledPageHeader background="transparent">
             <Flex justifyContent="space-between">
@@ -125,120 +118,108 @@ const GaugesVoting = () => {
           </StyledPageHeader>
         </>
       )}
-      <StyledPage>
-        <Box
-          pl={['16px', '16px', '24px']}
-          pr={['16px', '16px', '24px']}
-          mt={['0px', '0px', '32px', '-18px']}
-          pb={['32px', '32px', '52px']}
-        >
-          <TabMenu activeIndex={activeTab} onItemClick={setActiveTab} fullWidth={isMobile} isShowBorderBottom={false}>
-            <Tab>
-              {t('Gauges')} ({filterGauges.length})
-            </Tab>
-            <Tab>{t('My veCAKE/Votes')}</Tab>
-          </TabMenu>
+      <Box px="40px">
+        <TabMenu activeIndex={activeTab} onItemClick={setActiveTab} fullWidth={isMobile} isShowBorderBottom={false}>
+          <Tab>
+            {t('Gauges')} ({filterGauges.length})
+          </Tab>
+          <Tab>{t('My veCAKE/Votes')}</Tab>
+        </TabMenu>
+      </Box>
 
-          {activeTab === 0 ? (
-            <ResponsiveCard>
-              <Grid gridTemplateColumns={isDesktop ? '2.2fr 3fr' : '1fr'}>
-                <EpochPreview />
-                <Box ml={isDesktop ? '60px' : '0'} mt={isDesktop ? '0' : '1em'}>
-                  <Text color="secondary" textTransform="uppercase" bold>
-                    {t('proposed weights')}
-                  </Text>
-                  <Box mt={isDesktop ? '40px' : '0'} mb={isDesktop ? '20px' : 0}>
-                    <WeightsPieChart
-                      data={filterGauges}
-                      totalGaugesWeight={Number(totalGaugesWeight)}
-                      isLoading={isLoading}
-                    />
-                  </Box>
+      <Box
+        pl={['16px', '16px', '24px']}
+        pr={['16px', '16px', '24px']}
+        mt={['0px', '0px', '32px', '-18px']}
+        pb={['32px', '32px', '52px']}
+      >
+        {activeTab === 0 ? (
+          <Card>
+            <Grid gridTemplateColumns={isDesktop ? '2.2fr 3fr' : '1fr'}>
+              <EpochPreview />
+              <Box ml={isDesktop ? '60px' : '0'} mt={isDesktop ? '0' : '1em'}>
+                <Text color="secondary" textTransform="uppercase" bold>
+                  {t('proposed weights')}
+                </Text>
+                <Box mt={isDesktop ? '40px' : '0'} mb={isDesktop ? '20px' : 0}>
+                  <WeightsPieChart
+                    data={filterGauges}
+                    totalGaugesWeight={Number(totalGaugesWeight)}
+                    isLoading={isLoading}
+                  />
                 </Box>
-                {!isMobile && !isXl ? (
-                  <Grid gridTemplateColumns="1fr 1fr" gridGap="32px">
-                    <FilterFieldByType onFilterChange={setFilter} value={filter} />
-                    <FilterFieldInput initialValue={searchText} placeholder={t('Search')} onChange={setSearchText} />
-                  </Grid>
-                ) : null}
-              </Grid>
-              {/* for tablet fit */}
-              {isXl ? (
-                <Grid gridTemplateColumns="1fr 1fr">
+              </Box>
+              {!isMobile && !isXl ? (
+                <Grid gridTemplateColumns="1fr 1fr" gridGap="32px">
                   <FilterFieldByType onFilterChange={setFilter} value={filter} />
                   <FilterFieldInput initialValue={searchText} placeholder={t('Search')} onChange={setSearchText} />
                 </Grid>
               ) : null}
-              {/* for mobile sticky, make it redundancy */}
-              {isMobile ? (
-                <Grid
-                  background="background"
-                  mx={-16}
-                  p={16}
-                  gridTemplateColumns="1fr"
-                  gridGap="1em"
-                  position="sticky"
-                  top="0"
-                >
-                  {isXs ? (
+            </Grid>
+            {/* for tablet fit */}
+            {isXl ? (
+              <Grid gridTemplateColumns="1fr 1fr">
+                <FilterFieldByType onFilterChange={setFilter} value={filter} />
+                <FilterFieldInput initialValue={searchText} placeholder={t('Search')} onChange={setSearchText} />
+              </Grid>
+            ) : null}
+            {/* for mobile sticky, make it redundancy */}
+            {isMobile ? (
+              <Grid
+                background="background"
+                mx={-16}
+                p={16}
+                gridTemplateColumns="1fr"
+                gridGap="1em"
+                position="sticky"
+                top="0"
+              >
+                {isXs ? (
+                  <FilterFieldByType onFilterChange={setFilter} value={filter} />
+                ) : (
+                  <Grid gridTemplateColumns="2fr 1fr" gridGap="8px">
                     <FilterFieldByType onFilterChange={setFilter} value={filter} />
-                  ) : (
-                    <Grid gridTemplateColumns="2fr 1fr" gridGap="8px">
-                      <FilterFieldByType onFilterChange={setFilter} value={filter} />
-                      <FilterFieldSort onChange={setSort} />
-                    </Grid>
-                  )}
-                  {isXs ? (
-                    <Grid gridTemplateColumns="2fr 1fr" gridGap="8px">
-                      <FilterFieldInput placeholder={t('Search')} initialValue={searchText} onChange={setSearchText} />
-                      <FilterFieldSort onChange={setSort} />
-                    </Grid>
-                  ) : (
+                    <FilterFieldSort onChange={setSort} />
+                  </Grid>
+                )}
+                {isXs ? (
+                  <Grid gridTemplateColumns="2fr 1fr" gridGap="8px">
                     <FilterFieldInput placeholder={t('Search')} initialValue={searchText} onChange={setSearchText} />
-                  )}
-                </Grid>
-              ) : null}
-              {isMobile ? (
-                <GaugesList
-                  key={sort}
-                  data={filterGauges}
-                  isLoading={isLoading}
-                  totalGaugesWeight={Number(totalGaugesWeight)}
-                />
-              ) : (
-                <GaugesTable
-                  mt="1.5em"
-                  data={filterGauges}
-                  isLoading={isLoading}
-                  totalGaugesWeight={Number(totalGaugesWeight)}
-                />
-              )}
-            </ResponsiveCard>
-          ) : (
-            <Card innerCardProps={{ padding: '2em 2em 0 2em' }}>
-              <VoteTable />
-            </Card>
-          )}
-        </Box>
-      </StyledPage>
+                    <FilterFieldSort onChange={setSort} />
+                  </Grid>
+                ) : (
+                  <FilterFieldInput placeholder={t('Search')} initialValue={searchText} onChange={setSearchText} />
+                )}
+              </Grid>
+            ) : null}
+            {isMobile ? (
+              <GaugesList
+                key={sort}
+                data={filterGauges}
+                isLoading={isLoading}
+                totalGaugesWeight={Number(totalGaugesWeight)}
+              />
+            ) : (
+              <GaugesTable
+                mt="1.5em"
+                data={filterGauges}
+                isLoading={isLoading}
+                totalGaugesWeight={Number(totalGaugesWeight)}
+              />
+            )}
+          </Card>
+        ) : (
+          <Card innerCardProps={{ padding: '2em 2em 0 2em' }}>
+            <VoteTable />
+          </Card>
+        )}
+      </Box>
     </StyledGaugesVotingPage>
   )
 }
 
 const EpochPreview = () => {
-  return (
-    <Card isActive style={{ height: 'fit-content' }}>
-      <CurrentEpoch />
-    </Card>
-  )
-}
-
-const ResponsiveCard: React.FC<PropsWithChildren> = ({ children }) => {
-  const { isDesktop } = useMatchBreakpoints()
-  if (isDesktop) {
-    return <Card innerCardProps={{ padding: '2em 2em 0 2em' }}>{children}</Card>
-  }
-  return <Box pt="2em">{children}</Box>
+  return <CurrentEpoch />
 }
 
 export default GaugesVoting
