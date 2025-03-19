c7921844df02e4444addf5cacca41270de0cb4d0
memoyil <2213635+memoyil@users.noreply.github.com>
2025-03-19 09:25:31 +0100

    fix: Homepage api calls fail handle

--- apps/web/src/views/HomeV2/atom/homePageDataAtom.ts
+++ apps/web/src/views/HomeV2/atom/homePageDataAtom.ts
@@ -1,8 +1,35 @@
 import { atom } from 'jotai'
 import { HomePageData } from 'pages/api/home/types'
+import retry from 'async-retry'
+
+const MAX_RETRY = 5
 
 export const homePageDataAtom = atom(async () => {
-  const resp = await fetch('/api/home')
-  const data = await resp.json()
-  return data as HomePageData
+  return retry(
+    async (_, retryAttempt) => {
+      if (retryAttempt === MAX_RETRY + 1) {
+        return {
+          tokens: [],
+          pools: [],
+          currencies: [],
+          chains: [],
+          cakeRelated: undefined,
+          stats: undefined,
+          partners: [],
+          topWinner: undefined,
+        }
+      }
+      const resp = await fetch('/api/home')
+      if (!resp.ok) {
+        throw resp
+      }
+      const data = await resp.json()
+      return data as HomePageData
+    },
+    {
+      retries: MAX_RETRY,
+      minTimeout: 250,
+      maxTimeout: 1000,
+    },
+  )
 })
