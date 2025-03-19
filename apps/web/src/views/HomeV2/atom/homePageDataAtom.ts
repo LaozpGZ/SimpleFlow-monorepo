0e908aad8344424f7ef18ca58a5230fd7d537fb9
memoyil <2213635+memoyil@users.noreply.github.com>
2025-03-19 11:23:06 +0100

    feat: Use atom retry

--- apps/web/src/views/HomeV2/atom/homePageDataAtom.ts
+++ apps/web/src/views/HomeV2/atom/homePageDataAtom.ts
@@ -1,35 +1,19 @@
-import { atom } from 'jotai'
-import { HomePageData } from 'pages/api/home/types'
-import retry from 'async-retry'
+import { atomWithAsyncRetry } from 'utils/atomWithAsyncRetry'
 
-const MAX_RETRY = 5
-
-export const homePageDataAtom = atom(async () => {
-  return retry(
-    async (_, retryAttempt) => {
-      if (retryAttempt === MAX_RETRY + 1) {
-        return {
-          tokens: [],
-          pools: [],
-          currencies: [],
-          chains: [],
-          cakeRelated: undefined,
-          stats: undefined,
-          partners: [],
-          topWinner: undefined,
-        }
-      }
-      const resp = await fetch('/api/home')
-      if (!resp.ok) {
-        throw resp
-      }
-      const data = await resp.json()
-      return data as HomePageData
-    },
-    {
-      retries: MAX_RETRY,
-      minTimeout: 250,
-      maxTimeout: 1000,
-    },
-  )
+export const homePageDataAtom = atomWithAsyncRetry({
+  asyncFn: async () => {
+    const response = await fetch('/api/home')
+    if (!response.ok) throw new Error('Fetch error')
+    return response.json()
+  },
+  fallbackValue: {
+    tokens: [],
+    pools: [],
+    currencies: [],
+    chains: [],
+    cakeRelated: undefined,
+    stats: undefined,
+    partners: [],
+    topWinner: undefined,
+  },
 })
