659ce95859ac85833e2e46602c23f53530df0797
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-03-20 17:14:35 +0800

    chore: remove comments

--- apps/web/src/components/AdPanel/constants.ts
+++ apps/web/src/components/AdPanel/constants.ts
@@ -1,9 +1,7 @@
-// 存放共享的常量，避免循環依賴
 import { ConfigType } from './FAQ/types'
 
 export const commonLayoutWhitelistedPages = ['/swap', '/buy-crypto', '/prediction']
 
-// 其他常量也可以放在這裡
 export const pageToFaqTypeMap: Record<string, ConfigType> = {
   [commonLayoutWhitelistedPages[0]]: 'swap',
   [commonLayoutWhitelistedPages[1]]: 'prediction',
