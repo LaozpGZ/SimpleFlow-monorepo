8c75303d62478c2227b8740a3a1ee1551060ec75
ChefEric <173023571+chef-eric@users.noreply.github.com>
2025-03-20 15:28:11 +0800

    fix: update networkswitch solana link

--- apps/web/src/components/NetworkSwitcher.tsx
+++ apps/web/src/components/NetworkSwitcher.tsx
@@ -37,7 +37,7 @@ const NON_EVM_CHAINS = [
   {
     id: 2,
     name: 'Solana',
-    link: 'https://solana.pancakeswap.finance/swap',
+    link: 'https://solana.pancake.run/swap',
     image: 'https://tokens.pancakeswap.finance/images/symbol/sol.png',
   },
 ]
