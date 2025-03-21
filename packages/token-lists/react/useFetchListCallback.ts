b22c900cd61df53501e90c4e3a522e38729b7f76
chef-ryan <ryan@pancakeswap.com>
2025-03-21 08:35:16 +0800

    reduce homepage update rate to 5mins

--- packages/token-lists/react/useFetchListCallback.ts
+++ packages/token-lists/react/useFetchListCallback.ts
@@ -1,7 +1,8 @@
 import { nanoid } from '@reduxjs/toolkit'
 import { useCallback } from 'react'
-import { fetchTokenList } from './actions'
 import { TokenList } from '../src/types'
+import { fetchTokenList } from './actions'
+import { getTokenList } from './getTokenList'
 
 function useFetchListCallback(
   dispatch: (action?: unknown) => void,
@@ -14,7 +15,6 @@ function useFetchListCallback(
         dispatch(fetchTokenList.pending({ requestId, url: listUrl }))
       }
       // lazy load avj and token list schema
-      const getTokenList = (await import('./getTokenList')).default
       return getTokenList(listUrl)
         .then((tokenList) => {
           if (sendDispatch) {
