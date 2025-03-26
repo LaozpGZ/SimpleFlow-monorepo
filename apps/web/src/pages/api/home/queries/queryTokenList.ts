f326b1660f639175633a05df6a22b6a287cf22f0
chef-ryan <ryan@pancakeswap.com>
2025-03-26 21:25:12 +0800

    fix typo

--- apps/web/src/pages/api/home/queries/queryTokenList.ts
+++ apps/web/src/pages/api/home/queries/queryTokenList.ts
@@ -27,7 +27,7 @@ const _queryTokenList = async () => {
 export const queryTokenList = cacheByLRU(_queryTokenList, {
   ttl: 300 * 1000, // 5 minutes
   persist: {
-    name: 'hompage-tokenlist',
+    name: 'homepage-token-list',
     type: 'r2',
     version: 'v1',
   },
