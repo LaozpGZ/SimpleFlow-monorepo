4448d73e78dab8b4efcee574952632dc2aea16c9
chefjackson <116779127+chefjackson@users.noreply.github.com>
2025-04-02 15:26:34 +0800

    chore: only show infinity tab on supported chains (#909)

--- apps/web/src/views/Info/index.tsx
+++ apps/web/src/views/Info/index.tsx
@@ -1,9 +1,10 @@
+import { INFINITY_SUPPORTED_CHAINS } from '@pancakeswap/infinity-sdk'
 import { useTranslation } from '@pancakeswap/localization'
 import { SubMenuItems } from '@pancakeswap/uikit'
 import { useRouter } from 'next/router'
 import { useMemo } from 'react'
 import { multiChainQueryStableClient } from 'state/info/constant'
-import { useChainNameByQuery, useMultiChainPath } from 'state/info/hooks'
+import { useChainIdByQuery, useChainNameByQuery, useMultiChainPath } from 'state/info/hooks'
 
 import InfoNav from './components/InfoNav'
 
@@ -11,14 +12,13 @@ export const InfoPageLayout = ({ children }) => {
   const router = useRouter()
   const chainName = useChainNameByQuery()
   const chainPath = useMultiChainPath()
+  const chainId = useChainIdByQuery()
+  const isInfinitySupported = useMemo(() => INFINITY_SUPPORTED_CHAINS.includes(chainId as any), [chainId])
   const { t } = useTranslation()
   const isStableSwap = router.query.type === 'stableSwap'
+
   const subMenuItems = useMemo(() => {
     const config = [
-      {
-        label: t('Infinity'),
-        href: `/info/infinity${chainPath}`,
-      },
       {
         label: t('V3'),
         href: `/info/v3${chainPath}`,
@@ -28,13 +28,19 @@ export const InfoPageLayout = ({ children }) => {
         href: `/info${chainPath}`,
       },
     ]
+    if (isInfinitySupported) {
+      config.unshift({
+        label: t('Infinity'),
+        href: `/info/infinity${chainPath}`,
+      })
+    }
     if (multiChainQueryStableClient[chainName])
       config.push({
         label: t('StableSwap'),
         href: `/info${chainPath}?type=stableSwap`,
       })
     return config
-  }, [t, chainPath, chainName])
+  }, [t, chainPath, chainName, isInfinitySupported])
 
   const activeItem = useMemo(() => {
     if (router.pathname.includes('infinity')) return `/info/infinity${chainPath}`
