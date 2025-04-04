9cf1639273d97be04f3486a91804a4e8a785cc1e
chef-ryan <ryan@pancakeswap.com>
2025-04-04 18:21:52 +0800

    fix token info page issues

--- apps/web/src/components/FeeTierBreakdown.tsx
+++ apps/web/src/components/FeeTierBreakdown.tsx
@@ -1,8 +1,7 @@
-import { Protocol } from '@pancakeswap/farms'
 import { useTranslation } from '@pancakeswap/localization'
-import { Percent } from '@pancakeswap/swap-sdk-core'
 import { Text } from '@pancakeswap/uikit'
 import { FeeTierTooltip } from '@pancakeswap/widgets-internal'
+import { useInfinityFeeTier } from 'hooks/infinity/useInfinityFeeTier'
 import { usePoolById } from 'hooks/infinity/usePool'
 import { useMemo } from 'react'
 import { Address } from 'viem'
@@ -15,26 +14,14 @@ export const InfinityFeeTierBreakdown = ({ poolId, chainId }: FeeTierBreakdownPr
   const { t } = useTranslation()
   const [, pool] = usePoolById(poolId, chainId)
 
-  /* eslint-disable no-bitwise */
-  const [protocolFee, lpFee] = useMemo(() => [(pool?.protocolFee ?? 0) & 0xfff, pool?.fee ?? 0], [pool])
-
-  const percent = useMemo(() => {
-    const totalFee = (protocolFee + ((1e6 - protocolFee) * lpFee) / 1e6).toFixed(0)
-    return new Percent(totalFee, 1e6)
-  }, [protocolFee, lpFee])
-
-  const type = useMemo(
-    () => (pool?.poolType === 'Bin' ? Protocol.InfinityBIN : Protocol.InfinityCLAMM),
-    [pool?.poolType],
-  )
-  const protocol = useMemo(() => (pool?.poolType === 'Bin' ? 'Infinity LBAMM' : 'Infinity CLAMM'), [pool?.poolType])
+  const { protocol, type, percent, lpFee, protocolFee } = useInfinityFeeTier(pool)
 
   const tooltips = useMemo(() => {
     return (
       <>
         <Text bold> {t('%t% LP', { t: protocol.toUpperCase() })}</Text>
-        <Text> - {t('%p%% LP Fee', { p: new Percent(lpFee, 1e6).toSignificant(2) })}</Text>
-        <Text> - {t('%p%% Protocol Fee', { p: new Percent(protocolFee, 1e6).toSignificant(2) })}</Text>
+        <Text> - {t('%p%% LP Fee', { p: lpFee.toSignificant(2) })}</Text>
+        <Text> - {t('%p%% Protocol Fee', { p: protocolFee.toSignificant(2) })}</Text>
       </>
     )
   }, [lpFee, protocolFee, t, protocol])
