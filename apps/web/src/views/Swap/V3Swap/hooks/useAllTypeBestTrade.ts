7ccc44e0ac3cec946ef4df3efb8b15f537542a11
Chef Philip <chef.philip@pancakeswap.com>
2025-04-07 12:23:11 -0400

    chore: add cross-chain mock

--- apps/web/src/views/Swap/V3Swap/hooks/useAllTypeBestTrade.ts
+++ apps/web/src/views/Swap/V3Swap/hooks/useAllTypeBestTrade.ts
@@ -74,7 +74,9 @@ export const useAllTypeBestTrade = () => {
   const hasAvailableDutchOrder =
     bestOrder.enabled && bestOrder.order?.type === OrderType.DUTCH_LIMIT && bestOrder.isValidQuote
   const betterQuote = useBetterQuote(classicAmmOrder, hasAvailableDutchOrder ? currentOrder : undefined)
-  const finalOrder = xEnabled ? betterQuote : classicAmmOrder
+  const finalOrder =
+    bestOrder?.order?.type === OrderType.PCS_BRIDGE ? currentOrder : xEnabled ? betterQuote : classicAmmOrder
+
   const tradeLoaded = Boolean(finalOrder && !finalOrder.isLoading)
 
   return {
