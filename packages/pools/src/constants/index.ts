b69f5c2ee80732c3b716a34bf4efb4523a060e9f
memoyil <2213635+memoyil@users.noreply.github.com>
2025-04-12 11:24:09 +0200

    fix: Comment

--- packages/pools/src/constants/index.ts
+++ packages/pools/src/constants/index.ts
@@ -8,6 +8,6 @@ export * from './supportedChains'
 export const BSC_BLOCK_TIME = AVERAGE_CHAIN_BLOCK_TIMES[ChainId.BSC]
 
 export const BLOCKS_PER_DAY = (60 / BSC_BLOCK_TIME) * 60 * 24
-export const BLOCKS_PER_YEAR = BLOCKS_PER_DAY * 365 // 10512000
+export const BLOCKS_PER_YEAR = BLOCKS_PER_DAY * 365 // 21024000
 
 export const SECONDS_IN_YEAR = 31536000 // 365 * 24 * 60 * 60
