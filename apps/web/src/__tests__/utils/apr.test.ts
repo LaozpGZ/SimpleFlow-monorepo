2ef4c7f3efc0b198ef64556ce44e11ac94ab168d
memoyil <2213635+memoyil@users.noreply.github.com>
2025-04-12 11:18:12 +0200

    fix: Test

--- apps/web/src/__tests__/utils/apr.test.ts
+++ apps/web/src/__tests__/utils/apr.test.ts
@@ -52,7 +52,7 @@ describe('getFarmApr', () => {
       '',
       40,
     )
-    expect(cakeRewardsApr).toEqual(4204800)
+    expect(cakeRewardsApr).toEqual(8409600)
     expect(lpRewardsApr).toEqual(0)
   })
   it(`get the correct pool APR combined with LP APR`, () => {
@@ -64,7 +64,7 @@ describe('getFarmApr', () => {
       '0x0ed7e52944161450477ee417de9cd3a859b14fd0',
       40,
     )
-    expect(cakeRewardsApr).toEqual(4204800)
+    expect(cakeRewardsApr).toEqual(8409600)
     expect(lpRewardsApr).toEqual(lpAprs['0x0ed7e52944161450477ee417de9cd3a859b14fd0'])
   })
 })
