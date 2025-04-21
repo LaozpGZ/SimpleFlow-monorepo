421792d9a0a257577ad68367729849087b240f2e
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-04-21 19:55:57 +0800

    chore: remove console

--- apps/web/src/views/universalFarms/components/PoolListView.tsx
+++ apps/web/src/views/universalFarms/components/PoolListView.tsx
@@ -41,14 +41,6 @@ export const ListView: React.FC<IPoolListViewProps> = ({ data, onRowClick }) => 
     <ListContainer>
       {data.map((item) => {
         const hasRewards = checkHasReward(item.chainId, item.lpAddress)
-        // DEBUG LOG: Print pool info for the specific pair address
-        if (hasRewards) {
-          // eslint-disable-next-line no-console
-          console.log(
-            '[DEBUG][PoolListView] Pair 0x345E7D44E1eb8894b4524Cfc918906718bc1FFe2:',
-            JSON.stringify(item, null, 2),
-          )
-        }
         return (
           <ListItemContainer key={`${item.chainId}-${item.lpAddress}`} onClick={() => onRowClick?.(item)}>
             <Column gap="12px">
