d9f8ce14d45d14e487ea3dc96fea711f9e3a94d6
Chef Philip <chef.philip@pancakeswap.com>
2025-04-22 18:54:49 -0400

    chore: add approval step

--- apps/web/src/views/Swap/Bridge/api.ts
+++ apps/web/src/views/Swap/Bridge/api.ts
@@ -50,13 +50,10 @@ interface CalldataRequestSchema {
   commands: (BridgeDataSchema | SwapDataSchema)[]
 }
 
-<<<<<<< HEAD
 function getTokenAddress(currency: Currency): Address {
   return currency.isNative ? '0x0000000000000000000000000000000000000000' : currency.wrapped.address
 }
 
-=======
->>>>>>> 760de2f9a (chore: update approval)
 export const getBridgeCalldata = async ({
   currencyAmountIn,
   currencyAmountOut,
@@ -70,13 +67,9 @@ export const getBridgeCalldata = async ({
     const bridgeCommand: BridgeDataSchema = {
       command: Command.BRIDGE,
       data: {
-<<<<<<< HEAD
         inputToken: getTokenAddress(currencyAmountIn.currency),
         outputToken: getTokenAddress(currencyAmountOut.currency),
-=======
-        inputToken: currencyAmountIn.currency.wrapped.address,
-        outputToken: currencyAmountOut.currency.wrapped.address,
->>>>>>> 760de2f9a (chore: update approval)
+
         inputAmount: currencyAmountIn.quotient.toString(),
         originChainId: currencyAmountIn.currency.chainId,
         destinationChainId: currencyAmountOut.currency.chainId,
@@ -88,13 +81,8 @@ export const getBridgeCalldata = async ({
     }
 
     const calldataRequest: CalldataRequestSchema = {
-<<<<<<< HEAD
       inputToken: getTokenAddress(currencyAmountIn.currency),
       outputToken: getTokenAddress(currencyAmountOut.currency),
-=======
-      inputToken: currencyAmountIn.currency.wrapped.address,
-      outputToken: currencyAmountOut.currency.wrapped.address,
->>>>>>> 760de2f9a (chore: update approval)
       inputAmount: currencyAmountIn.quotient.toString(),
       originChainId: currencyAmountIn.currency.chainId,
       destinationChainId: currencyAmountOut.currency.chainId,
