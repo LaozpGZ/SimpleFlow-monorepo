f61d9c03afe81afc02e8d89ab0ca0559fe435cf7
chef-ryan <ryan@pancakeswap.com>
2025-04-23 11:06:28 +0800

    remove page

--- apps/web/src/pages/vecake-redeem/index.tsx
+++
@@ -1,8 +0,0 @@
-import { SUPPORT_CAKE_STAKING } from 'config/constants/supportChains'
-import { VeCakeRedeem } from 'views/CakeStaking/VeCakeRedeem'
-
-const VeCakeRedeemPage = () => <VeCakeRedeem />
-
-VeCakeRedeemPage.chains = SUPPORT_CAKE_STAKING
-
-export default VeCakeRedeemPage
