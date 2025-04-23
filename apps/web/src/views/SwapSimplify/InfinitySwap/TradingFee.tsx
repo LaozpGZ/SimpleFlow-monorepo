47d130b87c7d191bf84608ec0e2c2f877710d408
Chef Jerry <jerry@pancakeswap.com>
2025-04-23 14:25:29 +0800

    feat: dynamic ~

--- apps/web/src/views/SwapSimplify/InfinitySwap/TradingFee.tsx
+++ apps/web/src/views/SwapSimplify/InfinitySwap/TradingFee.tsx
@@ -1,8 +1,12 @@
+import { findHook, HOOK_CATEGORY } from '@pancakeswap/infinity-sdk'
 import { useTranslation } from '@pancakeswap/localization'
-import { PriceOrder } from '@pancakeswap/price-api-sdk'
+import { OrderType, PriceOrder } from '@pancakeswap/price-api-sdk'
+import { PoolType } from '@pancakeswap/smart-router'
 import { FlexGap, SkeletonV2, Text } from '@pancakeswap/uikit'
 import { formatAmount } from '@pancakeswap/utils/formatFractions'
 import { memo, useMemo } from 'react'
+import { Address } from 'viem'
+import { isAddress } from 'viem/utils'
 import { isXOrder } from 'views/Swap/utils'
 import { useIsWrapping, useSlippageAdjustedAmounts } from '../../Swap/V3Swap/hooks'
 import { computeTradePriceBreakdown } from '../../Swap/V3Swap/utils/exchange'
@@ -19,6 +23,22 @@ export const TradingFee: React.FC<TradingFeeProps> = memo(({ order, loaded }) =>
     () => computeTradePriceBreakdown(isXOrder(order) ? order.ammTrade : order?.trade),
     [order],
   )
+
+  const containsDynamicHooks = useMemo(() => {
+    if (!order?.trade || order.type !== OrderType.PCS_CLASSIC) return false
+    const chainId = order.trade.inputAmount.currency.chainId
+    const pools = order.trade.routes
+      .map((r) => r.pools)
+      .flat()
+      .filter((pool) => pool.type === PoolType.InfinityBIN || pool.type === PoolType.InfinityCL)
+    if (pools.length === 0) return false
+
+    const hooks = pools.map((pool) => pool.hooks).filter((hook) => hook && isAddress(hook)) as Address[]
+
+    return hooks.some((hook) => {
+      return findHook(hook, chainId)?.category?.includes(HOOK_CATEGORY.DynamicFees) ?? false
+    })
+  }, [order?.trade])
   const isWrapping = useIsWrapping()
 
   if (isWrapping || !order || !order.trade || !slippageAdjustedAmounts) {
@@ -37,9 +57,10 @@ export const TradingFee: React.FC<TradingFeeProps> = memo(({ order, loaded }) =>
             0 {inputAmount?.currency?.symbol}
           </Text>
         ) : (
-          <Text color="textSubtle" fontSize="14px">{`${formatAmount(lpFeeAmount, 4)} ${
-            inputAmount?.currency?.symbol
-          }`}</Text>
+          <Text color="textSubtle" fontSize="14px">{`${containsDynamicHooks ? '~' : ''}${formatAmount(
+            lpFeeAmount,
+            4,
+          )} ${inputAmount?.currency?.symbol}`}</Text>
         )}
       </SkeletonV2>
     </FlexGap>
