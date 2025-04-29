949db86361cbcc8d8f3d82620475814cdc25df1d
chef-ryan <ryan@pancakeswap.com>
2025-04-29 17:42:25 +0800

    update deps

--- packages/smart-router/tsup.config.ts
+++ packages/smart-router/tsup.config.ts
@@ -8,6 +8,7 @@ export default defineConfig((options) => ({
   },
   format: ['esm', 'cjs'],
   skipNodeModulesBundle: true,
+  noExternal: ['@pancakeswap/utils'],
   dts: false,
   treeshake: true,
   splitting: true,
