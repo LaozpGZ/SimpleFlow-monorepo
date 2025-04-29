chef-ryan/pancake-frontend

feat: add Route Card (#11515)
<!--
Before opening a pull request, please read the [contributing
guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
first
-->


<!-- start pr-codex -->

---

## PR-Codex overview
This PR focuses on enhancing the handling of bridge orders within the
swap functionality. It introduces a new `BRIDGE` type for routes and
modifies various components and utilities to account for this new order
type.

### Detailed summary
- Added `BRIDGE` to route types.
- Exported `SHORT_SYMBOL` in `NetworkSwitcher`.
- Updated logic to handle `BridgeOrder` in price breakdown computations.
- Modified route props to include `type`.
- Enhanced various components to support `BridgeOrder` checks.
- Adjusted tests to include `chainId` in actions.
- Updated `TransactionConfirmSwapContent` to handle bridge-specific
logic.
- Introduced conditional rendering for bridge routes in UI components.

> ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
question}`

<!-- end pr-codex -->

packages/price-api-sdk/src/orderPriceApiParsers.ts
@@ -1,6 +1,6 @@
 import { ChainId } from '@pancakeswap/chains'
 import { ExclusiveDutchOrder, createExclusiveDutchOrderTrade } from '@pancakeswap/pcsx-sdk'
-import { PoolType, V4Router } from '@pancakeswap/smart-router'
+import { PoolType, RouteType, V4Router } from '@pancakeswap/smart-router'
 import { Currency, CurrencyAmount, Percent, TradeType, type BigintIsh } from '@pancakeswap/swap-sdk-core'
 import { zeroAddress } from './getCurrencyPrice'
 import { getPoolTypeKey } from './getPoolType'
@@ -101,7 +101,14 @@ export function parseBridgeQuoteResponse<
     trade: {
       inputAmount: amountIn,
       outputAmount: CurrencyAmount.fromRawAmount(currencyOut, amountIn.quotient.toString()),
-      routes: [],
+      routes: [
+        {
+          path: [amountIn.currency, currencyOut],
+          inputAmount: amountIn,
+          outputAmount: CurrencyAmount.fromRawAmount(currencyOut, amountIn.quotient.toString()),
+          type: RouteType.BRIDGE,
+        },
+      ],
       tradeType,
     },
   }

LaozpGZ/SimpleFlow-monorepo

packages/price-api-sdk/src/orderPriceApiParsers.ts
@@ -1,6 +1,6 @@
 import { ChainId } from '@pancakeswap/chains'
 import { ExclusiveDutchOrder, createExclusiveDutchOrderTrade } from '@pancakeswap/pcsx-sdk'
-import { InfinityRouter, PoolType } from '@pancakeswap/smart-router'
+import { InfinityRouter, PoolType, RouteType } from '@pancakeswap/smart-router'
 import { Currency, CurrencyAmount, Percent, TradeType, type BigintIsh } from '@pancakeswap/swap-sdk-core'
 import { zeroAddress } from './getCurrencyPrice'
 import { getPoolTypeKey } from './getPoolType'
@@ -101,7 +101,14 @@ export function parseBridgeQuoteResponse<
     trade: {
       inputAmount: amountIn,
       outputAmount: CurrencyAmount.fromRawAmount(currencyOut, amountIn.quotient.toString()),
-      routes: [],
+      routes: [
+        {
+          path: [amountIn.currency, currencyOut],
+          inputAmount: amountIn,
+          outputAmount: CurrencyAmount.fromRawAmount(currencyOut, amountIn.quotient.toString()),
+          type: RouteType.BRIDGE,
+        },
+      ],
       tradeType,
     },
   }