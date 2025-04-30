4933369f5eccf68d1ae30c3877487613734f00ba
Chef Philip <chef.philip@pancakeswap.com>
2025-04-30 10:07:24 -0400

    chore: update address

--- apps/web/src/quoter/hook/useQuoterSync.ts
+++ apps/web/src/quoter/hook/useQuoterSync.ts
@@ -62,7 +62,7 @@ export const useQuoterSync = () => {
 
   const outputTokenOnInputChainId =
     outputCurrency?.isNative || needsDestinationSwap
-      ? Native.onChain(chainId).symbol
+      ? Native.onChain(chainId).wrapped
       : filterToken
       ? Object.values(allTokens)
           .filter(filterToken)
@@ -70,8 +70,7 @@ export const useQuoterSync = () => {
       : undefined
 
   if (outputCurrencyId && chainId && chainId !== outputCurrencyChainId && outputTokenOnInputChainId) {
-    outputCurrencyId =
-      typeof outputTokenOnInputChainId === 'string' ? outputTokenOnInputChainId : outputTokenOnInputChainId.address
+    outputCurrencyId = outputTokenOnInputChainId.address
     outputCurrencyChainId = chainId
   }
 
@@ -191,13 +190,16 @@ export const useQuoterSync = () => {
     isLoading: bridgeLoading,
   } = useBridgeMetadata({
     inputAmount: needsDestinationSwap
-      ? CurrencyAmount.fromRawAmount(Native.onChain(chainId), swapOrder?.trade?.outputAmount?.quotient.toString() || 0)
+      ? CurrencyAmount.fromRawAmount(
+          Native.onChain(chainId).wrapped,
+          swapOrder?.trade?.outputAmount?.quotient.toString() || 0,
+        )
       : swapOrder?.trade
       ? swapOrder?.trade?.outputAmount
       : amount,
     outputCurrency:
       needsDestinationSwap && stateOutputCurrencyChainId
-        ? Native.onChain(stateOutputCurrencyChainId)
+        ? Native.onChain(stateOutputCurrencyChainId).wrapped
         : stateOutputCurrency,
   })
 
