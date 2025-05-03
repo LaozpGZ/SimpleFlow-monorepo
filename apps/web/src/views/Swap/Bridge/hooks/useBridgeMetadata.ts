999357231a8435c703e4f8b63ffbe998524530f5
Chef Philip <chef.philip@pancakeswap.com>
2025-05-03 17:39:59 -0400

    chore: support multiple quotes

--- apps/web/src/views/Swap/Bridge/hooks/useBridgeMetadata.ts
+++ apps/web/src/views/Swap/Bridge/hooks/useBridgeMetadata.ts
@@ -1,8 +1,8 @@
-import { OrderType } from '@pancakeswap/price-api-sdk'
+import { BridgeOrder, OrderType } from '@pancakeswap/price-api-sdk'
 import { Currency, CurrencyAmount, TradeType } from '@pancakeswap/sdk'
 import { RouteType } from '@pancakeswap/smart-router'
 import { useQuery } from '@tanstack/react-query'
-import { UnsafeCurrency } from 'config/constants/types'
+import first from 'lodash/first'
 import { getMetadata, getTokenAddress } from '../api'
 
 export class BridgeTradeError extends Error {
@@ -12,54 +12,58 @@ export class BridgeTradeError extends Error {
   }
 }
 
-export function useBridgeMetadata({
-  inputAmount,
-  outputCurrency,
-}: {
-  inputAmount?: CurrencyAmount<Currency>
-  outputCurrency?: UnsafeCurrency
-}) {
-  const inputToken = inputAmount ? getTokenAddress(inputAmount.currency) : undefined
-  const originChainId = inputAmount?.currency.chainId
-  const outputToken = outputCurrency ? getTokenAddress(outputCurrency) : undefined
-  const destinationChainId = outputCurrency?.chainId
-  const amount = inputAmount?.quotient.toString()
+export type BridgeMetadataParams = {
+  inputAmount: CurrencyAmount<Currency>
+  outputCurrency: Currency
+}
+
+export function useBridgeMetadata(params: BridgeMetadataParams[]) {
+  const originChainId = first(params)?.inputAmount?.currency.chainId
+  const destinationChainId = first(params)?.outputCurrency?.chainId
 
   return useQuery({
-    queryKey: ['bridge-metadata', inputToken, originChainId, outputToken, destinationChainId, amount],
+    queryKey: ['bridge-metadata', originChainId, destinationChainId],
     queryFn: async () => {
-      const metadata = await getMetadata({
-        inputToken: inputToken!,
-        originChainId: originChainId!,
-        outputToken: outputToken!,
-        destinationChainId: destinationChainId!,
-        amount: amount!,
-      })
-
-      if (!metadata.supported) {
-        throw new BridgeTradeError(metadata.reason)
+      if (!params?.length) {
+        return undefined
       }
 
-      const outputAmount = CurrencyAmount.fromRawAmount(outputCurrency!, metadata.minOutputAmount)
+      async function exectureData({ inputAmount, outputCurrency }: BridgeMetadataParams): Promise<BridgeOrder> {
+        const metadata = await getMetadata({
+          inputToken: getTokenAddress(inputAmount.currency),
+          originChainId: originChainId!,
+          outputToken: getTokenAddress(outputCurrency),
+          destinationChainId: destinationChainId!,
+          amount: inputAmount.quotient.toString(),
+        })
 
-      return {
-        bridgeFee: CurrencyAmount.fromRawAmount(inputAmount!.currency, metadata.bridgeFee),
-        type: OrderType.PCS_BRIDGE,
-        trade: {
-          inputAmount,
-          outputAmount,
-          routes: [
-            {
-              path: [inputAmount!.currency, outputAmount.currency],
-              inputAmount,
-              outputAmount,
-              type: RouteType.BRIDGE,
-            },
-          ],
-          tradeType: TradeType.EXACT_INPUT,
-        },
+        if (!metadata.supported) {
+          throw new BridgeTradeError(metadata.reason)
+        }
+
+        const outputAmount = CurrencyAmount.fromRawAmount(outputCurrency!, metadata.minOutputAmount)
+
+        return {
+          bridgeFee: CurrencyAmount.fromRawAmount(inputAmount!.currency, metadata.bridgeFee),
+          type: OrderType.PCS_BRIDGE,
+          trade: {
+            inputAmount,
+            outputAmount,
+            routes: [
+              {
+                path: [inputAmount!.currency, outputAmount.currency],
+                inputAmount,
+                outputAmount,
+                type: RouteType.BRIDGE,
+              },
+            ],
+            tradeType: TradeType.EXACT_INPUT,
+          },
+        }
       }
+
+      return Promise.all(params.map(exectureData))
     },
-    enabled: !!inputAmount && !!outputCurrency && inputAmount.greaterThan(0) && originChainId !== destinationChainId,
+    enabled: !!params?.length && originChainId !== destinationChainId,
   })
 }
