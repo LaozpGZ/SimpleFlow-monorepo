a0fb5f966c78dce617c49e3274c84cd0fae58a34
Chef Philip <chef.philip@pancakeswap.com>
2025-05-06 11:44:56 -0400

    chore: add info

--- apps/web/src/views/Swap/Bridge/api.ts
+++ apps/web/src/views/Swap/Bridge/api.ts
@@ -89,8 +89,7 @@ export const getBridgeCalldata = async ({
             originChainId: command.trade.inputAmount.currency.chainId,
             destinationChainId: command.trade.outputAmount.currency.chainId,
             originChainRecipient: recipient,
-            // minOutputAmount: command.trade.outputAmount.quotient.toString(),
-            minOutputAmount: '1',
+            minOutputAmount: command.trade.outputAmount.quotient.toString(),
           },
         }
       }
