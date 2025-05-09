531cf5b1b5722f6ff474e7a2334919bdfad5b89d
Chef Philip <chef.philip@pancakeswap.com>
2025-05-09 17:39:18 -0400

    chore: revert hash

--- apps/web/src/quoter/atom/bestCrossChainAtom.ts
+++ apps/web/src/quoter/atom/bestCrossChainAtom.ts
@@ -498,7 +498,7 @@ export const bestCrossChainQuoteAtom = atomFamily((_option: QuoteQuery) => {
             ...result,
             data: placeHolder,
             hash: _option.hash,
-            placeholderHash: `${_option.placeholderHash}`,
+            placeholderHash: _option.placeholderHash,
             loading: !placeHolder,
           }
         }
@@ -506,7 +506,7 @@ export const bestCrossChainQuoteAtom = atomFamily((_option: QuoteQuery) => {
         return {
           ...result,
           hash: _option.hash,
-          placeholderHash: `${_option.placeholderHash}-${result?.data?.trade?.outputAmount?.quotient.toString()}`,
+          placeholderHash: _option.placeholderHash,
         }
       } catch (error) {
         console.error('Failed to get cross chain quote:', error)
