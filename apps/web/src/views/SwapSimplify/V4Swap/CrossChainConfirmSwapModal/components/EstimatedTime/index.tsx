d38a6947974c9d808cba6578d0bc8a7950383aff
Chef Philip <chef.philip@pancakeswap.com>
2025-05-11 23:26:06 -0400

    chore: fix broken

--- apps/web/src/views/SwapSimplify/V4Swap/CrossChainConfirmSwapModal/components/EstimatedTime/index.tsx
+++ apps/web/src/views/SwapSimplify/V4Swap/CrossChainConfirmSwapModal/components/EstimatedTime/index.tsx
@@ -1,5 +1,11 @@
+'use client'
+
 import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
 import { useMemo } from 'react'
+
+// Extend dayjs with duration plugin
+dayjs.extend(duration)
 
 export function EstimatedTime({ expectedFillTimeSec }: { expectedFillTimeSec?: number }) {
   const estimatedTimeDisplay = useMemo(() => {
