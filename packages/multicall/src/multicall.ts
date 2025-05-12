45d7ba5c86302bcf08de330cd8b0c3dad1125e1c
chef-ryan <ryan@pancakeswap.com>
2025-05-12 10:36:53 +0800

    support account context for hooks

--- packages/multicall/src/multicall.ts
+++ packages/multicall/src/multicall.ts
@@ -36,6 +36,7 @@ export async function multicallByGasLimit(
     dropUnexecutedCalls,
     signal,
     retryFailedCallsWithGreaterLimit,
+    account,
     ...rest
   }: CallByGasLimitParams,
 ) {
@@ -52,6 +53,7 @@ export async function multicallByGasLimit(
     chainId,
     dropUnexecutedCalls,
     signal,
+    account,
   })
   if (!retryFailedCallsWithGreaterLimit) {
     return callResult
@@ -105,7 +107,7 @@ export async function multicallByGasLimit(
 
 type CallParams = Pick<
   CallByGasLimitParams,
-  'chainId' | 'client' | 'gasBuffer' | 'blockConflictTolerance' | 'dropUnexecutedCalls' | 'signal'
+  'chainId' | 'client' | 'gasBuffer' | 'blockConflictTolerance' | 'dropUnexecutedCalls' | 'signal' | 'account'
 >
 
 export type SingleCallResult = {
@@ -155,6 +157,7 @@ async function call(calls: MulticallRequestWithGas[], params: CallParams): Promi
     blockConflictTolerance = getBlockConflictTolerance(chainId),
     dropUnexecutedCalls = false,
     signal,
+    account,
   } = params
   if (!calls.length) {
     return {
@@ -167,14 +170,9 @@ async function call(calls: MulticallRequestWithGas[], params: CallParams): Promi
 
   const contract = getMulticallContract({ chainId, client })
   try {
-    const { result } = await contract.simulate.multicallWithGasLimitation(
-      [calls, gasBuffer],
-      typeof window !== 'undefined' && window.ethereum?.selectedAddress
-        ? {
-            account: window.ethereum.selectedAddress,
-          }
-        : undefined,
-    )
+    const { result } = await contract.simulate.multicallWithGasLimitation([calls, gasBuffer], {
+      account,
+    })
     const { results, lastSuccessIndex, blockNumber } = formatCallReturn(result as CallReturnFromContract)
     if (lastSuccessIndex === calls.length - 1) {
       return {
