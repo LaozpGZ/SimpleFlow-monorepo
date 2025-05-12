12405cb27ab2b9631a75fcb1de0abbcb4bb34d03
Chef Philip <chef.philip@pancakeswap.com>
2025-05-12 11:28:00 -0400

    chore: remove mock

--- apps/web/src/pages/api/mock/type.ts
+++
@@ -1,9 +0,0 @@
-export interface BridgeRoute {
-  originChainId: number
-  originToken: string
-  destinationChainId: number
-  destinationToken: string
-  originTokenSymbol: string
-  destinationTokenSymbol: string
-  isNative: boolean
-}
