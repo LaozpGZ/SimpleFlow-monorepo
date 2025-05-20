32bdd0fe76bc02c6ad91e630ab53be37c32ce64e
Chef Philip <chef.philip@pancakeswap.com>
2025-05-20 15:46:34 -0400

    fix: native vs wrapped comparision

--- apps/web/src/quoter/atom/bestCrossChainAtom.ts
+++ apps/web/src/quoter/atom/bestCrossChainAtom.ts
@@ -167,7 +167,7 @@ export const bestCrossChainQuoteWithoutPlaceHolderAtom = atomFamily((_option: Qu
 
         // pass native token to bridge, server will convert to wrapped address for route check
         const isBridgeOnlyQuery =
-          baseCurrencyAmount.currency.symbol === quoteCurrency.wrapped.symbol && isOriginTokenSupported
+          baseCurrencyAmount.currency.wrapped.symbol === quoteCurrency.wrapped.symbol && isOriginTokenSupported
         // if origin/destination tokens is supported, then it's a bridge to swap quote
         const isBridgeToSwapQuery = isOriginTokenSupported
         const isSwapToBridgeQuery = !isOriginTokenSupported && isDestinationTokenSupported
