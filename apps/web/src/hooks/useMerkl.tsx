b9ccf3d8d38c2b1149d4ba6dfdd5548aa33958ec
memoyil <2213635+memoyil@users.noreply.github.com>
2025-05-26 17:07:44 +0200

    fix: Merkl api opportunity id

--- apps/web/src/hooks/useMerkl.tsx
+++ apps/web/src/hooks/useMerkl.tsx
@@ -59,7 +59,8 @@ export function useMerklInfo(poolAddress?: string): {
       const opportunities = merklDataV4?.filter(
         (opportunity) =>
           opportunity?.tokens?.[0]?.symbol?.toLowerCase().startsWith('cake-lp') ||
-          opportunity?.protocol?.id?.toLowerCase().startsWith('pancakeswap'),
+          opportunity?.protocol?.id?.toLowerCase().startsWith('pancakeswap') ||
+          opportunity?.protocol?.id?.toLowerCase().startsWith('pancake-swap'),
       )
 
       if (!opportunities || !opportunities.length) return undefined
