ba99fd46ebf520df8e4289f9fe89198db07a82ee
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-05-26 23:56:48 +0800

    chore: fix url pattern

--- scripts/updateMerkl/index.ts
+++ scripts/updateMerkl/index.ts
@@ -11,7 +11,7 @@ type MerklConfigPool = {
 
 export const chainIdToChainName = {
   1: 'ethereum',
-  56: 'bnb smart chain',
+  56: 'bsc',
   324: 'zksync',
   1101: 'polygon zkevm',
   8453: 'base',
@@ -52,9 +52,7 @@ const parseMerklConfig = (merklConfigResponse: any[]): MerklConfigPool[] => {
       chainId: pool.chainId,
       address: pool.identifier,
       link: encodeURI(
-        `https://merkl.angle.money/${chainIdToChainName[pool.chainId]}/pool/${pool.type === 'ERC20' ? 1 : 2}/${
-          pool.identifier
-        }`,
+        `https://app.merkl.xyz/opportunities/${chainIdToChainName[pool.chainId]}/${pool.type}/${pool.identifier}`,
       ),
     }))
 }
