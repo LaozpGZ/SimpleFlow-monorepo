23a4cdc6fd5676599b50613a9641f704b494a583
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-05-27 20:19:11 +0800

    chore: refetch the list

--- apps/web/src/views/SwapSimplify/InfinitySwap/SwapCommitButton.tsx
+++ apps/web/src/views/SwapSimplify/InfinitySwap/SwapCommitButton.tsx
@@ -295,7 +295,10 @@ const SwapCommitButtonInner = memo(function SwapCommitButtonInner({
 
       // Refresh balances
       if (refreshBalances) {
-        refreshBalances()
+        // delay refresh balances
+        setTimeout(() => {
+          refreshBalances()
+        }, 15000)
       }
     }
   }, [confirmState, txHash, refreshBalances])
