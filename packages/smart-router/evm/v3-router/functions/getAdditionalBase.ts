71cb17c7923ba7ab4f58a51acd8372eecf989d8e
chef-ryan <ryan@pancakeswap.com>
2025-05-28 19:16:20 +0800

    add cache time

--- packages/smart-router/evm/v3-router/functions/getAdditionalBase.ts
+++ packages/smart-router/evm/v3-router/functions/getAdditionalBase.ts
@@ -4,37 +4,44 @@ import memoize from '@pancakeswap/utils/memoize'
 import { Address, checksumAddress } from 'viem'
 import { ADDITIONAL_BASES, ADDITIONAL_BASES_TABLE } from '../../constants'
 
-const fetchConfig = memoize(async () => {
-  const url = `https://proofs.pancakeswap.com/cms-config/routing-base-config.json`
-  try {
-    const response = await fetch(url)
-    const data: {
-      [chainId: string]: {
-        [tokenAddress: Address]: {
-          address: Address
-          symbol: string
-          decimals: number
-        }[]
-      }
-    } = await response.json()
+const fetchConfig = memoize(
+  async () => {
+    const url = `https://proofs.pancakeswap.com/cms-config/routing-base-config.json`
+    try {
+      const response = await fetch(url)
+      const data: {
+        [chainId: string]: {
+          [tokenAddress: Address]: {
+            address: Address
+            symbol: string
+            decimals: number
+          }[]
+        }
+      } = await response.json()
 
-    const table: ADDITIONAL_BASES_TABLE = {}
-    Object.keys(data).forEach((key) => {
-      const chainId = Number(key) as ChainId
-      table[chainId] = {}
-      for (const [address, list] of Object.entries(data[key])) {
-        const src = checksumAddress(address as Address)
-        const to = list.map((item) => {
-          return new Token(chainId, checksumAddress(item.address), item.decimals, item.symbol, '')
-        })
-        table[chainId][src] = to
-      }
-    })
-    return table
-  } catch {
-    return ADDITIONAL_BASES
-  }
-})
+      const table: ADDITIONAL_BASES_TABLE = {}
+      Object.keys(data).forEach((key) => {
+        const chainId = Number(key) as ChainId
+        table[chainId] = {}
+        for (const [address, list] of Object.entries(data[key])) {
+          const src = checksumAddress(address as Address)
+          const to = list.map((item) => {
+            return new Token(chainId, checksumAddress(item.address), item.decimals, item.symbol, '')
+          })
+          table[chainId][src] = to
+        }
+      })
+      return table
+    } catch {
+      return ADDITIONAL_BASES
+    }
+  },
+  () => {
+    // Memoize for 1 minute, this is mainly for API side
+    // For client side, when user reloads the page, it will fetch again
+    return Math.floor(Date.now() / 60_1000)
+  },
+)
 
 type TokenBases = {
   [tokenAddress: Address]: Token[]
