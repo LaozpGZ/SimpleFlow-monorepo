b5e01ceb7c6d2d34b6b0c560f29dacd132010fb5
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-05-30 05:50:52 +0800

    fix: support dapp wallet icons

--- apps/web/src/components/WalletModalV2/WalletCopyButton.tsx
+++ apps/web/src/components/WalletModalV2/WalletCopyButton.tsx
@@ -1,11 +1,12 @@
 import { previouslyUsedWalletsAtom } from '@pancakeswap/ui-wallets'
-import { Box, CopyButton, Flex, FlexProps, Image, Text } from '@pancakeswap/uikit'
+import { Box, CopyButton, Flex, FlexProps, Image, Text, WalletFilledV2Icon } from '@pancakeswap/uikit'
+import { ASSET_CDN } from 'config/constants/endpoints'
 import { walletsConfig } from 'config/wallet'
 import { useActiveChainId } from 'hooks/useActiveChainId'
 import { useAtom } from 'jotai'
 import { useMemo } from 'react'
 import { styled } from 'styled-components'
-import { useConnect } from 'wagmi'
+import { useAccount, useConnect } from 'wagmi'
 
 interface CopyAddressProps extends FlexProps {
   account: string | undefined
@@ -21,6 +22,9 @@ const Wrapper = styled(Flex)`
 `
 
 const WalletIcon = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 32px;
   height: 32px;
   margin-right: 12px;
@@ -47,6 +51,25 @@ const WalletAddress = styled(Text)`
 const CopyButtonWrapper = styled(Box)`
   margin-left: 8px;
 `
+const DAPP_LIST = ['isBinance', 'isCoinbaseWallet', 'isOkxWallet', 'isTokenpocket']
+const DAPP_WALLET_ICON = {
+  [DAPP_LIST[0]]: `${ASSET_CDN}/web/wallets/binance-w3w.png`,
+  [DAPP_LIST[1]]: `${ASSET_CDN}/web/wallets/coinbase.png`,
+  [DAPP_LIST[2]]: `${ASSET_CDN}/web/wallets/okx-wallet.png`,
+  [DAPP_LIST[3]]: `${ASSET_CDN}/web/wallets/tokenpocket.png`,
+}
+
+const useDappIcon = () => {
+  const { connector } = useAccount()
+  const dappIcon = useMemo(() => {
+    const isDappWallet = DAPP_LIST.some((d) => connector?.provider?.[d] === true)
+    if (!isDappWallet) return undefined
+    const walletName = DAPP_LIST.find((d) => connector?.provider?.[d] === true)
+    if (!walletName) return undefined
+    return DAPP_WALLET_ICON?.[walletName]
+  }, [connector])
+  return { dappIcon }
+}
 
 export const CopyAddress: React.FC<React.PropsWithChildren<CopyAddressProps>> = ({
   account,
@@ -61,6 +84,7 @@ export const CopyAddress: React.FC<React.PropsWithChildren<CopyAddressProps>> = 
   const walletConfig = walletsConfig({ chainId, connect: connectAsync })
 
   const wallet = useMemo(() => walletConfig.find((w) => w.id === previouslyUsedWalletsId[0]), [walletConfig])
+  const { dappIcon } = useDappIcon()
 
   // Format the address to show only the first 6 and last 4 characters
   const formatAddress = (address: string | undefined) => {
@@ -72,7 +96,11 @@ export const CopyAddress: React.FC<React.PropsWithChildren<CopyAddressProps>> = 
     <Box position="relative" {...props} onClick={(e) => e.stopPropagation()}>
       <Wrapper>
         <WalletIcon>
-          <Image src={(wallet?.icon as string) || ''} width={40} height={40} alt="Wallet" />
+          {wallet?.icon || dappIcon ? (
+            <Image src={(wallet?.icon as string) || dappIcon} width={40} height={40} alt="Wallet" />
+          ) : (
+            <WalletFilledV2Icon width={28} height={28} color="primary" />
+          )}
         </WalletIcon>
         <AddressBox>
           <WalletAddress title={account}>{formatAddress(account)}</WalletAddress>
