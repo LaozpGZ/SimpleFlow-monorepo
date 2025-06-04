45e6664aceb44664ffc25ab8d10350e79bf66d54
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-06-04 15:38:44 +0800

    chore: remove console

--- apps/web/src/components/WalletModalV2/WalletModal.tsx
+++ apps/web/src/components/WalletModalV2/WalletModal.tsx
@@ -226,7 +226,6 @@ export const WalletContent = ({
   // Get top tokens by value
   const topTokens = balances
   const noAssets = (topTokens.length === 0 || totalBalanceUsd === 0) && !isLoading
-  console.log('topTokens???', topTokens)
   const handleClick = useCallback((newIndex: number) => {
     setView(newIndex)
   }, [])
