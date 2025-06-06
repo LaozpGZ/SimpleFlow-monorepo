40fc320af58a6729480cf3edec3027982ac6d5a9
memoyil <2213635+memoyil@users.noreply.github.com>
2025-06-06 19:45:08 +0200

    fix: Logout instead of refresh

--- apps/web/src/hooks/useSwitchNetwork.ts
+++ apps/web/src/hooks/useSwitchNetwork.ts
@@ -11,6 +11,7 @@ import { useAtom } from 'jotai/index'
 import { queryChainIdAtom } from 'hooks/useActiveChainId'
 import { EXCHANGE_PAGE_PATHS } from 'config/constants/exchange'
 import { getHashFromRouter } from 'utils/getHashFromRouter'
+import useAuth from 'hooks/useAuth'
 import { useSwitchNetworkLoading } from './useSwitchNetworkLoading'
 
 const checkSwitchReloadNeeded = async (connector: Connector, chainId: number, address: `0x${string}` | undefined) => {
@@ -100,6 +101,8 @@ export function useSwitchNetwork() {
   const { toastError } = useToast()
   const { isConnected, connector, address } = useAccount()
 
+  const { logout } = useAuth()
+
   const switchNetworkLocal = useSwitchNetworkLocal()
 
   const isLoading = _isLoading || loading
@@ -113,7 +116,7 @@ export function useSwitchNetwork() {
           .then(async (c) => {
             switchNetworkLocal(chainId)
             if (await checkSwitchReloadNeeded(connector, chainId, address)) {
-              window.location.reload()
+              await logout()
             }
             return c
           })
@@ -127,7 +130,18 @@ export function useSwitchNetwork() {
         resolve(switchNetworkLocal(chainId))
       })
     },
-    [isConnected, _switchNetworkAsync, isLoading, setLoading, switchNetworkLocal, toastError, t, connector, address],
+    [
+      isConnected,
+      _switchNetworkAsync,
+      isLoading,
+      setLoading,
+      switchNetworkLocal,
+      toastError,
+      t,
+      connector,
+      address,
+      logout,
+    ],
   )
 
   const switchNetwork = useCallback(
