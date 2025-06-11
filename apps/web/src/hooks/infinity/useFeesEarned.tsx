ef76cc35f4bfeb2abae1fcb39d8ff3fb8777e9a7
Chef Philip <chef.philip@pancakeswap.com>
2025-06-10 22:27:17 -0400

    chore: add log

--- apps/web/src/hooks/infinity/useFeesEarned.tsx
+++ apps/web/src/hooks/infinity/useFeesEarned.tsx
@@ -38,6 +38,14 @@ const fetchFeesEarned = ({
   tickUpper: number
   tokenId: bigint
 }) => {
+  console.log('chainId', chainId)
+  console.log('poolManager', poolManager)
+  console.log('poolId', poolId)
+  console.log('owner', owner)
+  console.log('tickLower', tickLower)
+  console.log('tickUpper', tickUpper)
+  console.log('tokenId', toHex(tokenId, { size: 32 }))
+
   return getViemClients({ chainId }).readContract({
     abi: FARMING_OFFCHAIN_CL_HELPER_ABI,
     address: INFI_CL_LP_FEES_HELPER_ADDRESSES[chainId],
