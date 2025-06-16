eb6bdb88f5374d68255ef7cf28e53854406b1a38
chef-ryan <ryan@pancakeswap.com>
2025-06-16 16:54:41 +0800

    change fallback logic

--- apps/web/src/utils/fallbackWithRank.ts
+++ apps/web/src/utils/fallbackWithRank.ts
@@ -1,13 +1,5 @@
 import { memoizeAsync } from '@pancakeswap/utils/memoize'
-import {
-  Chain,
-  createTransport,
-  FallbackTransport,
-  FallbackTransportConfig,
-  HttpRequestError,
-  Prettify,
-  Transport,
-} from 'viem'
+import { Chain, createTransport, FallbackTransport, FallbackTransportConfig, Prettify, Transport } from 'viem'
 
 type OnResponseFn = (
   args: {
@@ -107,29 +99,7 @@ export const fallbackWithRank = <const transports extends readonly Transport[]>(
                 status: 'error',
               })
 
-              // If we've reached the end of the fallbacks, throw the error.
-              if (i === transports.length - 1) throw err
-
-              // Check if at least one other transport includes the method
-              includes ??= transports.slice(i + 1).some((transport) => {
-                const { include, exclude } = transport({ chain }).config.methods || {}
-                if (include) return include.includes(method)
-                if (exclude) return !exclude.includes(method)
-                return true
-              })
-              if (!includes) throw err
-
-              if (shouldReRank(err as Error)) {
-                rankTransports({
-                  chain,
-                  // eslint-disable-next-line no-return-assign
-                  onTransports: (transports_) => (transports = transports_ as transports),
-                  transports,
-                })
-              }
-
-              // Otherwise, try the next fallback.
-              return fetch(i + 1)
+              throw err
             }
           }
           return fetch()
@@ -174,13 +144,4 @@ export const rankTransports = ({
   }
 
   rankTransports_()
-}
-
-const shouldReRank = (error: Error) => {
-  // If the error is a rate limit error, we should re-rank the transports.
-  if (error instanceof HttpRequestError && error.status === 429) {
-    return true
-  }
-
-  return false
 }
