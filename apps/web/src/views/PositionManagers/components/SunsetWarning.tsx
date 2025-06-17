db2a1296548d771289abd80732ea7c37e91703e9
chef-ryan <ryan@pancakeswap.com>
2025-06-17 18:46:23 +0800

    add trans

--- apps/web/src/views/PositionManagers/components/SunsetWarning.tsx
+++ apps/web/src/views/PositionManagers/components/SunsetWarning.tsx
@@ -1,23 +1,27 @@
+import { useTranslation } from '@pancakeswap/localization'
 import { Link, Message, MessageText, Text } from '@pancakeswap/uikit'
 
 const SunsetWarning = () => {
+  const { t } = useTranslation()
   return (
     <Message variant="warning" m="16px 0">
       <MessageText>
         <Text mb="8px" small bold>
-          Position Managers Sunsetting — 21 June 2025, 08:00 UTC
+          {t('Position Managers Sunsetting — 21 June 2025, 08:00 UTC')}
         </Text>
-        Action required: Withdraw all positions before this time.
+        {t('Action required: Withdraw all positions before this time.')}
         <br />
-        Note: After the deadline, positions will no longer be actively managed and may fall out of range, which could
-        lead to impermanent loss.
-        <br />
-        <br />
-        Reminder: Users are responsible for managing and withdrawing their positions. PancakeSwap will not be able to
-        recover or prevent any losses from positions left unmanaged.
+        {t(
+          'Note: After the deadline, positions will no longer be actively managed and may fall out of range, which could lead to impermanent loss.',
+        )}
         <br />
         <br />
-        For more details, read the{' '}
+        {t(
+          'Reminder: Users are responsible for managing and withdrawing their positions. PancakeSwap will not be able to recover or prevent any losses from positions left unmanaged.',
+        )}
+        <br />
+        <br />
+        {t('For more details, read the')}{' '}
         <Link
           style={{
             display: 'inline',
@@ -26,9 +30,9 @@ const SunsetWarning = () => {
           external
           href="https://blog.pancakeswap.finance/articles/action-required-retirement-of-position-managers-bril-defiedge-teahouse-range-and-alpaca"
         >
-          official blog post
+          {t('official blog post')}
         </Link>{' '}
-        or view the{' '}
+        {t('or view the')}{' '}
         <Link
           external
           href="https://x.com/PancakeSwap/status/1925120544396963964"
@@ -37,7 +41,7 @@ const SunsetWarning = () => {
             fontSize: '14px',
           }}
         >
-          announcement on X
+          {t('announcement on X')}
         </Link>
         .
       </MessageText>
