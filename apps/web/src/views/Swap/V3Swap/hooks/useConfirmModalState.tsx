1e369924c29ec96be2fd1d1ca7e8b435db1ea4d4
Chef Philip <chef.philip@pancakeswap.com>
2025-06-24 07:56:15 -0400

    chore: update

--- apps/web/src/views/Swap/V3Swap/hooks/useConfirmModalState.tsx
+++ apps/web/src/views/Swap/V3Swap/hooks/useConfirmModalState.tsx
@@ -541,7 +541,7 @@ const useConfirmActions = (
                   value: order.trade.inputAmount.currency.isNative
                     ? BigInt(order.trade.inputAmount.quotient.toString())
                     : undefined,
-                  gas: calculateGasMargin(gasLimit, 2000n),
+                  gas: calculateGasMargin(gasLimit),
                 })
               })
 
