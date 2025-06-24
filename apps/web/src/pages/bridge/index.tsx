50f891722fc7b85005cc35e209b53782ec48170c
chef-ryan <ryan@pancakeswap.com>
2025-06-24 18:43:22 +0800

    move ssr -> csr

--- apps/web/src/pages/bridge/index.tsx
+++ apps/web/src/pages/bridge/index.tsx
@@ -1,41 +1,7 @@
-import { CanonicalBridge } from '@pancakeswap/canonical-bridge'
-import { Flex, useMatchBreakpoints } from '@pancakeswap/uikit'
-import ConnectWalletButton from 'components/ConnectWalletButton'
-import { PUBLIC_NODES } from 'config/nodes'
 import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
 import { NextPageWithLayout } from 'utils/page.types'
 import { CHAIN_IDS } from 'utils/wagmi'
-import Page from 'views/Page'
-
-const BridgeView = () => {
-  const { isMobile } = useMatchBreakpoints()
-
-  return (
-    <Page removePadding hideFooterOnDesktop={false} showExternalLink={false} showHelpLink={false} noMinHeight>
-      <Flex
-        width="100%"
-        height="100%"
-        justifyContent="center"
-        position="relative"
-        px={isMobile ? '16px' : '24px'}
-        pb={isMobile ? '14px' : '48px'}
-        pt={isMobile ? '24px' : '64px'}
-        alignItems="flex-start"
-        maxWidth="unset"
-      >
-        <Suspense>
-          <CanonicalBridge
-            connectWalletButton={<ConnectWalletButton width="100%" />}
-            supportedChainIds={CHAIN_IDS}
-            // @ts-ignore
-            rpcConfig={PUBLIC_NODES}
-          />
-        </Suspense>
-      </Flex>
-    </Page>
-  )
-}
+import { BridgeView } from 'views/BridgeView'
 
 const BridgePage = dynamic(() => Promise.resolve(BridgeView), {
   ssr: false,
