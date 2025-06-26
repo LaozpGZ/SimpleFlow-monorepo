8387d17111e1486367e61c5804c70a75542a5bd3
Chef Philip <chef.philip@pancakeswap.com>
2025-06-26 13:34:46 -0400

    chore: simplify the text

--- apps/web/src/views/SwapSimplify/InfinitySwap/SwapCommitButton.tsx
+++ apps/web/src/views/SwapSimplify/InfinitySwap/SwapCommitButton.tsx
@@ -402,11 +402,11 @@ const SwapCommitButtonInner = memo(function SwapCommitButtonInner({
     if (isRecipientError) return t('Invalid recipient')
     if (tradeError instanceof BridgeTradeError) {
       if (tradeError.message.includes("doesn't have enough funds to support this deposit")) {
-        return t('Requested amount exceeds the available bridge liquidity. Please try again with a lower amount!')
+        return t('Retry with lower input amount!')
       }
 
       if (tradeError.message.includes('too low relative to fees')) {
-        return t('Input amount less than min. limit. Please try again with higher amount')
+        return t('Retry with higher input amount!')
       }
 
       return tradeError.message
