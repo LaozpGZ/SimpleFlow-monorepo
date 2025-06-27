9f130d29024100860fdcef47f8598caa740ee5e9
memoyil <2213635+memoyil@users.noreply.github.com>
2025-06-27 09:11:37 +0200

    fix: Gamification public

--- apps/gamification/config/nodes.ts
+++ apps/gamification/config/nodes.ts
@@ -106,7 +106,11 @@ export const PUBLIC_NODES = {
     'https://bsc-dataseed1.defibit.io',
     'https://bsc-dataseed1.bnbchain.org',
   ].filter(Boolean),
-  [ChainId.BSC_TESTNET]: ['https://data-seed-prebsc-1-s1.bnbchain.org:8545'],
+  [ChainId.BSC_TESTNET]: [
+    'https://bsc-testnet-dataseed.bnbchain.org',
+    'https://bsc-testnet.bnbchain.org',
+    'https://bsc-prebsc-dataseed.bnbchain.org',
+  ],
   [ChainId.ETHEREUM]: [
     getNodeRealUrl(ChainId.ETHEREUM, process.env.NEXT_PUBLIC_NODE_REAL_API_ETH) || '',
     process.env.NEXT_PUBLIC_NODIES_ETH || '',
