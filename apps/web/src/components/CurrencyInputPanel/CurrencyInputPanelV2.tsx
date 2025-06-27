77e5aadbd377cb7b2d5c91f14d2351bc68ecf2f7
chef-ryan <ryan@pancakeswap.com>
2025-06-27 19:20:24 +0800

    remove hide price impact check

--- apps/web/src/components/CurrencyInputPanel/CurrencyInputPanelV2.tsx
+++ apps/web/src/components/CurrencyInputPanel/CurrencyInputPanelV2.tsx
@@ -111,7 +111,7 @@ const CurrencyInputPanelV2 = memo(function CurrencyInputPanelV2({
     showUSDPrice ? currency ?? undefined : undefined,
     value !== undefined && Number.isFinite(+value) ? +value : undefined,
     {
-      hideIfPriceImpactTooHigh: true,
+      enabled: Boolean(value !== undefined && Number.isFinite(+value)),
     },
   )
 
