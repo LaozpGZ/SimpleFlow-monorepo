f5dedf0a698dcde8ace3799f65fbcf942f0bef68
ChefEric <173023571+chef-eric@users.noreply.github.com>
2025-06-27 15:57:45 +0800

    fix: add SentryErrorBoundary

--- apps/solana/src/pages/_app.tsx
+++ apps/solana/src/pages/_app.tsx
@@ -1,27 +1,31 @@
-import { ResetCSS } from '@pancakeswap/uikit'
-import Decimal from 'decimal.js'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import Script from 'next/script'
-import { useRouter } from 'next/router'
-import { useMemo } from 'react'
-
 import '@/components/LandingPage/components/tvl.css'
 import '@/components/LandingPage/liquidity.css'
 import '@/components/Toast/toast.css'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import 'react-day-picker/dist/style.css'
+
+import { Fragment, useMemo } from 'react'
+
+import Decimal from 'decimal.js'
 import { DefaultSeo } from 'next-seo'
 import { SEO } from 'next-seo.config'
-import 'react-day-picker/dist/style.css'
-import { Providers } from '@/provider'
-import Content from '@/components/Content'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { GoogleAnalytics } from '@next/third-parties/google'
+import { ResetCSS } from '@pancakeswap/uikit'
+
 import AppNavLayout from '@/components/AppLayout/AppNavLayout'
+import Content from '@/components/Content'
+import { SentryErrorBoundary } from '@/components/SentryErrorBoundary'
 import useThemeSync from '@/hooks/useThemeSync'
+import { Providers } from '@/provider'
 import { pageRoutePathnames } from '@/utils/config/routers'
 
 const CONTENT_ONLY_PATH = ['/', '404', '/moonpay']
 const OVERFLOW_HIDDEN_PATH = [pageRoutePathnames.pools, pageRoutePathnames.swap, pageRoutePathnames['legacy-swap']]
 const FULL_SIZE_PATH = [pageRoutePathnames.swap, pageRoutePathnames['legacy-swap']]
+const ProductionErrorBoundary = process.env.NODE_ENV === 'production' ? SentryErrorBoundary : Fragment
 
 Decimal.set({ precision: 1e3 })
 
@@ -62,17 +66,19 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       ) : null}
       <DefaultSeo {...SEO} />
       <Providers>
-        <Content {...props}>
-          <ResetCSS />
-          <GlobalHooks />
-          {onlyContent ? (
-            <Component {...pageProps} />
-          ) : (
-            <AppNavLayout overflowHidden={overflowHidden} fullSize={fullSize}>
+        <ProductionErrorBoundary>
+          <Content {...props}>
+            <ResetCSS />
+            <GlobalHooks />
+            {onlyContent ? (
               <Component {...pageProps} />
-            </AppNavLayout>
-          )}
-        </Content>
+            ) : (
+              <AppNavLayout overflowHidden={overflowHidden} fullSize={fullSize}>
+                <Component {...pageProps} />
+              </AppNavLayout>
+            )}
+          </Content>
+        </ProductionErrorBoundary>
       </Providers>
     </>
   )
