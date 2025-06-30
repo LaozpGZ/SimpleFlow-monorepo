565ed5bab80d0b6284fcf4181b9c8eb341ac4a5c
chef-ryan <ryan@pancakeswap.com>
2025-06-30 15:58:53 +0800

    resolve comments

--- apps/web/src/state/teams/helpers.ts
+++ apps/web/src/state/teams/helpers.ts
@@ -35,44 +35,40 @@ export const getTeam = async (teamId: number): Promise<Team | null> => {
  * Gets on-chain data and merges it with the existing static list of teams
  */
 export const getTeams = async (): Promise<TeamsById | null> => {
-  try {
-    const profileContract = getProfileContract()
-    const teamsById = fromPairs(teamsList.map((team) => [team.id, team]))
-    const nbTeams = await profileContract.read.numberTeams()
+  const profileContract = getProfileContract()
+  const teamsById = fromPairs(teamsList.map((team) => [team.id, team]))
+  const nbTeams = await profileContract.read.numberTeams()
 
-    const calls = Array.from({ length: Number(nbTeams) }).map(
-      (_, i) =>
-        ({
-          abi: pancakeProfileABI,
-          address: getPancakeProfileAddress(),
-          functionName: 'getTeamProfile',
-          args: [BigInt(i + 1)] as const,
-        } as const),
-    )
-    const client = publicClient({ chainId: ChainId.BSC })
-    const teamData = await client.multicall({
-      contracts: calls,
-      allowFailure: false,
-    })
+  const calls = Array.from({ length: Number(nbTeams) }).map(
+    (_, i) =>
+      ({
+        abi: pancakeProfileABI,
+        address: getPancakeProfileAddress(),
+        functionName: 'getTeamProfile',
+        args: [BigInt(i + 1)] as const,
+      } as const),
+  )
+  const client = publicClient({ chainId: ChainId.BSC })
+  const teamData = await client.multicall({
+    contracts: calls,
+    allowFailure: false,
+  })
 
-    const onChainTeamData = fromPairs(
-      teamData.map((team, index) => {
-        const { 0: teamName, 2: numberUsers, 3: numberPoints, 4: isJoinable } = team
+  const onChainTeamData = fromPairs(
+    teamData.map((team, index) => {
+      const { 0: teamName, 2: numberUsers, 3: numberPoints, 4: isJoinable } = team
 
-        return [
-          index + 1,
-          {
-            name: teamName,
-            users: Number(numberUsers),
-            points: Number(numberPoints),
-            isJoinable,
-          },
-        ]
-      }),
-    )
+      return [
+        index + 1,
+        {
+          name: teamName,
+          users: Number(numberUsers),
+          points: Number(numberPoints),
+          isJoinable,
+        },
+      ]
+    }),
+  )
 
-    return merge({}, teamsById, onChainTeamData)
-  } catch (error) {
-    return null
-  }
+  return merge({}, teamsById, onChainTeamData)
 }
--- apps/web/src/views/Teams/atom/teamsAtom.ts
+++ apps/web/src/views/Teams/atom/teamsAtom.ts
@@ -1,11 +1,11 @@
-import { atomWithAsyncRetry } from 'utils/atomWithAsyncRetry'
 import { getTeams } from 'state/teams/helpers'
 import { TeamsById } from 'state/types'
+import { atomWithAsyncRetry } from 'utils/atomWithAsyncRetry'
 
 export const teamsAtom = atomWithAsyncRetry<TeamsById | null>({
   asyncFn: async () => {
     const data = await getTeams()
     return data ?? null
   },
-  fallbackValue: null,
+  fallbackValue: {},
 })
