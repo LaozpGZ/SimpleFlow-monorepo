75445b6cd6152ffd1f89fb27730e784fdf423b04
memoyil <2213635+memoyil@users.noreply.github.com>
2025-07-01 12:15:56 +0200

    fix: Isloading

--- apps/web/src/hooks/useCurrencyUsdPrice.ts
+++ apps/web/src/hooks/useCurrencyUsdPrice.ts
@@ -16,8 +16,11 @@ export function useCurrencyUsdPrice(
 ): { data: number } {
   const price = useStablecoinPrice(currency, { enabled })
   return useMemo(() => {
-    if (!price) return { data: 0 }
-    return { data: parseFloat(price.greaterThan(1) ? price.toSignificant(6) : price.toSignificant(9)) }
+    if (!price) return { data: 0, isLoading: true }
+    return {
+      data: parseFloat(price.greaterThan(1) ? price.toSignificant(6) : price.toSignificant(9)),
+      isLoading: false,
+    }
   }, [price])
 }
 
