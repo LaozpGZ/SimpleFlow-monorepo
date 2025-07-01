dc9ef827c586734b00ca14af804d87a5f4cefbfc
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-07-02 03:08:47 +0800

    chore: update packages

--- apps/web/src/contexts/privy/wagmi/config.ts
+++ apps/web/src/contexts/privy/wagmi/config.ts
@@ -1,14 +1,14 @@
-import { createConfig } from '@privy-io/wagmi'
-import { bsc, bscTestnet, sepolia } from 'viem/chains'
-import { http } from 'wagmi'
+// import { createConfig } from '@privy-io/wagmi'
+// import { bsc, bscTestnet, sepolia } from 'viem/chains'
+// import { http } from 'wagmi'
 
-export const supportedChains = [sepolia, bscTestnet, bsc] as const
+// export const supportedChains = [sepolia, bscTestnet, bsc] as const
 
-export const config = createConfig({
-  chains: supportedChains,
-  transports: {
-    [bscTestnet.id]: http(),
-    [sepolia.id]: http(),
-    [bsc.id]: http(),
-  },
-})
+// export const config = createConfig({
+//   chains: supportedChains,
+//   transports: {
+//     [bscTestnet.id]: http(),
+//     [sepolia.id]: http(),
+//     [bsc.id]: http(),
+//   },
+// })
--- apps/web/src/contexts/privy/wagmi/provider.tsx
+++ apps/web/src/contexts/privy/wagmi/provider.tsx
@@ -1,6 +1,6 @@
-import { WagmiProvider as Provider } from '@privy-io/wagmi'
+// import { WagmiProvider as Provider } from '@privy-io/wagmi'
 import { PropsWithChildren } from 'react'
-import { type WagmiProviderProps } from 'wagmi'
+import { type WagmiProviderProps, WagmiProvider as Provider } from 'wagmi'
 
 export function WagmiProvider({ children, ...props }: PropsWithChildren<WagmiProviderProps>) {
   return <Provider {...props}>{children}</Provider>
