b73b551d151d3a12135fb72c13ba0b8aaabf5daa
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-07-03 14:07:52 +0800

    chore: set providers

---
+++ apps/web/src/contexts/Privy/firebase.tsx
@@ -0,0 +1,91 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup, UserCredential } from 'firebase/auth'
+import { createContext, ReactNode, useCallback, useContext, useEffect, useState } from 'react'
+
+import { firebaseApp } from './constants'
+
+// Define the context type
+interface AuthContextType {
+  token: string | undefined
+  getToken: () => Promise<string | undefined>
+  isLoading: boolean
+  login: () => Promise<void>
+}
+
+// Create the context
+const AuthContext = createContext<AuthContextType | undefined>(undefined)
+
+// Provider component
+interface AuthProviderProps {
+  children: ReactNode
+}
+
+export function FirebaseAuthProvider({ children }: AuthProviderProps) {
+  const [isLoading, setLoading] = useState(false)
+  const [token, setToken] = useState<string | undefined>()
+
+  const signInWithGoogle = async (): Promise<UserCredential> => {
+    try {
+      const auth = getAuth(firebaseApp)
+      const googleProvider = new GoogleAuthProvider()
+      const res = await signInWithPopup(auth, googleProvider)
+      return res
+    } catch (err) {
+      alert(err)
+      throw err
+    }
+  }
+
+  const login = async () => {
+    try {
+      setLoading(true)
+      const loginRes = await signInWithGoogle()
+      const idToken = await loginRes.user.getIdToken(true)
+      setToken(idToken)
+    } catch (err) {
+      console.error(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const getToken = useCallback(async () => {
+    if (token) {
+      return token
+    }
+    const auth = getAuth(firebaseApp)
+    if (!auth.currentUser) {
+      return undefined
+    }
+    const idToken = await auth.currentUser.getIdToken(true)
+    setToken(idToken)
+    return idToken
+  }, [token])
+
+  useEffect(() => {
+    const auth = getAuth(firebaseApp)
+    auth.onAuthStateChanged((user) => {
+      console.log('Auth state changed')
+    })
+    auth.onIdTokenChanged((user) => {
+      console.log('Auth on change', user)
+    })
+  }, [])
+
+  const value = {
+    token,
+    isLoading,
+    getToken,
+    login,
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+
+// Custom hook to use the auth context
+export function useFirebaseAuth() {
+  const context = useContext(AuthContext)
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
---
+++ apps/web/src/contexts/Privy/privy.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { PrivyProvider as Provider } from '@privy-io/react-auth'
+import { SmartWalletsProvider } from '@privy-io/react-auth/smart-wallets'
+import { PropsWithChildren } from 'react'
+
+import { CHAINS } from 'config/chains'
+import { useFirebaseAuth } from './firebase'
+
+export function PrivyProvider({ children }: PropsWithChildren) {
+  const { isLoading, getToken } = useFirebaseAuth()
+
+  return (
+    <Provider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID ?? 'z'}
+      clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID ?? ''}
+      config={{
+        defaultChain: CHAINS[0],
+        customAuth: {
+          isLoading,
+          getCustomAccessToken: getToken,
+        },
+        supportedChains: CHAINS,
+        appearance: {
+          accentColor: '#6A6FF5',
+          theme: '#222224',
+          showWalletLoginFirst: false,
+          logo: 'https://auth.privy.io/logos/privy-logo-dark.png',
+          walletChainType: 'ethereum-only',
+          walletList: ['detected_wallets', 'metamask'],
+        },
+        fundingMethodConfig: {
+          moonpay: {
+            useSandbox: true,
+          },
+        },
+        embeddedWallets: {
+          requireUserPasswordOnCreate: true,
+          showWalletUIs: true,
+          ethereum: {
+            createOnLogin: 'users-without-wallets',
+          },
+          solana: {
+            createOnLogin: 'off',
+          },
+        },
+        mfa: {
+          noPromptOnMfaRequired: false,
+        },
+        externalWallets: {},
+      }}
+    >
+      <SmartWalletsProvider>{children}</SmartWalletsProvider>
+    </Provider>
+  )
+}
