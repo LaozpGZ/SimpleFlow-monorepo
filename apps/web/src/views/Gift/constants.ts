5fd0cbc6933d0ade199623218b0860819b1e2046
Chef Philip <chef.philip@pancakeswap.com>
2025-07-05 15:24:56 -0400

    chore: update latest contract address

--- apps/web/src/views/Gift/constants.ts
+++ apps/web/src/views/Gift/constants.ts
@@ -1,5 +1,5 @@
 // bsc testnet
-export const GIFT_PANCAKE_V1_ADDRESS = '0x51495A68bC42e5810eD7dCa4Ea4E6f6c93171593'
+export const GIFT_PANCAKE_V1_ADDRESS = '0x7E17FcBE255f02848fEE2c856a1Ab56F98E31c9C'
 
 // @eslint-disable-next-line
 export const NEXT_PUBLIC_GIFT_API = process.env.NEXT_PUBLIC_GIFT_API
