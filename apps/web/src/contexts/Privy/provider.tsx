e76024901e2827253bef52c74fe2ccea5ae3ade0
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-07-07 13:52:34 +0800

    chore: fix discord  issue

--- apps/web/src/contexts/Privy/provider.tsx
+++ apps/web/src/contexts/Privy/provider.tsx
@@ -1,14 +1,13 @@
-import { usePrivy } from '@privy-io/react-auth'
 import { WagmiProvider as Provider } from '@privy-io/wagmi'
 import { PropsWithChildren } from 'react'
-import { type WagmiProviderProps, WagmiProvider } from 'wagmi'
+import { type WagmiProviderProps } from 'wagmi'
 
 export function WagmiWithPrivyProvider({ children, ...props }: PropsWithChildren<WagmiProviderProps>) {
-  const { authenticated, ready } = usePrivy()
+  // const { authenticated, ready } = usePrivy()
 
-  if (ready && authenticated) {
-    return <Provider {...props}>{children}</Provider>
-  }
+  // if ((ready && authenticated) || true) {
+  return <Provider {...props}>{children}</Provider>
+  // }
 
-  return <WagmiProvider {...props}>{children}</WagmiProvider>
+  // return <WagmiProvider {...props}>{children}</WagmiProvider>
 }
