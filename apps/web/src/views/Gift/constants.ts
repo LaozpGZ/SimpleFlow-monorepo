ca8668130113b57349b528f33bcf43a8f8d4b4a1
Chef Philip <chef.philip@pancakeswap.com>
2025-07-08 07:32:21 -0400

    chore: integrate mainnet

--- apps/web/src/views/Gift/constants.ts
+++ apps/web/src/views/Gift/constants.ts
@@ -2,8 +2,7 @@
 
 import { ChainId } from '@pancakeswap/chains'
 
-// 0x68e5f51980e2acbb9817e34f3a2db71f5ce2ece3
-export const GIFT_PANCAKE_V1_ADDRESS = '0x7E17FcBE255f02848fEE2c856a1Ab56F98E31c9C'
+export const GIFT_PANCAKE_V1_ADDRESS = '0x68E5f51980e2AcbB9817E34f3A2db71F5Ce2ecE3'
 
 // @ts-ignore
 export const NEXT_PUBLIC_GIFT_API = process.env.NEXT_PUBLIC_GIFT_API
