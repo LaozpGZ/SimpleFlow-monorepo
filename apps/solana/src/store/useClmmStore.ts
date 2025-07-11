f2bc4c092d80894138b4492e5492d28e8077e3a5
Chef Jerry <jerry@pancakeswap.com>
2025-07-11 19:56:15 +0800

    chore: revert

--- apps/solana/src/store/useClmmStore.ts
+++ apps/solana/src/store/useClmmStore.ts
@@ -274,22 +274,22 @@ export const useClmmStore = createStore<ClmmState>(
 
         buildData
           .execute({
-            sequentially: false,
-            simulate: true
-            // onTxUpdate: (data) => {
-            //   handleMultiTxRetry(data)
-            //   handleMultiTxToast({
-            //     t,
-            //     toastId,
-            //     processedId: transformProcessData({ processedId, data }),
-            //     txLength,
-            //     meta,
-            //     handler,
-            //     getSubTxTitle
-            //   })
-            // }
+            sequentially: true,
+            simulate: true,
+            onTxUpdate: (data) => {
+              handleMultiTxRetry(data)
+              handleMultiTxToast({
+                t,
+                toastId,
+                processedId: transformProcessData({ processedId, data }),
+                txLength,
+                meta,
+                handler,
+                getSubTxTitle
+              })
+            }
           })
-          .then(({ txIds }) => {
+          .then(() => {
             handleMultiTxToast({
               t,
               toastId,
