264df73db3f4fb137efec3c3dc9c1b2fec6426ca
Chef Philip <chef.philip@pancakeswap.com>
2025-07-13 11:26:21 -0400

    chore: add share gift link

--- apps/web/src/views/Gift/components/SendLinkView.tsx
+++ apps/web/src/views/Gift/components/SendLinkView.tsx
@@ -1,11 +1,12 @@
 import { useTranslation } from '@pancakeswap/localization'
 import { CurrencyAmount, NativeCurrency, Token } from '@pancakeswap/sdk'
-import { copyText, useToast } from '@pancakeswap/uikit'
+import { copyText, FlexGap, useToast } from '@pancakeswap/uikit'
 import { NoteContainer } from 'components/NoteContainer'
 import { ActionButton } from 'components/WalletModalV2/ActionButton'
 import { formatDollarAmount } from 'views/V3Info/utils/numbers'
 import { useCalculateTotalCostCreateGift } from '../hooks/useCalculateTotalCostCreateGift'
 import { generateClaimLink } from '../utils/generateClaimLink'
+import { ShareGiftLinkButton } from './ShareGiftLinkButton'
 
 export function SendLinkView({
   tokenAmount,
@@ -22,27 +23,34 @@ export function SendLinkView({
 
   const totalUsd = useCalculateTotalCostCreateGift({ tokenAmount, nativeAmount })
 
+  const giftShareText = t(
+    `Just sent you ${formatDollarAmount(totalUsd)}! 🎉 Tap this link and to claim it: 👉 ${claimLink}.
+
+Please connect your wallet to claim it!
+
+Alternatively, you can manually enter the code ${code} on PancakeSwap wallet to claim.
+  `,
+  )
+
   return (
     <>
       <NoteContainer mb="16px" p="8px">
-        {t(`Just sent you ${formatDollarAmount(totalUsd)}! 🎉 Tap this link and to claim it: 👉 ${claimLink}`)}
-        <br />
-        <br />
-        {t(`Please connect your wallet to claim it!`)}
-        <br />
-        <br />
-        {t(`Alternatively, you can manually enter the code ${code} on PancakeSwap wallet to claim.`)}
+        <pre style={{ textWrap: 'unset' }}>{giftShareText}</pre>
       </NoteContainer>
 
-      <ActionButton
-        variant="danger"
-        onClick={() => {
-          copyText(claimLink)
-          toastSuccess(t('Claim code'), t('Copied!'))
-        }}
-      >
-        {t('Copy')}
-      </ActionButton>
+      <FlexGap flexDirection="row" gap="8px">
+        <ShareGiftLinkButton text={giftShareText} claimLink={claimLink} />
+
+        <ActionButton
+          variant="danger"
+          onClick={() => {
+            copyText(giftShareText)
+            toastSuccess(t('Claim code'), t('Copied!'))
+          }}
+        >
+          {t('Copy')}
+        </ActionButton>
+      </FlexGap>
     </>
   )
 }
