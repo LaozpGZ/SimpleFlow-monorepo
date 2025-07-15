5196a7ab4548db57cda295f87ea140f24ba71206
Chef Philip <chef.philip@pancakeswap.com>
2025-07-14 21:15:07 -0400

    chore: update logic

--- apps/web/src/views/Gift/components/QRShareImageButton.tsx
+++ apps/web/src/views/Gift/components/QRShareImageButton.tsx
@@ -14,28 +14,29 @@ const QRShareImageButton: React.FC<QRShareImageButtonProps> = ({ elementId, code
   const { toastSuccess } = useToast()
   const { convertToCanvasWithLoading, isLoading } = useElementToCanvas()
 
-  const handleLinkFallback = useCallback(() => {
-    const claimLink = generateClaimLink({ code })
-    const shareText = t('Scan the QR code to claim your gift! %link%', { link: claimLink })
-
-    if (navigator.share) {
-      navigator
-        .share({
-          title: t('Gift from PancakeSwap!'),
-          text: shareText,
-          url: claimLink,
-        })
-        .catch(() => {
-          copyText(claimLink)
-          toastSuccess(t('Link copied'), t('Share link copied to clipboard!'))
-        })
-    } else {
-      copyText(claimLink)
-      toastSuccess(t('Link copied'), t('Share link copied to clipboard!'))
-    }
-  }, [code, t, toastSuccess])
-
   const shareImage = useCallback(async () => {
+    const claimLink = generateClaimLink({ code })
+
+    function handleLinkFallback() {
+      const shareText = t('Scan the QR code to claim your gift! %link%', { link: claimLink })
+
+      if (navigator.share) {
+        navigator
+          .share({
+            title: t('Gift from PancakeSwap!'),
+            text: shareText,
+            url: claimLink,
+          })
+          .catch(() => {
+            copyText(claimLink)
+            toastSuccess(t('Link copied'), t('Share link copied to clipboard!'))
+          })
+      } else {
+        copyText(claimLink)
+        toastSuccess(t('Link copied'), t('Share link copied to clipboard!'))
+      }
+    }
+
     const blob = await convertToCanvasWithLoading(elementId)
 
     if (blob) {
@@ -60,7 +61,7 @@ const QRShareImageButton: React.FC<QRShareImageButtonProps> = ({ elementId, code
     } else {
       handleLinkFallback()
     }
-  }, [elementId, code, handleLinkFallback, t])
+  }, [elementId, code, t])
 
   return (
     <Button onClick={shareImage} variant="secondary" width="100%" disabled={isLoading} isLoading={isLoading}>
