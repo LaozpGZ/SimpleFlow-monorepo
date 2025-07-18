3118c06d6c9b1f653091fe84d19846f64aa002ab
Chef Jerry <jerry@pancakeswap.com>
2025-07-18 21:03:27 +0800

    feat: locale selector

--- packages/uikit/src/widgets/SimpleMenu/components/LocaleSelector.tsx
+++ packages/uikit/src/widgets/SimpleMenu/components/LocaleSelector.tsx
@@ -1,19 +1,25 @@
-import { Language } from "@pancakeswap/localization";
+import { Language, languageList, useTranslation } from "@pancakeswap/localization";
 import { Box, LangSelector } from "../../../components";
 
 export type LocaleSelectorProps = {
-  currentLang: string;
-  langs: Language[];
-  setLang: (lang: Language) => void;
+  currentLang?: string;
+  langs?: Language[];
+  setLang?: (lang: Language) => void;
 };
 
 export const LocaleSelector: React.FC<LocaleSelectorProps> = ({ currentLang, langs, setLang }) => {
+  const { currentLanguage, setLanguage } = useTranslation();
+  const handleLanguageChange = (language: Language) => {
+    setLanguage(language);
+    setLang?.(language);
+  };
+
   return (
     <Box mt="4px">
       <LangSelector
-        currentLang={currentLang}
-        langs={langs}
-        setLang={setLang}
+        currentLang={currentLang ?? currentLanguage?.code}
+        langs={langs ?? languageList}
+        setLang={handleLanguageChange}
         buttonScale="xs"
         color="textSubtle"
         hideLanguage
