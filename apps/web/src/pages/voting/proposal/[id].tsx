4ba473f2c54091074e6ff4817e8bd1acd51d1d57
chef-ryan <ryan@pancakeswap.com>
2025-07-18 15:39:47 +0800

    fix

--- apps/web/src/pages/voting/proposal/[id].tsx
+++ apps/web/src/pages/voting/proposal/[id].tsx
@@ -1,9 +1,5 @@
 import dynamic from 'next/dynamic'
-import { NextSeo } from 'next-seo'
-import { useRouter } from 'next/router'
-import { useQuery } from '@tanstack/react-query'
 import { NextPageWithLayout } from 'utils/page.types'
-import { getProposal } from 'state/voting/helpers'
 import Overview from 'views/Voting/Proposal/Overview'
 
 const ProposalView = () => {
