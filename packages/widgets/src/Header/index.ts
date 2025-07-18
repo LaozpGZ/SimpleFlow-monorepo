64506b35038806a657ac959bfe9ec4d8ef2a43aa
Chef Jerry <jerry@pancakeswap.com>
2025-07-18 17:08:07 +0800

    feat: navigation

--- packages/widgets/src/Header/index.ts
+++ packages/widgets/src/Header/index.ts
@@ -1,16 +1,17 @@
-import { AnnouncementBanner, LocaleSelector, Navigation } from '@pancakeswap/uikit'
+import { AnnouncementBanner, LocaleSelector } from '@pancakeswap/uikit'
 import { PancakeSwapHeader } from './PancakeSwapHeader'
+import { PancakeSwapNavigation } from './components/PancakeSwapNavigation'
 
 PancakeSwapHeader.displayName = 'PancakeSwapHeader'
 
 export type PancakeSwapHeaderComponentType = typeof PancakeSwapHeader & {
   AnnouncementBanner: typeof AnnouncementBanner
   LocaleSelector: typeof LocaleSelector
-  Navigation: typeof Navigation
+  Navigation: typeof PancakeSwapNavigation
 }
 ;(PancakeSwapHeader as PancakeSwapHeaderComponentType).AnnouncementBanner = AnnouncementBanner
 ;(PancakeSwapHeader as PancakeSwapHeaderComponentType).LocaleSelector = LocaleSelector
-;(PancakeSwapHeader as PancakeSwapHeaderComponentType).Navigation = Navigation
+;(PancakeSwapHeader as PancakeSwapHeaderComponentType).Navigation = PancakeSwapNavigation
 
 export type { AnnouncementBannerProps, LocaleSelectorProps, NavigationProps } from '@pancakeswap/uikit'
 export type { PancakeSwapHeaderProps } from './PancakeSwapHeader'
