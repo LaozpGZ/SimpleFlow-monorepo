4f4d4b08d5a9a4b080d5a2315051d18dee8d6a00
Chef Philip <chef.philip@pancakeswap.com>
2025-07-21 09:34:57 -0400

    chore: only support crosschain for bsc chain

--- apps/web/src/quoter/utils/crosschain-utils/utils/ContextBuilder.ts
+++ apps/web/src/quoter/utils/crosschain-utils/utils/ContextBuilder.ts
@@ -12,6 +12,7 @@ import { availableBridgeRoutesAtom } from '../../../atom/availableBridgeRoutesAt
 import { bestSameChainWithoutPlaceHolderAtom } from '../../../atom/bestSameChainAtom'
 import { bridgeOnlyQuoteAtom } from '../../../atom/bridgeOnlyQuoteAtom'
 import { type BridgeQuoteParams, type QuoteContext } from '../types'
+import { CROSSCHAIN_INFINITY_SWAP_SUPPORTED_CHAINS } from '../config'
 
 interface AtomGetterFunction {
   <T>(atom: any): T
@@ -91,7 +92,11 @@ export class ContextBuilder {
           }
 
           // Only support swap to bridge for bsc chain
-          if (customSwapOption.infinitySwap && customSwapOption.baseCurrency?.chainId !== ChainId.BSC) {
+          if (
+            customSwapOption.infinitySwap &&
+            customSwapOption.baseCurrency?.chainId &&
+            !CROSSCHAIN_INFINITY_SWAP_SUPPORTED_CHAINS.includes(customSwapOption.baseCurrency?.chainId)
+          ) {
             customSwapOption.infinitySwap = false
           }
 
