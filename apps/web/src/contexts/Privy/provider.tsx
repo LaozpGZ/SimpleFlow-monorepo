5699e667a1a9c2246386191351570faa3356ecc3
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-07-22 16:43:05 +0800

    chore: fix issue

--- apps/web/src/contexts/Privy/provider.tsx
+++ apps/web/src/contexts/Privy/provider.tsx
@@ -2,7 +2,7 @@ import { usePrivy } from '@privy-io/react-auth'
 import { WagmiProvider as PrivyWagmiProvider } from '@privy-io/wagmi'
 import { useAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
-import { PropsWithChildren, useEffect } from 'react'
+import { PropsWithChildren, useCallback, useEffect, useRef } from 'react'
 import { type WagmiProviderProps } from 'wagmi'
 
 const lastWalletRecoveryAtom = atomWithStorage('lastWalletRecovery', 0)
@@ -10,19 +10,9 @@ const lastWalletRecoveryAtom = atomWithStorage('lastWalletRecovery', 0)
 export function WagmiWithPrivyProvider({ children, ...props }: PropsWithChildren<WagmiProviderProps>) {
   const { authenticated, ready, user, createWallet, setWalletRecovery, enrollInMfa } = usePrivy()
   const [lastRecovery, setLastRecovery] = useAtom(lastWalletRecoveryAtom)
-  // const attemptedWalletCreation = useRef(false)
+  const attemptedWalletCreation = useRef(false)
 
-  // useEffect(() => {
-  //   const createWalletWithUserManagedRecovery = async () => {
-  //     if (ready && authenticated && !user?.wallet && !attemptedWalletCreation.current) {
-  //       attemptedWalletCreation.current = true
-  //       await createWallet()
-  //     }
-  //   }
-  //   createWalletWithUserManagedRecovery()
-  // }, [ready, user, authenticated, createWallet])
-
-  useEffect(() => {
+  const handleWalletRecovery = useCallback(() => {
     if (authenticated && ready && user?.wallet?.recoveryMethod === 'privy' && user?.smartWallet) {
       const now = Date.now()
       const oneWeek = 7 * 24 * 60 * 60 * 1000
@@ -33,6 +23,17 @@ export function WagmiWithPrivyProvider({ children, ...props }: PropsWithChildren
       }
     }
   }, [ready, user, authenticated, lastRecovery, setWalletRecovery])
+
+  useEffect(() => {
+    const createWalletWithUserManagedRecovery = async () => {
+      if (ready && authenticated && !user?.wallet && !attemptedWalletCreation.current) {
+        attemptedWalletCreation.current = true
+        await createWallet()
+        handleWalletRecovery()
+      }
+    }
+    createWalletWithUserManagedRecovery()
+  }, [ready, user, authenticated, createWallet])
 
   return <PrivyWagmiProvider {...props}>{children}</PrivyWagmiProvider>
 }
