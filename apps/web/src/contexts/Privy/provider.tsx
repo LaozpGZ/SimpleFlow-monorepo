5245e6b24ebb63638e400de985d6a6ea85872d86
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-07-22 15:21:12 +0800

    chore: shouldn't create wallet by ourself

--- apps/web/src/contexts/Privy/provider.tsx
+++ apps/web/src/contexts/Privy/provider.tsx
@@ -2,7 +2,7 @@ import { usePrivy } from '@privy-io/react-auth'
 import { WagmiProvider as PrivyWagmiProvider } from '@privy-io/wagmi'
 import { useAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
-import { PropsWithChildren, useEffect, useRef } from 'react'
+import { PropsWithChildren, useEffect } from 'react'
 import { type WagmiProviderProps } from 'wagmi'
 
 const lastWalletRecoveryAtom = atomWithStorage('lastWalletRecovery', 0)
@@ -10,20 +10,20 @@ const lastWalletRecoveryAtom = atomWithStorage('lastWalletRecovery', 0)
 export function WagmiWithPrivyProvider({ children, ...props }: PropsWithChildren<WagmiProviderProps>) {
   const { authenticated, ready, user, createWallet, setWalletRecovery, enrollInMfa } = usePrivy()
   const [lastRecovery, setLastRecovery] = useAtom(lastWalletRecoveryAtom)
-  const attemptedWalletCreation = useRef(false)
+  // const attemptedWalletCreation = useRef(false)
+
+  // useEffect(() => {
+  //   const createWalletWithUserManagedRecovery = async () => {
+  //     if (ready && authenticated && !user?.wallet && !attemptedWalletCreation.current) {
+  //       attemptedWalletCreation.current = true
+  //       await createWallet()
+  //     }
+  //   }
+  //   createWalletWithUserManagedRecovery()
+  // }, [ready, user, authenticated, createWallet])
 
   useEffect(() => {
-    const createWalletWithUserManagedRecovery = async () => {
-      if (ready && authenticated && !user?.wallet && !attemptedWalletCreation.current) {
-        attemptedWalletCreation.current = true
-        await createWallet()
-      }
-    }
-    createWalletWithUserManagedRecovery()
-  }, [ready, user, authenticated, createWallet])
-
-  useEffect(() => {
-    if (authenticated && ready && user?.wallet?.recoveryMethod === 'privy') {
+    if (authenticated && ready && user?.wallet?.recoveryMethod === 'privy' && user?.smartWallet) {
       const now = Date.now()
       const oneWeek = 7 * 24 * 60 * 60 * 1000
 
