d1f6fe7396192cff6f714a0764e18e97474c094c
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-07-24 08:45:34 +0800

    chore: switch to firebase prod

--- apps/web/src/contexts/Privy/constants.ts
+++ apps/web/src/contexts/Privy/constants.ts
@@ -2,10 +2,10 @@ import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '',
-  authDomain: 'pancakeswap-dev-firebase.firebaseapp.com',
-  projectId: 'pancakeswap-dev-firebase',
-  storageBucket: 'pancakeswap-dev-firebase.firebasestorage.app',
-  messagingSenderId: '389585225139',
+  authDomain: 'pancakeswap-prod-firebase.firebaseapp.com',
+  projectId: 'pancakeswap-prod-firebase',
+  storageBucket: 'pancakeswap-prod-firebase.firebasestorage.app',
+  messagingSenderId: '901250967709',
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? '',
 }
 
