ecce7e5008e106404d675a5043568b8371094440
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-07-24 11:11:11 +0800

    chore: update env

--- apps/web/src/pages/api/auth/discord-callback.ts
+++ apps/web/src/pages/api/auth/discord-callback.ts
@@ -14,10 +14,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // 1. Exchange authorization code for access token
     const params = new URLSearchParams({
       client_id: process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID!,
-      client_secret: process.env.NEXT_PUBLIC_DISCORD_CLIENT_SECRET!,
+      client_secret: process.env.DISCORD_CLIENT_SECRET!,
       grant_type: 'authorization_code',
       code,
-      redirect_uri: process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URI!,
+      redirect_uri: process.env.DISCORD_REDIRECT_URI!,
     })
 
     const tokenRes = await fetch('https://discord.com/api/oauth2/token', {
