bf66ec208e02dff7b275f65c9d1827a5caf7898b
Chef Yogi <99634186+Chef-Yogi@users.noreply.github.com>
2025-07-25 14:04:50 +0800

    chore: remove test code

--- apps/web/src/hooks/useAuth.tsx
+++ apps/web/src/hooks/useAuth.tsx
@@ -28,7 +28,6 @@ const useAuth = () => {
   const login = useCallback(
     async (connectorID: ConnectorNames) => {
       const findConnector = CONNECTOR_MAP[connectorID] || undefined
-      console.log('findConnector', { findConnector })
       try {
         if (!findConnector) return undefined
 
--- apps/web/src/utils/wagmi.ts
+++ apps/web/src/utils/wagmi.ts
@@ -26,7 +26,7 @@ export const coinbaseConnector = coinbaseWallet({
 export const walletConnectConnector = walletConnect({
   // ignore the error in test environment
   // Error: To use QR modal, please install @walletconnect/modal package
-  showQrModal: true,
+  showQrModal: process.env.NODE_ENV !== 'test',
   projectId: 'e542ff314e26ff34de2d4fba98db70bb',
 })
 
