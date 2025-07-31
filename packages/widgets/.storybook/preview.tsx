443463b3f36437aa52785e03c17841f0eb774b9f
Chef Jerry <jerry@pancakeswap.com>
2025-07-31 23:03:28 +0800

    feat: examples

--- packages/widgets/.storybook/preview.tsx
+++ packages/widgets/.storybook/preview.tsx
@@ -1,14 +1,6 @@
-import { UIKitProvider, light } from '@pancakeswap/uikit'
 import type { Preview } from '@storybook/react-vite'
 
 const preview: Preview = {
-  decorators: [
-    (Story) => (
-      <UIKitProvider theme={light}>
-        <Story />
-      </UIKitProvider>
-    ),
-  ],
   parameters: {
     controls: {
       matchers: {
