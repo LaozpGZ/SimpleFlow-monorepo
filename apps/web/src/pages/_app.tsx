ea2346f43cc3ee9f73fe5eab402a832c5f1839f9
memoyil <2213635+memoyil@users.noreply.github.com>
2025-08-05 15:03:37 +0200

    chore: Remove import

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -21,7 +21,7 @@ import type { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import Script from 'next/script'
-import { Fragment, Suspense, useMemo } from 'react'
+import { Fragment, Suspense } from 'react'
 import { PersistGate } from 'redux-persist/integration/react'
 import 'utils/abortcontroller-polyfill'
 
