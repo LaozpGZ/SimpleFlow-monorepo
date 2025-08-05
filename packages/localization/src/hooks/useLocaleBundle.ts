639d545c0ec80343e8e9e4f9f81789c313634eab
chef-ryan <ryan@pancakeswap.com>
2025-08-05 19:07:15 +0800

    fix

--- packages/localization/src/hooks/useLocaleBundle.ts
+++ packages/localization/src/hooks/useLocaleBundle.ts
@@ -14,29 +14,26 @@ export const useLocaleBundle = () => {
     ver: 0,
     isFetching: !i18n.hasResourceBundle(lang, 'translation'),
   })
-  const switchBundle = useCallback(
-    async (lang: string) => {
-      if (!i18n.hasResourceBundle(lang, 'translation')) {
-        setState((prev) => ({ ...prev, isFetching: true }))
-        const localeData = await fetchLocale(lang)
-        if (localeData) {
-          i18n.addResourceBundle(lang, 'translation', localeData, true, true)
-          setState((prev) => ({
-            bundle: localeData,
-            ver: prev.ver + 1,
-            isFetching: false,
-          }))
-          return
-        }
+  const switchBundle = useCallback(async (lang: string) => {
+    if (!i18n.hasResourceBundle(lang, 'translation')) {
+      setState((prev) => ({ ...prev, isFetching: true }))
+      const localeData = await fetchLocale(lang)
+      if (localeData) {
+        i18n.addResourceBundle(lang, 'translation', localeData, true, true)
+        setState((prev) => ({
+          bundle: localeData,
+          ver: prev.ver + 1,
+          isFetching: false,
+        }))
+        return
       }
-      setState({
-        bundle: i18n.getResourceBundle(lang, 'translation') || full,
-        ver: state.ver + 1,
-        isFetching: false,
-      })
-    },
-    [state],
-  )
+    }
+    setState((prev) => ({
+      bundle: i18n.getResourceBundle(lang, 'translation') || full,
+      ver: prev.ver + 1,
+      isFetching: false,
+    }))
+  }, [])
 
   useEffect(() => {
     switchBundle(lang)
