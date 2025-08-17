8e3f591487be90cdc54f74d121af23f04fdf78b2
Chef Philip <chef.philip@pancakeswap.com>
2025-08-16 20:30:40 -0400

    chore: add Solana settings

--- apps/web/src/components/Menu/GlobalSettings/SolanaSettingsTab.tsx
+++ apps/web/src/components/Menu/GlobalSettings/SolanaSettingsTab.tsx
@@ -1,14 +1,14 @@
-import { useTranslation } from '@pancakeswap/localization'
-import { Flex, Text } from '@pancakeswap/uikit'
+import { Flex, FlexGap } from '@pancakeswap/uikit'
+import { DefaultExplorerSettingField } from 'components/Settings/DefaultExplorerSettingField'
+import { RPCConnectionSettingField } from 'components/Settings/RPCConnectionSettingField'
 
 export const SolanaSettingsTab = () => {
-  const { t } = useTranslation()
-
   return (
     <Flex pb="24px" flexDirection="column" alignItems="center" justifyContent="center" minHeight="200px">
-      <Text fontSize="18px" color="textSubtle">
-        {t('EVM Settings')}
-      </Text>
+      <FlexGap flexDirection="column" gap="24px">
+        <DefaultExplorerSettingField />
+        <RPCConnectionSettingField />
+      </FlexGap>
     </Flex>
   )
 }
