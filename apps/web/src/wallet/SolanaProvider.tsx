33fc06e705c71ce0a586ea576207cfe26d418edb
Chef Jerry <jerry@pancakeswap.com>
2025-08-18 15:00:18 +0800

    fix: deps issue

--- apps/web/src/wallet/SolanaProvider.tsx
+++ apps/web/src/wallet/SolanaProvider.tsx
@@ -18,7 +18,6 @@ import {
   TrustWalletAdapter,
 } from '@solana/wallet-adapter-wallets'
 import { initialize, SolflareWalletAdapter } from '@solflare-wallet/wallet-adapter'
-import { WalletConnectWalletAdapter } from '@walletconnect/solana-adapter'
 
 import { useAtomValue, useSetAtom } from 'jotai'
 import { rpcUrlAtom } from '@pancakeswap/utils/user'
@@ -41,69 +40,4 @@ export const SolanaWalletStateUpdater = () => {
   }, [connected, connecting, publicKey, setWalletState])
 
   return null
-}
-
-/**
- * @deprecated use @pancakeswap/ui-wallets/src/components/SolanaProvider instead
- */
-export const SolanaProvider: FC<PropsWithChildren<any>> = ({ children }) => {
-  const endpoint = useAtomValue(rpcUrlAtom)
-  const _walletConnect = useMemo(() => {
-    const connectWallet: WalletConnectWalletAdapter[] = []
-    try {
-      connectWallet.push(
-        new WalletConnectWalletAdapter({
-          network: defaultNetWork,
-          options: {
-            projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PJ_ID,
-            metadata: {
-              name: 'PancakeSwap',
-              description: 'Trade, earn, and own crypto on the all-in-one multichain DEX',
-              url: 'https://solana.pancakeswap.finance/swap',
-              icons: ['https://pancakeswap.finance/favicon.ico'],
-            },
-          },
-        }),
-      )
-    } catch (e) {
-      // console.error('WalletConnect error', e)
-    }
-    return connectWallet
-  }, [])
-
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-      // new SlopeWalletAdapter({ endpoint }),
-      ..._walletConnect,
-      new GlowWalletAdapter(),
-      new TrustWalletAdapter(),
-      new MathWalletAdapter({ endpoint }),
-      new TokenPocketWalletAdapter(),
-      new CoinbaseWalletAdapter({ endpoint }),
-      new SolongWalletAdapter({ endpoint }),
-      new Coin98WalletAdapter({ endpoint }),
-      new SafePalWalletAdapter({ endpoint }),
-      new BitpieWalletAdapter({ endpoint }),
-      new BitgetWalletAdapter({ endpoint }),
-      new ExodusWalletAdapter({ endpoint }),
-      new OKXWalletAdapter(),
-      new BackpackWalletAdapter(),
-    ],
-    [endpoint, _walletConnect],
-  )
-
-  const onWalletError = (error: WalletError, adapter?: Adapter) => {
-    // if (!adapter) return
-  }
-
-  return (
-    <ConnectionProvider endpoint={endpoint} config={{ disableRetryOnRateLimit: true }}>
-      <WalletProvider wallets={wallets} onError={onWalletError} autoConnect>
-        <SolanaWalletStateUpdater />
-        <WalletModalProvider>{children}</WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  )
 }
