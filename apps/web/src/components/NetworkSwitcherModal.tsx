76115f603ad22948770fed8b45e4591efda06ba1
memoyil <2213635+memoyil@users.noreply.github.com>
2025-08-25 07:44:42 +0200

    feat: Trade social comp banner (#12089)
    <!--
    Before opening a pull request, please read the [contributing
    guidelines](https://github.com/pancakeswap/pancake-frontend/blob/develop/CONTRIBUTING.md)
    first
    -->
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR focuses on enhancing the functionality and structure of various
    components within the application. Key updates include adjustments to
    hooks, modifications in network handling, and improvements in ad
    configurations.
    
    ### Detailed summary
    - Added `updateSort` to the dependency array in `useMiniPoolsData`.
    - Updated `handleDismiss` in `NetworkSwitcherModal` to include
    `setIsOpen`.
    - Introduced `inline` property in `AdsCampaignConfig`.
    - Replaced `deadline` with `start` and `end` in `AdsCampaignConfig`.
    - Removed `isWrongNetwork` from `useActiveWeb3React` in `NetworkModal`.
    - Simplified blockhash retrieval in `SendAssetForm`.
    - Enhanced localization strings in `translations.json`.
    - Updated `NetworkSwitcher` to include `setIsNetworkSwitcherOpen` in
    dependency array.
    - Added inline styling based on `config.inline` in `AdTextRender`.
    - Modified `getImageUrlFromToken` to accept `UnifiedCurrency`.
    - Updated image URL handling in `CurrencyLogo`.
    - Added new ad configuration for `TRADE_SOCIAL` in `useAdsConfig`.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your
    question}`
    
    <!-- end pr-codex -->

--- apps/web/src/components/NetworkSwitcherModal.tsx
+++ apps/web/src/components/NetworkSwitcherModal.tsx
@@ -221,7 +221,7 @@ export const NetworkSwitcherModal = () => {
 
   const handleDismiss = useCallback(() => {
     setIsOpen(false)
-  }, [])
+  }, [setIsOpen])
 
   if (!chainId || router.pathname.includes('/info')) {
     return null
