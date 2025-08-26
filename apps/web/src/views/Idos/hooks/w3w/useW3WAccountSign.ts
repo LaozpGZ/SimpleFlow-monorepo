e44b02135a1f6263e32afda4ad6aaae060bff47a
Chef Jerry <jerry@pancakeswap.com>
2025-08-26 12:44:11 +0800

    fix: type

--- apps/web/src/views/Idos/hooks/w3w/useW3WAccountSign.ts
+++ apps/web/src/views/Idos/hooks/w3w/useW3WAccountSign.ts
@@ -92,7 +92,7 @@ const w3wSign = async ({
       throw new W3WSignNotSupportedError('W3W sign not supported')
     }
 
-    const binanceW3W = window.binancew3w as BinancewW3W
+    const binanceW3W = window.binancew3w
 
     const result = await binanceW3W.pcs.sign({
       binanceChainId: `${chainId}`,
