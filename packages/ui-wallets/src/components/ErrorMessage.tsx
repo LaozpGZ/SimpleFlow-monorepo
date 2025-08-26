cbec419aeb5e77c034756aa59ca88977eda30ee5
Chef Jerry <jerry@pancakeswap.com>
2025-08-26 17:10:03 +0800

    fix: icon style

--- packages/ui-wallets/src/components/ErrorMessage.tsx
+++ packages/ui-wallets/src/components/ErrorMessage.tsx
@@ -2,6 +2,6 @@ import { Text, WarningIcon } from '@pancakeswap/uikit'
 
 export const ErrorMessage = ({ message }: { message: string }) => (
   <Text bold color="failure">
-    <WarningIcon width="16px" color="failure" style={{ verticalAlign: 'middle' }} /> {message}
+    <WarningIcon width="16px" color="failure" style={{ verticalAlign: 'middle', display: 'inline' }} /> {message}
   </Text>
 )
