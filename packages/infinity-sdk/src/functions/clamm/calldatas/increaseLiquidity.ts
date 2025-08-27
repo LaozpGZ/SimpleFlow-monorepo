013a908b82231c7057c86bdf4451d4928ebef940
Chef Jerry <jerry@pancakeswap.com>
2025-08-27 23:05:52 +0800

    fix: add close native action

--- packages/infinity-sdk/src/functions/clamm/calldatas/increaseLiquidity.ts
+++ packages/infinity-sdk/src/functions/clamm/calldatas/increaseLiquidity.ts
@@ -1,4 +1,4 @@
-import { Hex } from 'viem'
+import { Address, Hex, zeroAddress } from 'viem'
 import { ACTIONS } from '../../../constants/actions'
 import { CLPositionConfig } from '../../../types'
 import { ActionsPlanner } from '../../../utils/ActionsPlanner'
@@ -10,11 +10,17 @@ export const encodeCLPositionManagerIncreaseLiquidityCalldata = (
   liquidity: bigint,
   amount0Max: bigint,
   amount1Max: bigint,
+  recipient: Address,
   hookData: Hex = '0x',
   deadline: bigint
 ) => {
   const planner = new ActionsPlanner()
   planner.add(ACTIONS.CL_INCREASE_LIQUIDITY, [tokenId, liquidity, amount0Max, amount1Max, hookData])
-  const calls = planner.finalizeModifyLiquidityWithClose(positionConfig.poolKey)
+
+  const containNativeCurrency = positionConfig.poolKey.currency0 === zeroAddress
+
+  const calls = containNativeCurrency
+    ? planner.finalizeModifyLiquidityWithCloseNative(positionConfig.poolKey, recipient)
+    : planner.finalizeModifyLiquidityWithClose(positionConfig.poolKey)
   return encodeCLPositionModifyLiquidities(calls, deadline)
 }
