09737681eaa2d479f2b5ce804dfb3f255bc31592
chef-ryan <ryan@pancakeswap.com>
2025-09-03 15:15:16 +0800

    update switch for limit and twap

--- apps/web/src/wallet/network.switch.config.ts
+++ apps/web/src/wallet/network.switch.config.ts
@@ -1,1 +1,1 @@
-export const SOLANA_SUPPORTED_PATH = ['/bridge', '/sol/wallet-test', '/swap', '/home']
+export const SOLANA_SUPPORTED_PATH = ['/bridge', '/sol/wallet-test', '/swap', '/home', '/swap/limit', '/swap/twap']
