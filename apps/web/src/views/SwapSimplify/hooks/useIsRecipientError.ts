18e4e33f0dab78e325910df0ae689bd051e3c7b9
Chef Philip <chef.philip@pancakeswap.com>
2025-09-08 09:45:44 -0400

    chore: support recipient solana

--- apps/web/src/views/SwapSimplify/hooks/useIsRecipientError.ts
+++ apps/web/src/views/SwapSimplify/hooks/useIsRecipientError.ts
@@ -3,16 +3,27 @@ import { useGetENSAddressByName } from 'hooks/useGetENSAddressByName'
 import { useMemo } from 'react'
 import { useSwapState } from 'state/swap/hooks'
 import { safeGetAddress } from 'utils'
+import { Field } from 'state/swap/actions'
+import { isSolana } from '@pancakeswap/chains'
+import { isValidSolanaAddress } from 'utils/isValidSolanaAddress'
 import { useAllowRecipient } from '../../Swap/V3Swap/hooks'
 
 export const useIsRecipientError = () => {
-  const { recipient } = useSwapState()
+  const {
+    recipient,
+    [Field.OUTPUT]: { chainId: outputChainId },
+  } = useSwapState()
   const allowRecipient = useAllowRecipient()
   const debounceEnsName = useDebounce(recipient, 500)
   const recipientENSAddress = useGetENSAddressByName(debounceEnsName ?? undefined)
 
   const isRecipientError = useMemo(() => {
     if (!allowRecipient || recipient === null) return false
+
+    if (outputChainId && isSolana(outputChainId)) {
+      return !isValidSolanaAddress(recipient)
+    }
+
     const address = safeGetAddress(recipient) ? recipient : safeGetAddress(recipientENSAddress)
     return Boolean(recipient.length > 0 && !address)
   }, [recipient, allowRecipient, recipientENSAddress])
