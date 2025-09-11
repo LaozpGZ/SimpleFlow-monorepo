b4a981066f596c59dc5e23415908cb6bb57d15dc
chef-ryan <ryan@pancakeswap.com>
2025-09-11 09:37:02 +0800

    for test

--- apps/solana/src/test-i18n.tsx
+++ apps/solana/src/test-i18n.tsx
@@ -4,6 +4,7 @@ export const TestI18n = () => {
   const { t } = useTranslation()
   return (
     <p>
+      {t('Hello World!')}
       <Trans i18nTemplate="Hello <0>World</0>!" components={[<strong key={0} />]} />
     </p>
   )
