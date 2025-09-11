400402adaaa7fa68a2560f43a3eb2e332cf1ae08
chef-ryan <ryan@pancakeswap.com>
2025-09-11 12:50:15 +0800

    fix

--- packages/localization/src/config/extendList.ts
+++ packages/localization/src/config/extendList.ts
@@ -2,6 +2,6 @@ import list from './translation.extend.json'
 
 export const extendEnList: Record<string, string> = Object.keys(list).reduce((acc, key) => {
   // eslint-disable-next-line no-param-reassign
-  acc[key] = list[key].value
+  acc[key] = (list as any)[key]!.value
   return acc
 }, {} as Record<string, string>)
