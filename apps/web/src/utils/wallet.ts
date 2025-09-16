57026eb38d31aca4d68f087509f80fdf5aa00915
chef-ryan <ryan@pancakeswap.com>
2025-09-16 17:45:57 +0800

    fix walelt assets check

--- apps/web/src/utils/wallet.ts
+++ apps/web/src/utils/wallet.ts
@@ -1,24 +1,30 @@
+import { memoizeAsync } from '@pancakeswap/utils/memoize'
 import { Connector } from 'wagmi'
 
-export const checkWalletCanRegisterToken = async (connector: Connector) => {
-  try {
-    if (typeof connector.getProvider !== 'function') return false
+export const checkWalletCanRegisterToken = memoizeAsync(
+  async (connector: Connector) => {
+    try {
+      if (typeof connector.getProvider !== 'function') return false
 
-    const provider = (await connector.getProvider()) as any
+      const provider = (await connector.getProvider()) as any
 
-    return (
-      !!provider?.request &&
-      typeof provider.request === 'function' &&
-      // Some providers throw if they don’t support it
-      (await provider
-        .request({ method: 'wallet_watchAsset', params: { type: 'ERC20', options: {} } })
-        .then(() => true)
-        .catch(() => true)) // they support the method even if the dummy call fails
-    )
-  } catch (error: any) {
-    // If the provider rejects "unknown method", it doesn’t support asset registration
-    if (error?.code === -32601) return false
-    console.error(error, 'Error while checking wallet token registration support')
-    return false
-  }
-}
+      return (
+        !!provider?.request &&
+        typeof provider.request === 'function' &&
+        // Some providers throw if they don’t support it
+        (await provider
+          .request({ method: 'wallet_watchAsset', params: { type: 'ERC20', options: {} } })
+          .then(() => true)
+          .catch(() => true)) // they support the method even if the dummy call fails
+      )
+    } catch (error: any) {
+      // If the provider rejects "unknown method", it doesn’t support asset registration
+      if (error?.code === -32601) return false
+      console.error(error, 'Error while checking wallet token registration support')
+      return false
+    }
+  },
+  {
+    resolver: (connector: Connector) => `${connector.name}-${connector.uid}-${connector.id}`,
+  },
+)
