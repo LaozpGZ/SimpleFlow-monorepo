e18a952117c4f3259d4abbb3df765fd135d102e9
memoyil <2213635+memoyil@users.noreply.github.com>
2025-09-18 11:22:51 +0200

    fix: Bottom nav issue

--- packages/uikit/src/components/BottomNav/styles.tsx
+++ packages/uikit/src/components/BottomNav/styles.tsx
@@ -3,11 +3,14 @@ import { Flex } from "../Box";
 
 const StyledBottomNav = styled(Flex)`
   position: fixed;
-  bottom: 0px;
+  bottom: 0;
+  left:0;
+  right:0
   width: 100%;
   padding: 5px 8px;
   background: ${({ theme }) => theme.colors.backgroundAlt};
   border-top: 1px solid ${({ theme }) => theme.colors.cardBorder};
+  padding:0 env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)
   html[data-useragent*="TokenPocket_iOS"] & {
     padding-bottom: 45px;
   }
