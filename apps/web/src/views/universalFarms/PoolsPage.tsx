c07ae9056c343e2b914ab84c2f0c7afb4d47a6ce
chef-ryan <ryan@pancakeswap.com>
2025-09-18 17:31:57 +0800

    bring sol net back

--- apps/web/src/views/universalFarms/PoolsPage.tsx
+++ apps/web/src/views/universalFarms/PoolsPage.tsx
@@ -70,7 +70,7 @@ export const PoolsPage = () => {
     <FarmSearchContextProvider>
       <Card>
         <CardHeader p={isMobile ? '16px' : undefined}>
-          <PoolsFilterPanel onChange={handleFilterChange} value={poolsFilter} includeSolana={false}>
+          <PoolsFilterPanel onChange={handleFilterChange} value={poolsFilter} includeSolana>
             {(isMobile || isMd) && (
               <>
                 <CreatePoolButton scale="sm" width="100%" height="40px" />
