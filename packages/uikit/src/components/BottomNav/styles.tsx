99e8732a7d48f930796b3be13d6f9a055fa20d6d
memoyil <2213635+memoyil@users.noreply.github.com>
2025-09-18 11:23:22 +0200

    fix: Remove padding

--- packages/uikit/src/components/BottomNav/styles.tsx
+++ packages/uikit/src/components/BottomNav/styles.tsx
@@ -7,7 +7,6 @@ const StyledBottomNav = styled(Flex)`
   left:0;
   right:0
   width: 100%;
-  padding: 5px 8px;
   background: ${({ theme }) => theme.colors.backgroundAlt};
   border-top: 1px solid ${({ theme }) => theme.colors.cardBorder};
   padding:0 env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)
