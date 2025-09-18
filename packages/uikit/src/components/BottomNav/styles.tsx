4315e00812fa85baa65de71e4f76f1ee1b25630c
memoyil <2213635+memoyil@users.noreply.github.com>
2025-09-18 11:39:20 +0200

    fix: Right

--- packages/uikit/src/components/BottomNav/styles.tsx
+++ packages/uikit/src/components/BottomNav/styles.tsx
@@ -4,13 +4,13 @@ import { Flex } from "../Box";
 const StyledBottomNav = styled(Flex)`
   position: fixed;
   bottom: 0;
-  left:0;
-  right:0
+  left: 0;
+  right: 0;
   width: 100%;
   background: ${({ theme }) => theme.colors.backgroundAlt};
   border-top: 1px solid ${({ theme }) => theme.colors.cardBorder};
-  padding:0 env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)
-  html[data-useragent*="TokenPocket_iOS"] & {
+  padding: 0 env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left) html[data-useragent * =
+    "TokenPocket_iOS"] & {
     padding-bottom: 45px;
   }
   z-index: 20;
