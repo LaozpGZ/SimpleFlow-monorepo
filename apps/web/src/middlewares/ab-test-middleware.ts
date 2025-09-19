f6f80bdf4b052d0431bd409305e78987cb4fe9c0
Chef Philip <chef.philip@pancakeswap.com>
2025-09-18 21:04:26 -0400

    chore: update

--- apps/web/src/middlewares/ab-test-middleware.ts
+++ apps/web/src/middlewares/ab-test-middleware.ts
@@ -1,5 +1,6 @@
 import { getCookieKey } from 'config/experimentalFeatures'
 import { NextFetchEvent, NextResponse } from 'next/server'
+import { RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies'
 import { getExperimentalFeatureAccessList } from '../flags'
 import { ONE_YEAR_SECONDS } from './constants'
 import { ExtendedNextReq, MiddlewareFactory, NextMiddleware } from './types'
@@ -8,6 +9,10 @@ export const withABTesting: MiddlewareFactory = (next: NextMiddleware) => {
   return async (request: ExtendedNextReq, _next: NextFetchEvent) => {
     const clientId = request?.clientId
     const response = (await next(request, _next)) || NextResponse.next()
+
+    const cookies = new RequestCookies(request.headers)
+
+    console.log('cookies', cookies)
 
     console.log('clientId', clientId)
 
