5f8309a0bf8b496e12934991a1274fe0e569773c
Chef Jerry <jerry@pancakeswap.com>
2025-09-22 15:53:14 +0800

    feat: significantDigits

--- apps/web/src/views/universalFarms/components/Modals/solana/SolanaV3RemovePositionModal.tsx
+++ apps/web/src/views/universalFarms/components/Modals/solana/SolanaV3RemovePositionModal.tsx
@@ -269,7 +269,7 @@ const RemovedAmountInfo: React.FC<{
           </Text>
         </Flex>
         <Flex>
-          <FormattedCurrencyAmount currencyAmount={amount0} />
+          <FormattedCurrencyAmount currencyAmount={amount0} significantDigits={8} />
         </Flex>
       </Flex>
       <Flex justifyContent="flex-end" mb="8px">
@@ -285,7 +285,7 @@ const RemovedAmountInfo: React.FC<{
           </Text>
         </Flex>
         <Flex>
-          <FormattedCurrencyAmount currencyAmount={amount1} />
+          <FormattedCurrencyAmount currencyAmount={amount1} significantDigits={8} />
         </Flex>
       </Flex>
       <Flex justifyContent="flex-end" mb="8px">
