b36a51891e9799e0d1f28ef6014dbfc9dd037244
memoyil <2213635+memoyil@users.noreply.github.com>
2025-09-22 12:20:06 +0200

    fix: Remove padding in limit

--- apps/web/src/pages/swap/limit.tsx
+++ apps/web/src/pages/swap/limit.tsx
@@ -7,7 +7,7 @@ import { PCSLimitOrdersView } from 'views/PCSLimitOrders'
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <Page showExternalLink={false} showHelpLink={false}>
+    <Page showExternalLink={false} showHelpLink={false} removePadding>
       {children}
     </Page>
   )
