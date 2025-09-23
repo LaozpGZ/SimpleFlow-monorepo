1420d11f9d58856f3d31a95c610912a5ec37e7e4
Chef Penguin <penguin@pancakeswap.com>
2025-09-23 12:04:52 +0530

    chore: comment

--- apps/web/src/views/PCSLimitOrders/hooks/usePlaceLimitOrder.ts
+++ apps/web/src/views/PCSLimitOrders/hooks/usePlaceLimitOrder.ts
@@ -123,9 +123,9 @@ export const usePlaceLimitOrder = ({ onError, onSuccess }: UsePlaceLimitOrder = 
     })
 
     try {
-      const estimatedGas = await contract.estimateGas.placeOrder([encodedPoolKey, targetTick, zeroForOne, liquidity], {
-        account,
-      })
+      // const estimatedGas = await contract.estimateGas.placeOrder([encodedPoolKey, targetTick, zeroForOne, liquidity], {
+      //   account,
+      // })
 
       // console.log('placeOrder: estimatedGas', estimatedGas)
 
@@ -137,7 +137,7 @@ export const usePlaceLimitOrder = ({ onError, onSuccess }: UsePlaceLimitOrder = 
           account,
           chain: contract.chain,
           value,
-          gas: calculateGasMargin(estimatedGas),
+          // gas: calculateGasMargin(estimatedGas),
         })
       })
 
