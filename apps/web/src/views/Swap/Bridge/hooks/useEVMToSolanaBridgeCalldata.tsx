cf838fa0bdb9a567150f7ef9a0655f35c772ba98
Chef Philip <chef.philip@pancakeswap.com>
2025-09-24 07:23:36 -0400

    fix: add custome receipient in calldata

--- apps/web/src/views/Swap/Bridge/hooks/useEVMToSolanaBridgeCalldata.tsx
+++ apps/web/src/views/Swap/Bridge/hooks/useEVMToSolanaBridgeCalldata.tsx
@@ -4,6 +4,8 @@ import useAccountActiveChain from 'hooks/useAccountActiveChain'
 import { BridgeOrderWithCommands, isBridgeOrder } from 'views/Swap/utils'
 import { Calldata } from 'hooks/usePermit2'
 import { isEvm, isSolana } from '@pancakeswap/chains'
+import { useSwapState } from 'state/swap/hooks'
+import { isValidSolanaAddress } from 'utils/isValidSolanaAddress'
 import { getSolanaBridgeCalldata } from '../api'
 import { RELAY_STEP_ID } from '../types'
 
@@ -23,6 +25,7 @@ export const useEVMToSolanaBridgeCalldata = ({
   | undefined => {
   const { account, solanaAccount } = useAccountActiveChain()
   const [allowedSlippage] = useUserSlippage()
+  const { recipient: recipientAddress } = useSwapState()
 
   const isEvmToSolanaBridge =
     isBridgeOrder(order) &&
@@ -48,15 +51,22 @@ export const useEVMToSolanaBridgeCalldata = ({
       solanaAccount,
       account,
       allowedSlippage,
+      recipientAddress,
     ],
     queryFn: async () => {
       if (!order || !solanaAccount || !account) {
         return undefined
       }
 
+      if (recipientAddress && !isValidSolanaAddress(recipientAddress)) {
+        return undefined
+      }
+
+      const recipient = recipientAddress || solanaAccount
+
       const calldataResponse = await getSolanaBridgeCalldata({
         order,
-        recipient: solanaAccount,
+        recipient,
         user: account,
         allowedSlippage,
       })
