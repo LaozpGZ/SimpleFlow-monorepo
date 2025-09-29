8de0293858ece97aa88c19b1fc1055cd07b21936
Chef Penguin <penguin@pancakeswap.com>
2025-09-29 14:37:05 +0530

    fix: reset output token on input select if not a valid pair

--- apps/web/src/views/PCSLimitOrders/state/currency/setCurrencyAtoms.ts
+++ apps/web/src/views/PCSLimitOrders/state/currency/setCurrencyAtoms.ts
@@ -1,15 +1,18 @@
-import { UnifiedCurrency } from '@pancakeswap/swap-sdk-core'
+import { Currency } from '@pancakeswap/swap-sdk-core'
 import { atom } from 'jotai'
 
 import currencyId from 'utils/currencyId'
 import { Field } from 'views/PCSLimitOrders/types/limitOrder.types'
 import { independentFieldAtom } from 'views/PCSLimitOrders/state/form/fieldAtoms'
+import { accountActiveChainAtom } from 'wallet/atoms/accountStateAtoms'
+import { getCurrencyAddress } from 'utils/getCurrencyAddress'
 import { outputCurrencyAtom, inputCurrencyAtom, inputCurrencyIdAtom, outputCurrencyIdAtom } from './currencyAtoms'
 import { customMarketPriceAtom } from '../form/customMarketPriceAtom'
+import { supportedPoolsListAtom } from '../pools/poolsListAtom'
 
 export const setCurrencyAtom = atom(
   null,
-  async (get, set, { field, newCurrency }: { field: Field; newCurrency: UnifiedCurrency }) => {
+  async (get, set, { field, newCurrency }: { field: Field; newCurrency: Currency }) => {
     const newId = currencyId(newCurrency)
 
     const otherCurrency = field === Field.CURRENCY_A ? await get(outputCurrencyAtom) : await get(inputCurrencyAtom)
@@ -24,8 +27,25 @@ export const setCurrencyAtom = atom(
     // Clear custom market price when currency changes since it's no longer valid
     set(customMarketPriceAtom, undefined)
 
-    if (field === Field.CURRENCY_A) set(inputCurrencyIdAtom, newId)
-    else set(outputCurrencyIdAtom, newId)
+    if (field === Field.CURRENCY_A) {
+      set(inputCurrencyIdAtom, newId)
+
+      // Further, check if input currency is selected and that the current output currency makes a valid pair
+      const { chainId } = get(accountActiveChainAtom)
+      const allPools = await get(supportedPoolsListAtom)
+      const validPools = allPools.filter(
+        (pool) =>
+          (pool.chainId === chainId &&
+            pool.currency0 === getCurrencyAddress(newCurrency) &&
+            pool.currency1 === getCurrencyAddress(otherCurrency)) ||
+          (pool.currency0 === getCurrencyAddress(otherCurrency) && pool.currency1 === getCurrencyAddress(newCurrency)),
+      )
+
+      // Reset output token if not a valid pair
+      if (validPools.length === 0) {
+        set(outputCurrencyIdAtom, '')
+      }
+    } else set(outputCurrencyIdAtom, newId)
   },
 )
 
