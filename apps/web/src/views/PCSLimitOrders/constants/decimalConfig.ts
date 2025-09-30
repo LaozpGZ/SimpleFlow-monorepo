7258e4662b692828d085b995b792012c5c63e114
Chef Penguin <penguin@pancakeswap.com>
2025-09-30 11:28:25 +0530

    chore: orders summary show conditions

--- apps/web/src/views/PCSLimitOrders/constants/decimalConfig.ts
+++ apps/web/src/views/PCSLimitOrders/constants/decimalConfig.ts
@@ -4,7 +4,8 @@
  */
 export const LIMIT_ORDER_DECIMAL_CONFIG = {
   btcb: 8,
-}
+} as const
+
 export const getSymbolDecimals = (symbol: string | undefined) => {
   return symbol ? LIMIT_ORDER_DECIMAL_CONFIG[symbol.toLowerCase()] || 6 : 6
 }
