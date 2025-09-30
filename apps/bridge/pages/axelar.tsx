b0483d3db3e1ba69cd827e577d0424a7a1b0b775
chef-ryan <ryan@pancakeswap.com>
2025-09-30 15:27:49 +0800

    fix build issues

--- apps/bridge/pages/axelar.tsx
+++ apps/bridge/pages/axelar.tsx
@@ -1,9 +1,13 @@
 import { useMemo } from 'react'
-import { SquidWidget } from '@0xsquid/widget'
+import dynamic from 'next/dynamic'
 import { AppConfig } from '@0xsquid/widget/widget/core/types/config'
 import { Box, PancakeTheme } from '@pancakeswap/uikit'
 import { useTheme } from 'styled-components'
 import PageContainer from 'components/Page'
+
+const SquidWidget = dynamic(() => import('@0xsquid/widget').then((mod) => ({ default: mod.SquidWidget })), {
+  ssr: false,
+})
 
 const lightStyle = {
   neutralContent: '#7a6eaa',
