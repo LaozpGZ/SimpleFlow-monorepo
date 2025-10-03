7561d05076eafc494b143a280a9b5e7b05d050bf
Chef Penguin <penguin@pancakeswap.com>
2025-10-03 16:41:46 +0530

    chore: add tokens

--- packages/tokens/src/constants/monad.ts
+++ packages/tokens/src/constants/monad.ts
@@ -24,4 +24,28 @@ export const monadTokens = {
     'Monad Cake',
     'https://www.monad.xyz/',
   ),
+  musdt: new ERC20Token(
+    ChainId.MONAD_MAINNET,
+    '0xF05266b1B1759D3A2746cd3b031b802C2496b278',
+    18,
+    'MUSDT',
+    'Mock USDT',
+    'https://tether.to/',
+  ),
+  meth: new ERC20Token(
+    ChainId.MONAD_MAINNET,
+    '0xD59aaE32bcF95AAe58119751d5E355C9855cDC31',
+    18,
+    'METH',
+    'Mock ETH',
+    'https://ethereum.org',
+  ),
+  mbnb: new ERC20Token(
+    ChainId.MONAD_MAINNET,
+    '0x95aaBC8387963DD589292915374AdD2920BA3cc7',
+    18,
+    'MBNB',
+    'Mock BNB',
+    'https://www.binance.org',
+  ),
 }
