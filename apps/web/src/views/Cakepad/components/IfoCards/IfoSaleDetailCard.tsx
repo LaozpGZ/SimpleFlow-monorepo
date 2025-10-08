adeb0b2d59b38d8f7bfc0f4cecbcef3e8d73b9dc
Chef Penguin <penguin@pancakeswap.com>
2025-10-08 17:08:31 +0530

    chore: text

--- apps/web/src/views/Cakepad/components/IfoCards/IfoSaleDetailCard.tsx
+++ apps/web/src/views/Cakepad/components/IfoCards/IfoSaleDetailCard.tsx
@@ -16,7 +16,7 @@ export const IfoSaleDetailCard: React.FC = () => {
           <Box mt="2px">
             <CheckmarkCircleIcon color={theme.colors.primary60} width="20px" />
           </Box>
-          <Text color="primary60">{t('Eligible to join when this IFO goes live!')}</Text>
+          <Text color="primary60">{t('Eligible to join when this event goes live!')}</Text>
         </FlexGap>
         <FlexGap flexDirection="column" gap="16px" mt="16px">
           {pools.map((pool) => (
