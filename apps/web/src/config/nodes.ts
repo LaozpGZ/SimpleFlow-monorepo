43f53b5569a714924777f7fb609542b1eb965282
Chef Penguin <penguin@pancakeswap.com>
2025-10-20 10:17:32 +0530

    chore: remove test rpc url

--- apps/web/src/config/nodes.ts
+++ apps/web/src/config/nodes.ts
@@ -86,17 +86,14 @@ export const SERVER_NODES = {
 
 export const PUBLIC_NODES: Record<ChainId, string[] | readonly string[]> = {
   [ChainId.BSC]: [
-    // ! Ths RPC is for Cakepad IFO QA Testing. Remove it after testing, Do not merge.
-    // [VirtualNet QA2] Snapshot ID: 0x071dee289d1188a6e800fad531e121ba9aa58527689cc54c01a7b3006089dba3
-    'https://virtual.binance.eu.rpc.tenderly.co/af3297d9-7af2-4da1-ae78-d73951654e58',
-    // process.env.NEXT_PUBLIC_NODE_PRODUCTION || '',
-    // getNodeRealUrl(ChainId.BSC, process.env.NEXT_PUBLIC_NODE_REAL_API_ETH) || '',
-    // process.env.NEXT_PUBLIC_NODIES_BSC || '',
-    // // getGroveUrl(ChainId.BSC, process.env.NEXT_PUBLIC_GROVE_API_KEY) || '',
-    // 'https://bsc.publicnode.com',
-    // 'https://binance.llamarpc.com',
-    // 'https://bsc-dataseed1.defibit.io',
-    // 'https://bsc-dataseed1.bnbchain.org',
+    process.env.NEXT_PUBLIC_NODE_PRODUCTION || '',
+    getNodeRealUrl(ChainId.BSC, process.env.NEXT_PUBLIC_NODE_REAL_API_ETH) || '',
+    process.env.NEXT_PUBLIC_NODIES_BSC || '',
+    // getGroveUrl(ChainId.BSC, process.env.NEXT_PUBLIC_GROVE_API_KEY) || '',
+    'https://bsc.publicnode.com',
+    'https://binance.llamarpc.com',
+    'https://bsc-dataseed1.defibit.io',
+    'https://bsc-dataseed1.bnbchain.org',
   ].filter(Boolean),
   [ChainId.BSC_TESTNET]: [
     getNodeRealUrl(ChainId.BSC_TESTNET, process.env.SERVER_NODE_REAL_API_ETH) || '',
