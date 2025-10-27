aa746fe6a8df8a22f6646f0ccffeb28a1e977211
memoyil <2213635+memoyil@users.noreply.github.com>
2025-10-27 18:22:40 +0100

    fix: To integer

--- apps/web/src/views/Liquidity/LiquidityView.tsx
+++ apps/web/src/views/Liquidity/LiquidityView.tsx
@@ -210,7 +210,7 @@ export const LiquidityView = () => {
 
   const { tokenId: tokenIdFromUrl } = router.query
 
-  const parsedTokenId = Number.isFinite(Number(tokenIdFromUrl)) ? BigInt(tokenIdFromUrl as string) : undefined
+  const parsedTokenId = Number.isInteger(Number(tokenIdFromUrl)) ? BigInt(tokenIdFromUrl as string) : undefined
 
   const { loading: positionDetailsLoading, position: positionDetails } = useV3PositionFromTokenId(parsedTokenId)
 
