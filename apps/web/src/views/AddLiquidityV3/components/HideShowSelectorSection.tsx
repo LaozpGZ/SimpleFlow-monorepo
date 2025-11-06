f4c7af118df1f283e69cae86ef9da53ab005d9e2
memoyil <2213635+memoyil@users.noreply.github.com>
2025-11-06 11:26:08 +0100

    fix: Fee selector height

--- apps/web/src/views/AddLiquidityV3/components/HideShowSelectorSection.tsx
+++ apps/web/src/views/AddLiquidityV3/components/HideShowSelectorSection.tsx
@@ -1,22 +1,24 @@
 import { useTranslation } from '@pancakeswap/localization'
-import { AutoRow, Button, ChevronDownIcon, Text, useIsomorphicEffect, useMatchBreakpoints } from '@pancakeswap/uikit'
+import { AutoRow, Button, ChevronDownIcon, Text, useMatchBreakpoints } from '@pancakeswap/uikit'
 import { LightGreyCard } from 'components/Card'
-import { Dispatch, ReactNode, SetStateAction, useRef } from 'react'
+import { Dispatch, ReactNode, SetStateAction } from 'react'
 import styled from 'styled-components'
 
 const StyledLightGreyCard = styled(LightGreyCard)`
   padding: 16px;
   border-radius: 16px;
-  height: fit-content;
   border: 1px solid ${({ theme }) => theme.colors.cardBorder};
   border-bottom: 2px solid ${({ theme }) => theme.colors.cardBorder};
 
-  height: 0px;
+  .content-wrapper {
+    overflow: hidden;
+    max-height: 0;
+    transition: max-height 1s ease-in-out;
+  }
 
-  transition: height 0.2s ease-in-out;
-  will-change: height;
-
-  overflow: hidden;
+  &.expanded .content-wrapper {
+    max-height: 1000px;
+  }
 `
 
 interface HideShowSelectorSectionPropsType {
@@ -26,8 +28,6 @@ interface HideShowSelectorSectionPropsType {
   heading: ReactNode
   content: ReactNode
 }
-
-const PADDING = 32
 
 export default function HideShowSelectorSection({
   noHideButton,
@@ -40,31 +40,9 @@ export default function HideShowSelectorSection({
 
   const { isXs } = useMatchBreakpoints()
 
-  const parentRef = useRef<HTMLDivElement>(null)
-  const headerRef = useRef<HTMLDivElement>(null)
-  const contentRef = useRef<HTMLDivElement>(null)
-
-  useIsomorphicEffect(() => {
-    const parent = parentRef.current
-    const header = headerRef.current
-    const content = contentRef.current
-    if (parent && content && showOptions) {
-      const headerHeight = header?.scrollHeight ?? 0
-      const contentHeight = content.scrollHeight
-      parent.style.height = `${contentHeight + headerHeight + PADDING * 2 + 16}px`
-    } else if (parent && !showOptions) {
-      parent.style.height = `${header?.scrollHeight ?? 0 + PADDING * 2}px`
-    }
-  }, [showOptions])
-
   return (
-    <StyledLightGreyCard ref={parentRef}>
-      <AutoRow
-        justifyContent="space-between"
-        alignItems="center"
-        marginBottom={showOptions ? '8px' : '0px'}
-        ref={headerRef}
-      >
+    <StyledLightGreyCard className={showOptions ? 'expanded' : ''}>
+      <AutoRow justifyContent="space-between" alignItems="center" marginBottom={showOptions ? '8px' : '0px'}>
         {heading ?? <div />}
         {noHideButton || (
           <Button
@@ -88,7 +66,7 @@ export default function HideShowSelectorSection({
           </Button>
         )}
       </AutoRow>
-      {showOptions && <div ref={contentRef}>{content}</div>}
+      <div className="content-wrapper">{showOptions ? content : null}</div>
     </StyledLightGreyCard>
   )
 }
