b5006b11b077e4cd910709989cc926bdb5956346
memoyil <2213635+memoyil@users.noreply.github.com>
2025-11-18 12:34:18 +0100

    fix: Bin create

--- apps/web/src/views/CreateLiquidityPool/hooks/useCreateDepositAmounts.ts
+++ apps/web/src/views/CreateLiquidityPool/hooks/useCreateDepositAmounts.ts
@@ -254,19 +254,19 @@ export const useBinDepositAmounts = () => {
 
       if (currency === 0) {
         setInputValue0(amount)
-        setDepositCurrencyAmounts({
-          ...depositCurrencyAmounts,
+        setDepositCurrencyAmounts((prev) => ({
+          ...prev,
           depositCurrencyAmount0: amount === '' ? null : CurrencyAmount.fromRawAmount(c0, parsedAmount),
-        })
+        }))
       } else {
         setInputValue1(amount)
-        setDepositCurrencyAmounts({
-          ...depositCurrencyAmounts,
+        setDepositCurrencyAmounts((prev) => ({
+          ...prev,
           depositCurrencyAmount1: amount === '' ? null : CurrencyAmount.fromRawAmount(c1, parsedAmount),
-        })
+        }))
       }
     },
-    [currency0, currency1, depositCurrencyAmounts, setDepositCurrencyAmounts],
+    [currency0, currency1, setDepositCurrencyAmounts],
   )
 
   const resetAmounts = useCallback(() => {
