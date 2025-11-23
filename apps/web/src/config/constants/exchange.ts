2bec31ab905e40d48886dd0b6dc6aba517ebaee4
Chef Penguin <penguin@pancakeswap.com>
2025-11-23 12:48:40 +0530

    feat: gas margin by chain, set 10% for monad

--- apps/web/src/config/constants/exchange.ts
+++ apps/web/src/config/constants/exchange.ts
@@ -197,3 +197,10 @@ export const LIMIT_ORDERS_DOCS_URL = 'https://docs.pancakeswap.finance/products/
 
 export const EXCHANGE_PAGE_PATHS = ['/swap', '/limit-orders', 'liquidity', '/add', '/find', '/remove', '/stable', '/v2']
 export const UNIVERSAL_PAGE_PATHS = ['/liquidity/pools', '/liquidity/positions']
+
+// Override gas buffer per chain. If not present, use a default value
+export const GAS_MARGIN_BY_CHAIN: Partial<Record<ChainId, bigint>> = {
+  // Only 10% buffer for Monad Mainnet due to full consumption of gas limit
+  // https://docs.monad.xyz/developer-essentials/gas-pricing#gas-limit-not-gas-used
+  [ChainId.MONAD_MAINNET]: 1000n,
+}
