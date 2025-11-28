8061b229d32df14327678aeef410e312198ab191
memoyil <2213635+memoyil@users.noreply.github.com>
2025-11-28 11:32:07 +0100

    fix: Comment

--- packages/widgets-internal/swap/LiquidityChartRangeInput/Brush.tsx
+++ packages/widgets-internal/swap/LiquidityChartRangeInput/Brush.tsx
@@ -50,8 +50,8 @@ const BRUSH_EXTENT_MARGIN_PX = 2;
  */
 const compare = (a: [number, number], b: [number, number], xScale: ScaleLinear<number, number>): boolean => {
   // normalize pixels to 1 decimals
-  const aNorm = a.map((x) => (Number.isNaN(x) ? "0" : xScale(x)?.toFixed(1)));
-  const bNorm = b.map((x) => (Number.isNaN(x) ? "0" : xScale(x)?.toFixed(1)));
+  const aNorm = a.map((x) => xScale(x)?.toFixed(1) || "0");
+  const bNorm = b.map((x) => xScale(x)?.toFixed(1) || "0");
   return aNorm.every((v, i) => v === bNorm[i]);
 };
 
