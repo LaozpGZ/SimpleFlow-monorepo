544f5692b85d47294b6fb72de061fe8d57a5dbbb
memoyil <2213635+memoyil@users.noreply.github.com>
2025-12-03 16:31:58 +0100

    fix: Add null/undefined checks

--- packages/ui-wallets/src/hooks/useMetamaskVersionWarning.ts
+++ packages/ui-wallets/src/hooks/useMetamaskVersionWarning.ts
@@ -6,11 +6,14 @@ import { useConnections, useConnectorClient } from 'wagmi'
  * This hook will check Metamask version and return true if it's less than 13.3.0.
  */
 
-function isOutdatedVersion(current: string, minimum: string) {
-  const a = current.split('.').map(Number)
-  const b = minimum.split('.').map(Number)
+function isOutdatedVersion(minimum: string, current?: string | null) {
+  if (typeof current !== 'string') return false
 
-  // Ensure major.minor.patch format
+  const a = current.split('.').map((v) => Number(v))
+  const b = minimum.split('.').map((v) => Number(v))
+
+  if (a.some(Number.isNaN)) return false
+
   while (a.length < 3) a.push(0)
   while (b.length < 3) b.push(0)
 
@@ -35,7 +38,7 @@ export const useMetamaskVersionWarning = () => {
       const version =
         /MetaMask\/v?(\d+\.\d+\.\d+(?:-[0-9A-Za-z.-]+)?(?:\+[0-9A-Za-z.-]+)?)/i.exec(clientVersion)?.[1] ?? null
 
-      if (version && isOutdatedVersion(version, '13.3.0')) {
+      if (version && isOutdatedVersion('13.3.0', version)) {
         toggleMetamaskVersionWarning(true)
       } else {
         toggleMetamaskVersionWarning(false)
