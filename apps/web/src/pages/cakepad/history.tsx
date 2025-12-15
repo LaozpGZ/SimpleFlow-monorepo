8f9370fe53fa046dafc766e0e75f3ebcde28e11f
chef-ryan <ryan@pancakeswap.com>
2025-12-15 20:18:37 +0800

    update ifo supported chains

--- apps/web/src/pages/cakepad/history.tsx
+++ apps/web/src/pages/cakepad/history.tsx
@@ -6,6 +6,7 @@ import { NextPageWithLayout } from 'utils/page.types'
 import PastIfo from 'views/Ifos/PastIfo'
 import { PageMeta } from 'components/Layout/Page'
 import { useIfoConfigs } from 'views/Cakepad/hooks/useIfoConfigs'
+import { IFO_SUPPORT_CHAINS } from 'config/cakepad.config'
 
 const View = () => {
   useIfoConfigs()
@@ -23,6 +24,6 @@ const PastIfoPage = dynamic(() => Promise.resolve(View), {
   ssr: false,
 }) as NextPageWithLayout
 
-PastIfoPage.chains = [...SUPPORTED_CHAIN_IDS]
+PastIfoPage.chains = [...IFO_SUPPORT_CHAINS]
 
 export default PastIfoPage
