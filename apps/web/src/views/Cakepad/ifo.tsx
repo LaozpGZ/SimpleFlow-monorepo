d38a9191e8e12fd79c17a29fa3ef094cb238e81f
chef-ryan <ryan@pancakeswap.com>
2025-12-15 12:49:59 +0800

    update flicker issue

--- apps/web/src/views/Cakepad/ifo.tsx
+++ apps/web/src/views/Cakepad/ifo.tsx
@@ -1,4 +1,5 @@
 import { safeGetAddress } from 'utils'
+import { useCheckAndSwitchChain } from 'hooks/useCheckAndSwitchChain'
 import { IfoV2Provider } from './contexts/IfoV2Provider'
 import CurrentIfo from './CurrentIfo'
 import useIfo from './hooks/useIfo'
@@ -6,6 +7,7 @@ import { IfoPresetPage } from './components/IfoCards/IfoPresetCard/IfoPresetCard
 
 const DisplayIfo = () => {
   const { config } = useIfo()
+  useCheckAndSwitchChain(config?.chainId)
 
   // If no contract address, use preset data
   if ((!config.contractAddress || !safeGetAddress(config.contractAddress)) && config.presetData) {
