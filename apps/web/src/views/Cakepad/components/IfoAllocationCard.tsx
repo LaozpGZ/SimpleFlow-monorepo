a607ef45b421a1ef0532db8e3be917fc8b3e2c0b
Chef Penguin <penguin@pancakeswap.com>
2025-12-16 11:56:32 +0530

    chore: swap url based on chain

--- apps/web/src/views/Cakepad/components/IfoAllocationCard.tsx
+++ apps/web/src/views/Cakepad/components/IfoAllocationCard.tsx
@@ -1,7 +1,11 @@
 import { useTranslation } from '@pancakeswap/localization'
 import { Card, CardBody, Button, Text, FlexGap } from '@pancakeswap/uikit'
 import { NumberDisplay } from '@pancakeswap/widgets-internal'
+import { Native } from '@pancakeswap/sdk'
+import { CHAIN_QUERY_NAME } from 'config/chains'
+import { useMemo } from 'react'
 import AddToWalletButton from 'components/AddToWallet/AddToWalletButton'
+import useIfo from '../hooks/useIfo'
 
 interface IfoAllocationCardProps {
   symbol: string
@@ -17,8 +21,15 @@ export const IfoAllocationDisplay: React.FC<IfoAllocationCardProps> = ({
   allocatedAmount,
 }) => {
   const { t } = useTranslation()
+  const { chainId } = useIfo()
   const amount = allocatedAmount ?? '0'
-  const swapUrl = `https://pancakeswap.finance/swap?chain=bsc&inputCurrency=${tokenAddress}&outputCurrency=BNB`
+
+  const swapUrl = useMemo(() => {
+    const chainQueryName = chainId ? CHAIN_QUERY_NAME[chainId] : 'bsc'
+    const nativeCurrency = chainId ? Native.onChain(chainId) : Native.onChain(56) // Default to BSC if no chainId
+    const nativeSymbol = nativeCurrency.symbol
+    return `https://pancakeswap.finance/swap?chain=${chainQueryName}&inputCurrency=${tokenAddress}&outputCurrency=${nativeSymbol}`
+  }, [chainId, tokenAddress])
 
   return (
     <FlexGap flexDirection="column">
